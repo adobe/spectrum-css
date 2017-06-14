b14fa1f9028fda4ea4f479dde01217bafae476e3
Larry Davis <lawdavis@adobe.com>
2017-06-14 15:18:51 -0700

    SDL-86 spectrum-css npmignore makes src/ unavailable in releases
    * Don't copy src/ files into dist/, instead just let src/ be included

--- tasks/pre-build-copy.js
+++ tasks/pre-build-copy.js
@@ -34,8 +34,6 @@ var excludeExtensions = [
 ];
 
 
-var srcSource = 'src/';
-var srcDest = 'dist/src/';
 var abstractSource = 'node_modules/@spectrum/spectrum-abstract-stylus/dist/';
 var abstractDest = 'temp/abstract/';
 var iconStylusSource = 'node_modules/@spectrum/spectrum-icons/dist/font/stylus/';
@@ -59,10 +57,6 @@ function createDistDirs() {
     fsx.mkdirs(iconFontDest),
     fsx.mkdirs(iconStylusDest)
   ]);
-}
-
-function copySource() {
-  return asyncCopy('source', srcSource, srcDest);
 }
 
 function copyAbstractSource() {
@@ -92,7 +86,6 @@ module.exports = function() {
   log.info('Starting pre-processing');
   return createDistDirs().then(() => {
     return Promise.all([
-      copySource(),
       copyAbstractSource(),
       copyIconFont(),
       copyIconStylus()
