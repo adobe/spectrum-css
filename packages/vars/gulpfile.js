bba1d2f2b2be5b298f458bd2f1e4a115d908312f
Larry Davis <lawdavis@adobe.com>
2019-06-24 14:44:41 -0700

    Begin Lerna migration

---
+++ packages/vars/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const fs = require('fs');
+const del = require('del');
+
+gulp.task('clean', function() {
+  return del('dist/*');
+});
+
+gulp.task('build-prepare', function(cb) {
+  var dir = 'dist';
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  cb();
+});
+
+/**
+ * Builds a list of unique variables from DNA for each theme and scale.
+ */
+gulp.task('build-vars', function(cb) {
+  let vars = require('./');
+  for (let theme in vars.themes) {
+    fs.writeFileSync(`dist/spectrum-${theme}.css`, vars.generate(theme, vars.themes[theme]));
+  }
+
+  for (let scale in vars.scales) {
+    fs.writeFileSync(`dist/spectrum-${scale}.css`, vars.generate(scale, vars.scales[scale]));
+  }
+
+  cb();
+});
+
+gulp.task('default', gulp.series('clean', 'build-prepare', 'build-vars'));
