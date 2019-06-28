8a1e67681aec02633c9fd85785f0be2eaa5cd5ed
Larry Davis <lawdavis@adobe.com>
2019-06-28 00:49:03 -0700

    Select the current nav item

--- site/resources/js/site.js
+++ site/resources/js/site.js
@@ -3,19 +3,33 @@ window.addEventListener('DOMContentLoaded', function() {
     const params = new URLSearchParams(location.search);
     var iframe = document.querySelector('iframe');
     iframe.src = 'packages/' + params.get('component') + '/dist/docs/' + params.get('example') + '.html';
+    var navItem = document.querySelector('[data-component="' + params.get('component') + '"][data-example="' + params.get('example') + '"]');
+    if (navItem) {
+      selectNavItem(navItem.parentElement);
+    }
   }
 
   document.addEventListener('click', function(event) {
     var target = event.target;
     if (target.classList.contains('js-componentLink')) {
       navigate(target.getAttribute('data-component'), target.getAttribute('data-example'));
+      selectNavItem(target.parentElement);
     }
   });
-});
 
-function navigate(component, example) {
-  const params = new URLSearchParams(location.search);
-  params.set('component', component);
-  params.set('example', example);
-  window.history.replaceState({}, '', `${location.pathname}?${params}`);
-}
+  var selectedNavItem = null;
+  function selectNavItem(navItem) {
+    if (selectedNavItem) {
+      selectedNavItem.classList.remove('is-selected');
+    }
+    navItem.classList.add('is-selected');
+    selectedNavItem = navItem;
+  }
+
+  function navigate(component, example) {
+    const params = new URLSearchParams(location.search);
+    params.set('component', component);
+    params.set('example', example);
+    window.history.replaceState({}, '', `${location.pathname}?${params}`);
+  }
+});
