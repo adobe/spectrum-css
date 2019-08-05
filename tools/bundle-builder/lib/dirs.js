35dcbdeaeb001e21b85b182f6b10ce8415781c0e
Larry Davis <lawdavis@adobe.com>
2019-08-05 08:44:05 -0700

    Fix package resolution for toplevel builds

--- tools/bundle-builder/lib/dirs.js
+++ tools/bundle-builder/lib/dirs.js
@@ -16,18 +16,24 @@ exports.cwd = process.cwd();
 exports.builder = path.resolve(__dirname, '..');
 exports.siteResources = path.resolve(__dirname, '..', 'node_modules', '@spectrum-css', 'site');
 exports.topLevel = path.resolve(exports.builder, '..', '..');
+exports.isTopLevel = (exports.cwd === exports.topLevel);
 exports.site = path.resolve(exports.topLevel, 'site');
-exports.resolve = function(component) {
-  return path.dirname(require.resolve(component, {
-    paths: [
-      path.join(process.cwd(), 'node_modules')
-    ]
-  }));
-}
 
-if (exports.cwd === exports.topLevel) {
+if (exports.isTopLevel) {
   exports.components = path.join(exports.cwd, 'components');
 }
 else {
   exports.components = path.join(exports.cwd, 'node_modules', '@spectrum-css');
 }
+
+exports.resolve = function(component) {
+  if (exports.isTopLevel) {
+    return path.join(exports.components, component.split('/').pop());
+  }
+
+  return path.dirname(require.resolve(component, {
+    paths: [
+      path.join(process.cwd(), 'node_modules')
+    ]
+  }));
+};
