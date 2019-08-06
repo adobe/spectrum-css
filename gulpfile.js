ba35c0a419ade5d2a4f69023658844c0f40ebd67
Larry Davis <lawdavis@adobe.com>
2019-08-06 12:23:08 -0700

    More robust error handling for async tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-const builder = require(require('./tools/bundle-builder');
+const gulp = require('gulp');
+const builder = require('./tools/bundle-builder');
 const site = require('./site/gulpfile.js');
 Object.assign(exports, builder);
 Object.assign(exports, site);
