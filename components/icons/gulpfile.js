385768d5abaec5950036c1fa211faff325d3e36c
Larry Davis <lawdavis@adobe.com>
2019-08-08 12:33:57 -0700

    Add A4U update script, update icons to 4.1.0

--- components/icons/gulpfile.js
+++ components/icons/gulpfile.js
@@ -9,9 +9,90 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var svgstore = require('gulp-svgstore');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const path = require('path');
+const svgmin = require('gulp-svgmin');
+const replace = require('gulp-replace');
+const sort = require('gulp-sort');
+const svgcombiner = require('gulp-svgcombiner');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
+
+// Blocked by https://jira.corp.adobe.com/browse/A4U-177
+// const a4uPath = require.resolve('@a4u/a4u-collection-essential-ui-icons-release');
+const a4uPath = path.join(process.cwd(), '..', '..', 'node_modules', '@a4u/a4u-collection-essential-ui-icons-release');
+
+function clean() {
+  return del([
+    'combined/**',
+    'medium/**',
+    'large/**',
+  ]);
+}
+
+function copyLargeIcons() {
+  return gulp.src(path.join(a4uPath, 'assets', 'UI_Icons_SVG_Large/*.svg'))
+    .pipe(gulp.dest('large/'));
+}
+
+function copyMediumIcons() {
+  return gulp.src(path.join(a4uPath, 'assets', 'UI_Icons_SVG_Medium/*.svg'))
+    .pipe(gulp.dest('medium/'));
+}
+
+function sanitizeIcons() {
+  return gulp.src('{medium,large}/*.svg')
+    .pipe(replace(/ data-name=".*?"/g, ''))
+    .pipe(replace(/ id=".*?"/g, ''))
+    .pipe(replace(/ class=".*?"/g, ''))
+    .pipe(replace(/<defs>[\s\S]*?<\/defs>/m, ''))
+    .pipe(replace(/<title>[\s\S]*?<\/title>/m, ''))
+    .pipe(svgmin({
+      plugins: [
+        {
+          removeAttrs: {
+            attrs: [
+              'class',
+              'data-name',
+              'id'
+            ]
+          }
+        },
+        { collapseGroups: true }
+      ]
+    }))
+    .pipe(gulp.dest('./'));
+}
+
+function generateCombinedIcons() {
+  return gulp.src('{medium,large}/*.svg')
+    .pipe(sort())
+    .pipe(svgcombiner({
+      processName: function(filePath) {
+        // Clean filename
+        return path.basename(filePath, path.extname(filePath)).replace(/S_UI(.*?)_.*/, '$1');
+      },
+      processClass: function(filePath) {
+        // Return the last directory
+        return 'spectrum-UIIcon--' + path.dirname(filePath).split(path.sep).pop();
+      }
+    }))
+    .pipe(gulp.dest('combined/'));
+}
+
+// Only ran by Adobe
+let updateIcons = gulp.series(
+  gulp.series(
+    clean,
+    gulp.parallel(
+      copyMediumIcons,
+      copyLargeIcons
+    )
+  ),
+  sanitizeIcons,
+  generateCombinedIcons
+);
 
 var tasks = require('@spectrum-css/component-builder');
 
@@ -55,5 +136,6 @@ let build = gulp.parallel(
   tasks.buildCSS
 );
 
+exports.updateIcons = updateIcons;
 exports.build = exports.buildLite = build;
 exports.default = build;
