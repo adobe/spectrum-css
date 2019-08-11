cd360424e22786aafc2e6bedc63cdd27da674e02
Larry Davis <lawdavis@adobe.com>
2019-08-11 09:36:37 -0700

    Fix sidebar hide/show due to media query width mismatch

--- site/resources/js/site.js
+++ site/resources/js/site.js
@@ -174,25 +174,31 @@ window.addEventListener('DOMContentLoaded', function() {
   // Sidebar
   var sideBar = document.querySelector('#site-sidebar');
   var overlay = document.querySelector('#site-overlay');
-  let mql = window.matchMedia('(max-width: 768px)');
-  function handleMQLChange() {
-    if (mql.matches) {
+  let scaleMQL = window.matchMedia('(max-width: 768px)');
+  function handleScaleMQLChange() {
+    if (scaleMQL.matches) {
       switcher.scale = 'large';
     }
     else {
       switcher.scale = 'medium';
+    }
+  }
+  scaleMQL.addListener(handleScaleMQLChange);
 
+  let sidebarMQL = window.matchMedia('(max-width: 960px)');
+  function handleSidebarMQLChange() {
+    if (!sidebarMQL.matches) {
       // Get rid of the overlay if we resize while the sidebar is open
       hideSideBar();
     }
   }
+  sidebarMQL.addListener(handleSidebarMQLChange);
 
-  mql.addListener(handleMQLChange);
-
-  handleMQLChange();
+  handleScaleMQLChange();
+  handleSidebarMQLChange();
 
   function showSideBar() {
-    if (mql.matches) {
+    if (sidebarMQL.matches) {
       overlay.addEventListener('click', hideSideBar);
       sideBar.classList.add('is-open');
       overlay.classList.add('is-open');
