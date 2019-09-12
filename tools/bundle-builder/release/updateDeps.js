31786b130d3d4b982a718634c100a6878d010cfe
Larry Davis <lawdavis@adobe.com>
2019-09-12 07:45:32 -0700

    chore: generate a single commit per update

--- tools/bundle-builder/release/updateDeps.js
+++ tools/bundle-builder/release/updateDeps.js
@@ -1,65 +1,63 @@
-const gitSemverTags = require('git-semver-tags');
 const semver = require('semver');
 const path = require('path');
 const fsp = require('fs').promises;
 const gulp = require('gulp');
 const exec = require('../lib/exec');
 
-function getLastTag(bundlePackage) {
-  return new Promise((resolve, reject) => {
-    // Find the last release of this bundle
-    gitSemverTags({
-      lernaTags: true,
-      package: bundlePackage.name
-    }, (err, tags) => {
-      if (tags) {
-        let lastTag = tags.shift();
-        resolve(lastTag);
-      }
-      else {
-        reject(new Error('Could not find last tag!'))
-      }
-    });
-  });
-}
-
-async function getPackage(dir) {
+async function readPackage(dir) {
   return JSON.parse(await fsp.readFile(path.join(dir || '' , 'package.json')));
 }
 
+async function writePackage(pkg, dir) {
+  return (await fsp.writeFile(path.join(dir || '' , 'package.json'), JSON.stringify(pkg, null, 2)));
+}
+
 let releaseTypeToCommitType = {
-  'major': 'feat', // hope these don't happen
-  'premajor': 'feat', // hope these don't happen
+  'major': 'BREAKING CHANGE',
+  'premajor': 'feat', // hope these don't happen?
   'minor': 'feat',
   'preminor': 'feat',
   'patch': 'fix',
   'prepatch': 'fix',
-  'prerelease': 'fix' // hope these don't happen
+  'prerelease': 'fix' // hope these don't happen?
 };
 
-async function analyzeComponents(bundlePackage, lastTag) {
+async function analyzeComponents(bundlePackage) {
   let updates = [];
+
+  // This package is always kept up to date with breaking changes
+  let allowBreakingChanges = bundlePackage.name === '@spectrum-css/spectrum-css';
 
   for (let [depName, requiredDepVersion] of Object.entries(bundlePackage.devDependencies)) {
     if (depName.startsWith('@spectrum-css/')) {
       // Don't require.resolve here, we want to check what's actually in the repo
       let depPath = path.join('..', '..', 'components', depName.replace('@spectrum-css/', ''));
 
-      let depPackage = await getPackage(depPath);
+      let depPackage = await readPackage(depPath);
 
       // Check if the dep matches the package entry
-      if (semver.satisfies(depPackage.version, requiredDepVersion)) {
+      if (semver.satisfies(depPackage.version, requiredDepVersion) || allowBreakingChanges) {
         let exactRequiredDepVersion = requiredDepVersion.replace('^', '');
         let releaseType = semver.diff(exactRequiredDepVersion, depPackage.version);
 
         if (releaseType) {
           let commitType = releaseTypeToCommitType[releaseType];
-          console.log(`Updating ${depPackage.name} to ${depPackage.version}!`);
+          if (semver.inc(exactRequiredDepVersion, 'major') === depPackage.version) {
+            // We've gone from prerelease to major
+            // diff does not catch this https://github.com/npm/node-semver/issues/288
+            // in the case of 2.0.0 components, this is a non-breaking feature
+            // in the future, it won't be. this code should be changed to consider this a breaking change
+            commitType = 'feat';
+          }
+
+          console.log(`Updating ${depPackage.name} to ${depPackage.version} due to ${commitType} commit (release type ${releaseType})!`);
 
           updates.push({
             name: depPackage.name,
+            oldVersion: exactRequiredDepVersion,
             version: depPackage.version,
-            type: commitType
+            type: commitType,
+            releaseType: releaseType
           });
         }
       }
@@ -72,52 +70,59 @@ async function analyzeComponents(bundlePackage, lastTag) {
   return updates;
 }
 
-function findUpdates() {
-  let bundlePackage;
-  let lastTag;
-  return getPackage()
-    .then(package => {
-      bundlePackage = package;
-      return getLastTag(bundlePackage);
-    })
-    .then(tag => {
-      lastTag = tag;
-      return analyzeComponents(bundlePackage, lastTag);
-    });
+function updateDep(pkg, update) {
+  pkg.devDependencies[update.name] = `^${update.version}`;
 }
 
-async function updateDep(dependency) {
-  let pkg = await getPackage();
-  pkg.devDependencies[dependency.name] = dependency.version;
-  return fsp.writeFile('package.json', JSON.stringify(pkg, null, 2));
-}
+let typePriority = {
+  'patch': 0,
+  'minor': 1,
+  'major': 2
+};
 
 async function updateDeps() {
-  let tasks = await findUpdates()
-    .then(changedDependencies => {
-      let tasks = [];
-      for (let dependency of changedDependencies.filter(Boolean)) {
-        // Update each dep, and create one commit per update
-        let task = () => {
-          let message = `${dependency.type}: update ${dependency.name} to ${dependency.version}`;
-          return updateDep(dependency)
-            .then(() => {
-              return exec.promise(`git commit package.json -m "${message}"`);
-            });
-        };
+  let pkg = await readPackage();
+  let updates = (await analyzeComponents(pkg)).filter(Boolean);
 
-        tasks.push(task);
-      }
-      return tasks;
-    });
+  if (!updates.length) {
+    console.log(`Not updating dependencies for ${pkg.name}, no changes since last release`);
+    return;
+  }
 
-  function runTask() {
-    if (tasks.length) {
-      tasks.shift()().then(runTask );
+  let type = 'patch';
+  let message = '';
+  let breaking = '';
+  for (let update of updates) {
+    // Release commit should have the type of the most impactful change
+    if (typePriority[update.releaseType] > typePriority[type]) {
+      type = update.releaseType;
+    }
+    updateDep(pkg, update);
+    let messageString = ` - ${update.type}: update ${update.name} from ${update.oldVersion} to ${update.version}\n`;
+
+    if (update.releaseType === 'major') {
+      breaking += messageString;
+    }
+    else {
+      message += messageString;
     }
   }
 
-  runTask();
+  // Increment the version of the package
+  let increment = type;
+
+  if (pkg.version.match(/-(alpha)|(beta)/)) {
+    increment = 'prerelease';
+  }
+
+  pkg.version = semver.inc(pkg.version, increment);
+
+  message = `chore(release): release ${pkg.name}@${pkg.version}\n\n${breaking}${message}`;
+
+  await writePackage(pkg);
+
+  await exec.promise(`git commit package.json -m "${message}"`);
+  await exec.promise(`git tag "${pkg.name}@${pkg.version}"`);
 }
 
 module.exports = updateDeps;
