4d4fc5b3ba3f58c4fa271df294994af3dceb86fd
Larry Davis <lawdavis@adobe.com>
2019-09-27 16:49:17 -0700

    chore: graduate peer dependency versions ahead of release

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,33 @@ async function releaseBundles() {
   await subrunner.runTaskOnPackages('release', bundles);
 };
 
+function graduatePeerDeps() {
+  function isPrerelease(version) {
+    return version.match(/-(alpha)|(beta)/);
+  }
+
+  return gulp.src('components/*/package.json')
+   .pipe(through.obj(function translateJSON(file, enc, cb) {
+      let data = JSON.parse(String(file.contents));
+
+      if (data.peerDependencies) {
+        for (let [peerDep, version] of Object.entries(data.peerDependencies)) {
+          if (isPrerelease(version)) {
+            version = version.replace('^', '');
+            let newVersion = `^${semver.major(version)}.${semver.minor(version)}.${semver.patch(version)}`;
+            console.log(`${data.name}: Graduating ${peerDep} to ${newVersion}`);
+            data.peerDependencies[peerDep] = newVersion;
+          }
+        }
+      }
+
+      file.contents = Buffer.from(JSON.stringify(data, null, 2));
+
+      cb(null, file);
+    }))
+    .pipe(gulp.dest('components/'));
+}
+
 function packageLint() {
   return gulp.src('components/*/package.json')
    .pipe(through.obj(function translateJSON(file, enc, cb) {
@@ -127,6 +154,7 @@ See the [Spectrum CSS documentation](https://opensource.adobe.com/spectrum-css/)
   }));
 }
 
+exports.graduatePeerDeps = graduatePeerDeps;
 exports.readmeLint = readmeLint;
 exports.packageLint = packageLint;
 
