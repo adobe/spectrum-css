258f4a2ce9b1b9ff8de4db4a98225fcbc26e9028
Jian Liao <jianliao@adobe.com>
2019-10-15 10:44:26 -0700

    fix: set correct base url for sitemap generation

--- sitemap_generator.js
+++ sitemap_generator.js
@@ -1,16 +1,7 @@
 const sitemap=require('nextjs-sitemap-generator');
-const version=require('./package').version;
-const fs = require('fs');
 
 sitemap({
-  baseUrl: `https://opensource.adobe.com/spectrum-css/${version}/docs`,
+  baseUrl: 'https://opensource.adobe.com/spectrum-css',
   pagesDirectory: './spectrum-css/components/',
   targetDirectory : './spectrum-css/static/'
 });
-
-fs.writeFileSync('./spectrum-css/robots.txt', `# All robots allowed
-User-agent: *
-Disallow:
-
-# Sitemap files
-Sitemap: https://opensource.adobe.com/spectrum-css/${version}/docs/static/sitemap.xml`);
