bfc76aaef3dff1812549716e36636923d2edc924
Larry Davis <lawdavis@adobe.com>
2019-11-07 13:45:24 -0800

    fix: correct positioning of children of wrapped Dialogs, closes #389
    * fix Dialog examples if page loaded directly
    * add wrapped Dialog example

--- site/resources/js/enhancement.js
+++ site/resources/js/enhancement.js
@@ -490,11 +490,24 @@ function openDialog(dialog, withOverlay) {
   }
 
   dialog.classList.add('is-open');
+
+  // Support wrapped dialogs
+  var innerDialog = dialog.querySelector('.spectrum-Dialog');
+  if (innerDialog) {
+    innerDialog.classList.add('is-open');
+  }
 }
 
 function closeDialog(dialog) {
   document.getElementById('spectrum-underlay').classList.remove('is-open');
   dialog.classList.remove('is-open');
+
+  // Support wrapped dialogs
+  var innerDialog = dialog.querySelector('.spectrum-Dialog');
+  if (innerDialog) {
+    innerDialog.classList.remove('is-open');
+  }
+
   setTimeout(function() {
     dialog.classList.remove('spectrum-CSSExample-dialog');
   }, 10);
