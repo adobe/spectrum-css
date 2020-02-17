629013ebfba6c8cf84436e3be32b79951852e2ee
Jian Liao <jianliao@adobe.com>
2020-02-17 14:08:24 -0800

    test: only upload when is a pr

--- backstop_data/deploy_comment.sh
+++ backstop_data/deploy_comment.sh
@@ -2,33 +2,34 @@
 
 set -e
 
-# Generate the dynamic branch name with short commit hash and current timestamp
-BRANCH_NAME=$(git rev-parse --short HEAD)-result-$(date +%Y-%m-%d_%H-%M-%S)
-
-# Create the folder with the branch name and copy the result into it
-mkdir -p $HOME/temp/$BRANCH_NAME && cp -R backstop_data/* $HOME/temp/$BRANCH_NAME
-
-# Create github action folder and copy the action file into it
-mkdir -p $HOME/temp/.github/workflows/ && cp backstop_data/push.yml $HOME/temp/.github/workflows/
-
-cd $HOME/temp
-
-git init .
-
-git checkout --orphan $BRANCH_NAME
-
-git add -A .
-
-git commit -q -m "Deploy ${TRAVIS_REPO_SLUG} VR test result to github.com/jianliao/spectrum-css-vr-test-result.git:${BRANCH_NAME}"
-
-git show --stat-count=10 HEAD
-
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/jianliao/spectrum-css-vr-test-result.git
-
-git push origin $BRANCH_NAME
-
-# Create a comment to the PR
+# Only upload test result when the job is triggered by pull request
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+
+    # Generate the dynamic branch name with short commit hash and current timestamp
+    BRANCH_NAME=$(git rev-parse --short HEAD)-result-$(date +%Y-%m-%d_%H-%M-%S)
+
+    # Create the folder with the branch name and copy the result into it
+    mkdir -p $HOME/temp/$BRANCH_NAME && cp -R backstop_data/* $HOME/temp/$BRANCH_NAME
+
+    # Create github action folder and copy the action file into it
+    mkdir -p $HOME/temp/.github/workflows/ && cp backstop_data/push.yml $HOME/temp/.github/workflows/
+
+    cd $HOME/temp
+
+    git init .
+
+    git checkout --orphan $BRANCH_NAME
+
+    git add -A .
+
+    git commit -q -m "Deploy ${TRAVIS_REPO_SLUG} VR test result to github.com/jianliao/spectrum-css-vr-test-result.git:${BRANCH_NAME}"
+
+    git show --stat-count=10 HEAD
+
+    git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/jianliao/spectrum-css-vr-test-result.git
+
+    git push origin $BRANCH_NAME
+
     TEST_RESULT_URL=https://jianliao.github.io/spectrum-css-vr-test-result/$BRANCH_NAME/html_report/index.html
     echo !!!! TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST
     echo !!!! Test result url: $TEST_RESULT_URL
