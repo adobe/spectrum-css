3a8e9e8b3c3b064cb9dcf92cf2949844bdd1ecf4
Larry Davis <lawdavis@adobe.com>
2020-03-03 12:44:27 -0800

    feat: refactor Stepper to use new Textfield properly
    BREAKING CHANGE: Must apply .is-disabled for disabled steppers
    
    BREAKING CHANGE: Must apply .is-focused and .is-keyboardFocused

--- site/resources/js/enhancement.js
+++ site/resources/js/enhancement.js
@@ -126,6 +126,36 @@ governing permissions and limitations under the License.
   });
 }());
 
+// Stepper
+(function() {
+  function setFocus(stepper, input, focused) {
+    var focusClass = input.classList.contains('focus-ring') ? 'is-keyboardFocused' : 'is-focused';
+    if (focused) {
+      stepper.classList.add(focusClass);
+    }
+    else {
+      stepper.classList.remove('is-keyboardFocused');
+      stepper.classList.remove('is-focused');
+    }
+  }
+
+  document.addEventListener('focusin', function(event) {
+    var stepper = event.target.closest('.spectrum-Stepper');
+
+    if (stepper) {
+      setFocus(stepper, event.target, true);
+    }
+  });
+
+  document.addEventListener('focusout', function(event) {
+    var stepper = event.target.closest('.spectrum-Stepper');
+
+    if (stepper) {
+      setFocus(stepper, event.target, false);
+    }
+  });
+}());
+
 // Dropdown
 (function() {
   var openDropdown = null;
