8d1281c0c4f86226466fdb78163d245b5ed91a7e
Jian Liao <jianliao@adobe.com>
2020-05-18 14:30:46 -0700

    feat: add adobe global footer

---
+++ site/includes/adobe-globalnav.js
@@ -0,0 +1,17 @@
+<script>
+window.adobeGlobalnavConfig = {
+  locale: 'en',
+  footer: {
+      target: document.getElementById('footer_global'),
+      theme:{
+          backgroundColor:'#FFFFFF',
+          columnTitleColor: '#00FF00',
+          linkColor: '#4B4B4B',
+          linkHoverColor: '#2C2C2C',
+          separatorColor: '',
+          caretColor: ''
+      },
+      excludeNavigation: true
+  }
+}
+</script>
