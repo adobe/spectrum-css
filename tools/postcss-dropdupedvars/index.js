39adb3d53c7bc2b043f033b759ae12f7cd7bf560
Larry Davis <lazdnet@gmail.com>
2020-10-06 09:54:25 -0700

    feat: add postcss-dropdupedvars

---
+++ tools/postcss-dropdupedvars/index.js
@@ -0,0 +1,22 @@
+const postcss = require('postcss');
+
+function dropDupes(root, variableList) {
+  root.walkRules((rule, ruleIndex) => {
+    let seen = {};
+    rule.walkDecls((decl) => {
+      if (decl.prop.startsWith('--')) {
+        if (seen[decl.prop]) {
+          seen[decl.prop].remove();
+        }
+
+        seen[decl.prop] = decl;
+      }
+    });
+  });
+}
+
+module.exports = postcss.plugin('postcss-remapvars', function() {
+  return (root, result) => {
+    dropDupes(root);
+  }
+});
