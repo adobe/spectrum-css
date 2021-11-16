25137e69cfff4efd9468e4c62126e542da5d951c
Larry Davis <lazdnet@gmail.com>
2021-11-16 11:36:54 -0800

    feat: use PickerButton in Combobox and DatePicker
    BREAKING CHANGE: InputGroup now uses PickerButton, see migration guide

--- site/resources/js/enhancement.js
+++ site/resources/js/enhancement.js
@@ -96,21 +96,45 @@ governing permissions and limitations under the License.
 
 // Inputgroup
 (function() {
-  function setFocus(inputgroup, input, focused) {
-    var focusClass = input.classList.contains('focus-ring') ? 'is-keyboardFocused' : 'is-focused';
+  function setFocus(inputgroup, focused, event) {
+    var textfields = inputgroup.querySelectorAll('.spectrum-Textfield');
+    var inputs = inputgroup.querySelectorAll('.spectrum-InputGroup-input');
+    var input = inputs[0];
+    var focusClass = event.target.classList.contains('focus-ring') ? 'is-keyboardFocused' : 'is-focused';
+    var pickerButton = inputgroup.querySelector('.spectrum-PickerButton');
     if (focused) {
       inputgroup.classList.add(focusClass);
+      pickerButton.classList.add(focusClass);
+      if (event.target.tagName !== 'INPUT') {
+        input.focus();
+      }
+
+      Array.prototype.forEach.call(textfields, (textfield) => {
+        textfield.classList.add(focusClass);
+      });
     } else {
+      pickerButton.classList.remove('is-keyboardFocused');
+      pickerButton.classList.remove('is-focused');
       inputgroup.classList.remove('is-keyboardFocused');
       inputgroup.classList.remove('is-focused');
+
+      Array.prototype.forEach.call(textfields, (textfield) => {
+        textfield.classList.remove('is-focused');
+        textfield.classList.remove('is-keyboardFocused');
+      });
     }
   }
 
   document.addEventListener('focusin', function(event) {
     var inputgroup = event.target.closest('.spectrum-InputGroup');
 
+    if (event.target.closest('.spectrum-Menu')) {
+      // Don't mess with focus on menuitems
+      return;
+    }
+
     if (inputgroup) {
-      setFocus(inputgroup, event.target, true);
+      setFocus(inputgroup, true, event);
     }
   });
 
@@ -118,7 +142,7 @@ governing permissions and limitations under the License.
     var inputgroup = event.target.closest('.spectrum-InputGroup');
 
     if (inputgroup) {
-      setFocus(inputgroup, event.target, false);
+      setFocus(inputgroup, false, event);
     }
   });
 }());
