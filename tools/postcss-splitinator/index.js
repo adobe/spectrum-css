f97cd97e3ba45eb0484789a916cb204c242867ee
Larry Davis <lazdnet@gmail.com>
2022-05-25 11:42:59 -0700

    test: fix tests
    also add license

--- tools/postcss-splitinator/index.js
+++ tools/postcss-splitinator/index.js
@@ -1,3 +1,15 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 const postcss = require('postcss');
 
 function getName(selector, prop) {
@@ -16,7 +28,7 @@ function getName(selector, prop) {
 
   let selectorParts = selector.replace(/\s+/g, '').replace(/,/g, '').split('.');
 
-  return `--system-` + (`${selectorParts.join('-')}-${prop.substr(2)}`).replace(/-+/g, '-').toLowerCase();
+  return '--' + (`system-` + `${selectorParts.join('-')}-${prop.substr(2)}`).replace(/-+/g, '-').toLowerCase();
 }
 
 function process(root, options) {
