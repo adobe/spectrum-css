3b1663847398fb18a2340dedba6fd047c8d43cce
Patrick Fulton <pfulton@adobe.com>
2023-02-07 14:00:24 -0500

    feat: adds inlinealert stories

--- components/inlinealert/stories/inlinealert.stories.js
+++ components/inlinealert/stories/inlinealert.stories.js
@@ -3,23 +3,45 @@ import { Template } from "./template";
 
 export default {
   title: "Inlinealert",
-  description: "The Inlinealert component is...",
+  description: "In-line alerts display a non-modal message associated with objects in a view. These are often used in form validation, providing a place to aggregate feedback related to multiple fields.",
   component: "Inlinealert",
   argTypes: {
-    size: {
-      name: "Size",
+    headerText: {
+      name: "Header Text",
       type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        disable: false,
+        category: "Component",
+      },
+      control: { type: "text" },
+    },
+    text: {
+      name: "Text",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        disable: false,
+        category: "Component",
+      },
+      control: { type: "text" },
+    },
+    variant: {
+      name: "In-line alert variants",
+      type: { name: "string" },
       table: {
         type: { summary: "string" },
         category: "Component",
       },
-      options: ["s", "m", "l", "xl"],
+      options: ["neutral", "info", "positive", "notice", "negative", "closable"],
       control: "select"
-    },
+    }
   },
   args: {
-    rootClass: "spectrum-",
-    size: "m",
+    rootClass: "spectrum-InLineAlert",
+    headerText: "in-line alert header",
+    text: "This is an alert.",
+    variant: "neutral",
   },
   parameters: {
     actions: {
--- components/inlinealert/stories/template.js
+++ components/inlinealert/stories/template.js
@@ -2,12 +2,17 @@ import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 // import { ifDefined } from 'lit-html/directives/if-definedjs';
 
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js"
+import { Template as Button } from "@spectrum-css/button/stories/template.js"
+
 import "../index.css";
 
 export const Template = ({
   rootClass = "spectrum-InlineAlert",
   customClasses = [],
-  size = "m",
+  headerText,
+  text,
+  variant="neutral",
   ...globals
 }) => {
   const { express } = globals;
@@ -19,11 +24,57 @@ export const Template = ({
     console.warn(e);
   }
 
+  let iconName;
+  switch (variant) {
+    case 'info':
+      iconName = 'Info'
+      break;
+    case 'positive':
+      iconName = 'CheckmarkCircle';
+      break;
+    case 'notice':
+    case 'negative':
+    case 'closable':
+      iconName = 'Alert';
+      break;
+    default:
+      iconName = undefined;
+  }
+
+  const iconMarkup = typeof iconName !== 'undefined' ?
+    html`
+      ${Icon({
+        ...globals,
+        iconName
+      })}
+    ` : '';
+
+  const closableMarkup = variant === 'closable' ? 
+    html`
+      <div class="spectrum-InLineAlert-footer">
+        ${Button({
+          ...globals,
+          treatment: 'outline',
+          variant: 'primary',
+          icon: false,
+          hideLabel: false,
+          label: 'Ok',
+        })}
+      </div>
+    ` : '';
+
   return html`
     <div class=${classMap({
       [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+      [`${rootClass}--${variant}`]: typeof variant !== "undefined",
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    })}>
+      <div class="${rootClass}-header">
+        ${variant.charAt(0).toUpperCase() + variant.slice(1)} ${headerText}
+        ${iconMarkup}
+      </div>
+      <div class="${rootClass}-content">${text}</div>
+      ${closableMarkup}
+    </div>
   `;
 }
