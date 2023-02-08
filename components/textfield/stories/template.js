c2683815ea200d7540e4c9a3fb3a87ad03b99568
Patrick Fulton <pfulton@adobe.com>
2023-02-08 13:59:26 -0500

    fix(textfield): remove skin.css import from storyfile

--- components/textfield/stories/template.js
+++ components/textfield/stories/template.js
@@ -5,7 +5,6 @@ import { classMap } from "lit-html/directives/class-map.js";
 import { Template as Icon } from '@spectrum-css/icon/stories/template.js';
 
 import '../index.css';
-import '../skin.css';
 
 export const Template = ({
   rootClass = "spectrum-Textfield",
