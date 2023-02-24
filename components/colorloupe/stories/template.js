73322c261771aa83133b511351dd4417be146a00
castastrophe <castastrophe@users.noreply.github.com>
2023-02-24 12:25:10 -0500

    chore: generate new storybook assets

---
+++ components/colorloupe/stories/template.js
@@ -0,0 +1,21 @@
+import { html } from 'lit-html';
+import { classMap } from 'lit-html/directives/class-map.js';
+// import { ifDefined } from 'lit-html/directives/if-defined.js';
+
+import '../index.css';
+import '../skin.css';
+
+export const Template = ({
+  rootClass = "spectrum-ColorLoupe",
+  size = "m",
+  customClasses = [],
+  // ...globals
+}) => {
+  return html`
+    <div class=${classMap({
+    [rootClass]: true,
+    [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+    ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+  })}></div>
+  `;
+}
