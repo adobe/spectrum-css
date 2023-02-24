32ad8922fb7b2b416b19ad0ebb6ff3756b86631c
Megan Logsdon <megan@heysparkbox.com>
2023-02-24 16:14:19 -0500

    feat: add template for cycle button story

--- components/cyclebutton/stories/template.js
+++ components/cyclebutton/stories/template.js
@@ -2,20 +2,54 @@ import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 // import { ifDefined } from 'lit-html/directives/if-defined.js';
 
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js"
+
 import '../index.css';
 import '../skin.css';
+
+import "@spectrum-css/actionbutton/index.css"
 
 export const Template = ({
   rootClass = "spectrum-CycleButton",
   customClasses = [],
   size = "m",
-  // ...globals
+  initialIcon = "Play",
+  selectedIcon = "Pause",
+  isSelected,
+  ...globals
 }) => {
+  
   return html`
-    <div class=${classMap({
+    <button class=${classMap({
+      "spectrum-ActionButton": true, 
+      [`spectrum-ActionButton--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+      "spectrum-ActionButton--quiet": true,
       [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    })} 
+      @click=${(evt) => {
+        const icons = evt.target.querySelectorAll('svg');
+        for (var i = 0; i < icons.length; ++i) {
+          icons[i].classList.toggle('is-selected');
+        }
+      }}
+    >
+    ${initialIcon ? 
+      Icon({
+        ...globals,
+        iconName: initialIcon,
+        size,
+        customClasses: ['spectrum-ActionButton-icon', `${rootClass}-item`, (!isSelected ? 'is-selected' : '')]
+      })
+    : ""}
+    ${selectedIcon ? 
+      Icon({
+        ...globals,
+        iconName: selectedIcon,
+        size,
+        customClasses: ['spectrum-ActionButton-icon', `${rootClass}-item`, (isSelected ? 'is-selected' : '')]
+      })
+    : ""}
+  </button>
   `;
 }
