9b3e00320011882e314cb2d0bc078b94c57c6600
castastrophe <castastrophe@users.noreply.github.com>
2023-02-24 12:23:22 -0500

    feat: add story for infield button component

--- components/infieldbutton/stories/infieldbutton.stories.js
+++ components/infieldbutton/stories/infieldbutton.stories.js
@@ -1,6 +1,8 @@
 // Import the component markup template
 import { Template } from "./template";
 
+import { default as IconStories } from "../../icon/stories/icon.stories.js";
+
 export default {
   title: "Infield button",
   description: "The Infield button component is...",
@@ -16,10 +18,53 @@ export default {
       options: ["s", "m", "l", "xl"],
       control: "select"
     },
+    quiet: {
+      name: "Quiet",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean"
+    },
+    position: {
+      name: "Position",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      options: ["left", "right"],
+      control: "select"
+    },
+    icon:
+      IconStories && IconStories.argTypes && IconStories.argTypes.iconName
+        ? IconStories.argTypes.iconName
+        : {},
+    isDisabled: {
+      name: "Disabled",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean"
+    },
+    isInvalid: {
+      name: "Invalid",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean"
+    },
   },
   args: {
-    rootClass: "spectrum-",
+    rootClass: "spectrum-InfieldButton",
     size: "m",
+    position: "left",
+    icon: "Add"
   },
   parameters: {
     actions: {
