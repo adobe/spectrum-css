2a60a6497fcecae7323b2a1f0a11667d65951830
castastrophe <castastrophe@users.noreply.github.com>
2023-02-24 12:23:22 -0500

    feat: add story for infield button component

--- components/infieldbutton/stories/template.js
+++ components/infieldbutton/stories/template.js
@@ -2,19 +2,43 @@ import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 // import { ifDefined } from 'lit-html/directives/if-defined.js';
 
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js"
+
 import "../index.css";
 
 export const Template = ({
   rootClass = "spectrum-InfieldButton",
   customClasses = [],
   size = "m",
-  // ...globals
+  position = "start",
+  quiet,
+  icon = "Add",
+  isDisabled,
+  isInvalid,
+  ...globals
 }) => {
+
+  const iconMarkup = typeof icon !== 'undefined' ?
+    html`
+      ${Icon({
+        ...globals,
+        iconName: icon,
+        customClasses: [`${rootClass}-icon`],
+      })}
+    ` : '';
+  
   return html`
-    <div class=${classMap({
+    <button class=${classMap({
       [rootClass]: true,
       [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+      [`${rootClass}--${position}`]: typeof position !== "undefined",
+      [`${rootClass}--quiet`]: quiet,
+      "is-invalid": isInvalid,
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+  })} ?disabled=${isDisabled} aria-haspopup="listbox">
+    <div class="${rootClass}-fill">
+      ${iconMarkup}
+    </div>
+  </button>
   `;
 }
