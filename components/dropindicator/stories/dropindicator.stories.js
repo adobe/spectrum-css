199f3da18e930591e3f994445d69c2c2ecbc410c
castastrophe <castastrophe@users.noreply.github.com>
2023-02-27 20:51:06 -0500

    chore: slider storybook assets

--- components/dropindicator/stories/dropindicator.stories.js
+++ components/dropindicator/stories/dropindicator.stories.js
@@ -3,23 +3,34 @@ import { Template } from "./template";
 
 export default {
   title: "Drop indicator",
-  description: "The Drop indicator component is...",
+  description: "The Drop indicator component is used to show the insertion position into a list or table.",
   component: "Dropindicator",
   argTypes: {
-    size: {
-      name: "Size",
+    direction: {
+      name: "Direction",
       type: { name: "string", required: true },
       table: {
         type: { summary: "string" },
         category: "Component",
       },
-      options: ["s", "m", "l", "xl"],
+      options: ["horizontal", "vertical"],
       control: "select"
     },
+    size: {
+      name: "Size",
+      description: "Size of the drop indicator, requires a unit be provided; i.e., 200px or 100%.",
+      type: { name: "string", required: false },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      control: { type: "text" },
+    }
   },
   args: {
-    rootClass: "spectrum-",
-    size: "m",
+    rootClass: "spectrum-DropIndicator",
+    direction: "vertical",
+    size: "300px"
   },
   parameters: {
     actions: {
