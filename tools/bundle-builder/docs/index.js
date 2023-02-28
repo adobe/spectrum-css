1b52b031b00410ad6abbd8ba6064634f7f34645e
castastrophe <castastrophe@users.noreply.github.com>
2023-02-28 09:04:39 -0500

    chore: update copyrights in repo

--- tools/bundle-builder/docs/index.js
+++ tools/bundle-builder/docs/index.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Adobe. All rights reserved.
+Copyright 2023 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -252,7 +252,7 @@ function buildSite_getData() {
     } catch (safeloadError) {
       logger.error('Uh, oh... during buildDocs_getData, yaml loading failed for'.yellow, componentName.red);
       throw safeloadError;
-    } 
+    }
 
     if (path.basename(file.basename) === 'metadata.yml') {
       file.basename = componentName;
