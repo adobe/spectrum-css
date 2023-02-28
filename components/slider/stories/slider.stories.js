5f37a225c01c1ea161fc230d8752a678e5763717
castastrophe <castastrophe@users.noreply.github.com>
2023-02-27 20:51:06 -0500

    chore: slider storybook assets

--- components/slider/stories/slider.stories.js
+++ components/slider/stories/slider.stories.js
@@ -120,6 +120,11 @@ Default.args = {
   max: 20,
   values: [14],
   step: 2,
+  showOffset: false,
+};
+Default.argTypes = {
+  isRange: { table: { disable: true } },
+  isRamp: { table: { disable: true } },
 };
 
 export const Filled = Template.bind({});
@@ -138,7 +143,10 @@ FilledOffset.args = {
 export const Ramp = Template.bind({});
 Ramp.args = {
   ...Default.args,
-  variant: "ramp",
+  isRamp: true,
+};
+Ramp.argTypes = {
+  fillColor: { table: { disable: true } },
 };
 
 export const Range = Template.bind({});
@@ -158,4 +166,7 @@ export const Disabled = Template.bind({});
 Disabled.args = {
   ...Default.args,
   isDisabled: true,
+};
+Disabled.argTypes = {
+  fillColor: { table: { disable: true } },
 };
