51fa8afbe7f61ff7690eec9429cae256a2881e54
Patrick Fulton <360251+pfulton@users.noreply.github.com>
2023-03-01 16:13:40 -0500

    chore: add inlinealert, progress circle, picker button, statuslight storybook (#1638)
    * feat: adds inlinealert stories
    
    * feat: adds progresscircle stories
    
    * chore: fixup classname, add progresscircle
    
    * feat(pickerbutton): add story and template
    
    * feat(statuslight): add story and template
    
    * fix: implement changes based upon PR feedback
    
    * chore: tidy up after PR review

---
+++ components/inlinealert/stories/inlinealert.stories.js
@@ -0,0 +1,67 @@
+// Import the component markup template
+import { Template } from "./template";
+
+export default {
+  title: "In-line Alert",
+  description: "In-line alerts display a non-modal message associated with objects in a view. These are often used in form validation, providing a place to aggregate feedback related to multiple fields.",
+  component: "InLineAlert",
+  argTypes: {
+    headerText: {
+      name: "Header Text",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        disable: false,
+        category: "Content",
+      },
+      control: { type: "text" },
+    },
+    text: {
+      name: "Text",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        disable: false,
+        category: "Component",
+      },
+      control: { type: "text" },
+    },
+    variant: {
+      name: "Variants",
+      type: { name: "string" },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      options: ["neutral", "info", "positive", "notice", "negative", "closable"],
+      control: "select"
+    },
+    isClosable: {
+      name: "Closable",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Advanced",
+      },
+      control: "boolean",
+    },
+  },
+  args: {
+    rootClass: "spectrum-InLineAlert",
+    headerText: "in-line alert header",
+    text: "This is an alert.",
+    variant: "neutral",
+    isClosable: false,
+  },
+  parameters: {
+    actions: {
+      handles: []
+    },
+    status: {
+      type: process.env.MIGRATED_PACKAGES.includes('inlinealert') ? 'migrated' : undefined
+    }
+  }
+};
+
+export const Default = Template.bind({});
+Default.args = {};
--- components/statuslight/stories/statuslight.stories.js
+++ components/statuslight/stories/statuslight.stories.js
@@ -16,10 +16,52 @@ export default {
       options: ["s", "m", "l", "xl"],
       control: "select"
     },
+    label: {
+      name: "Label",
+      type: { name: "string" },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      control: { type: "text" },
+    },
+    variant: {
+      name: "Variant",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      options: [
+        "accent",
+        "info",
+        "neutral",
+        "positive",
+        "notice",
+        "negative",
+        "gray",
+        "red",
+        "orange",
+        "yellow",
+        "chartreuse",
+        "celery",
+        "green",
+        "seafoam",
+        "cyan",
+        "blue",
+        "indigo",
+        "purple",
+        "fuchsia",
+        "magenta",
+      ],
+      control: "select"
+    },
   },
   args: {
-    rootClass: "spectrum-",
+    rootClass: "spectrum-StatusLight",
     size: "m",
+    label: "Status",
+    variant: "info",
   },
   parameters: {
     actions: {
