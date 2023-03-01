d15016fedfdd8f78507e37498a905a2ff0781719
castastrophe <castastrophe@users.noreply.github.com>
2023-03-01 15:44:30 -0500

    chore: add inputgroup story and related element updates

--- components/pickerbutton/stories/template.js
+++ components/pickerbutton/stories/template.js
@@ -15,8 +15,12 @@ export const Template = ({
   size = "m",
   label,
   position,
+<<<<<<< HEAD
   iconType = "ui",
   iconName = "ChevronDown200",
+=======
+  icon = "menu",
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
   isDisabled = false,
   isFocused = false,
   isOpen = false,
@@ -24,35 +28,67 @@ export const Template = ({
   isInvalid = false,
   isQuiet = false,
   customClasses = [],
+<<<<<<< HEAD
   isRounded = false,
   customStyles = {},
   onclick,
+=======
+  customStyles = {},
+  id,
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
   ...globals
 }) => {
   const [_, updateArgs] = useArgs();
 
+<<<<<<< HEAD
+=======
+  let iconName = "ChevronDown100";
+  switch (size) {
+    case "s":
+      iconName = "ChevronDown75";
+      break;
+    case "l":
+      iconName = "ChevronDown200";
+      break;
+    case "xl":
+      iconName = "ChevronDown300";
+      break;
+  }
+
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
   return html`
     <button
       class=${classMap({
         [rootClass]: true,
+<<<<<<< HEAD
         [`${rootClass}--textuiicon`]: label && iconType === "ui",
         [`${rootClass}--uiicononly`]: !label && iconType === "ui",
         [`${rootClass}--texticon`]: label && iconType !== "ui",
         [`${rootClass}--icononly`]: !label && iconType !== "ui",
         [`${rootClass}--${position}`]: typeof position !== "undefined",
         [`${rootClass}--rounded`]: isRounded,
+=======
+        [`${rootClass}--uiicononly`]: !label,
+        [`${rootClass}--${position}`]: typeof position !== "undefined",
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
         [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
         'is-disabled': isDisabled,
         'is-focused': isFocused,
         'is-open': isOpen && !isDisabled,
+<<<<<<< HEAD
         'is-invalid': isInvalid && !isDisabled,
         'is-valid': isValid && !isDisabled,
+=======
+        'is-invalid': isInvalid,
+        'is-valid': isValid,
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
         [`${rootClass}--quiet`]: isQuiet,
         ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
       })}
       style=${ifDefined(styleMap(customStyles))}
       id=${ifDefined(id)}
       aria-haspopup="listbox"
+<<<<<<< HEAD
       @click=${onclick ?? function () {
         if (isDisabled) return;
         updateArgs({ isOpen: !isOpen });
@@ -65,6 +101,20 @@ export const Template = ({
           iconName: iconName ?? "ChevronDown200",
           size,
           customClasses: [ iconType === "ui" ? `${rootClass}-UIIcon` : `${rootClass}-menuIcon` ],
+=======
+      @click=${() => {
+        updateArgs({ isOpen: !isOpen });
+      }}
+    >
+      ${label ? html`<span class="${rootClass}-label is-placeholder">${label}</span>`: ''}
+      <!-- @todo when does this wrapper get added -->
+      <div class="${rootClass}-fill">
+        ${Icon({
+          ...globals,
+          iconName,
+          size,
+          customClasses: [ `${rootClass}-UIIcon`], //`${rootClass}-menuIcon`
+>>>>>>> 5f16a9479 (chore: add inputgroup story and related element updates)
         })}
       </div>
     </button>
