86b86120d83392ce098ba0ad8285c8ed24ddba70
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-03-01 13:05:59 -0700

    feat(colorslider): display disabled and focused states in story

--- components/colorslider/stories/template.js
+++ components/colorslider/stories/template.js
@@ -1,6 +1,7 @@
 import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
-// import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { Template as ColorHandle } from "@spectrum-css/colorhandle/stories/template.js";
+
 
 import '../index.css';
 import '../skin.css';
@@ -8,25 +9,31 @@ import '../skin.css';
 export const Template = ({
   rootClass = "spectrum-ColorSlider",
   customClasses = [],
-  // ...globals
+  isDisabled = false,
+  isFocused = false,
+  colorHandleStyle = "--spectrum-picked-color: rgb(255, 0, 0);",
+  ...globals
 }) => {
   return html`
     <div class=${classMap({
       [rootClass]: true,
+      'is-disabled': isDisabled,
+      'is-focused': isFocused,
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}>
-  <div class="spectrum-ColorSlider-checkerboard" role="presentation">
-    <div class="spectrum-ColorSlider-gradient" role="presentation" style="background: linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%);"></div>
+  })}
+  ?disabled=${isDisabled}
+  ?focused=${isFocused}>
+  <div class="${rootClass}-checkerboard" role="presentation">
+    <div class="${rootClass}-gradient" role="presentation" style="background: linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%);"></div>
   </div>
-
-  <div class="spectrum-ColorHandle spectrum-ColorSlider-handle" style="--spectrum-picked-color: rgb(255, 0, 0)">
-    <div class="spectrum-ColorHandle-color"></div>
-    <svg class="spectrum-ColorLoupe">
-      <!-- use ColorLoupe markup here -->
-    </svg>
-  </div>
-
-  <input type="range" class="spectrum-ColorSlider-slider" min="0" max="100" step="1">
+  ${ColorHandle({
+    ...globals,
+    isDisabled,
+    customClasses: [`${rootClass}-handle`],
+    colorHandleStyle
+    })
+  }
+  <input type="range" class="${rootClass}-slider" min="0" max="100" step="1">
   </div>
   `;
 }
