23d4837dce24aff3dbbd0463fc1e42f9daa14355
Megan Logsdon <megan@heysparkbox.com>
2023-03-02 14:51:18 -0500

    chore: initial basic template setup for tabs storybook

--- components/tabs/stories/template.js
+++ components/tabs/stories/template.js
@@ -1,6 +1,8 @@
 import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
-// import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { styleMap } from 'lit-html/directives/style-map.js';
+import { repeat } from 'lit-html/directives/repeat.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import "../index.css";
 import "../skin.css";
@@ -9,28 +11,39 @@ export const Template = ({
   rootClass = "spectrum-Tabs",
   customClasses = [],
   size = "m",
-  // ...globals
+  orientation = "horizontal",
+  density,
+  isQuiet,
+  isEmphasized,
+  items,
+  style = {
+    "--spectrum-tabs-textonly-tabitem-selection-indicator-background-color-selected": "#000",
+    "width": "35px"
+  },
+  ...globals
 }) => {
   return html`
     <div class=${classMap({
       [rootClass]: true,
       [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
-      [`${rootClass}--horizontal`]: true,
+      [`${rootClass}--${orientation}`]: typeof orientation !== "undefined",
+      [`${rootClass}--quiet`]: isQuiet,
+      [`${rootClass}--emphasized`]: isEmphasized,
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
     })}>
-      <div class="spectrum-Tabs-item is-selected" tabindex="0">
-        <span class="spectrum-Tabs-itemLabel">Tab 1</span>
-      </div>
-      <div class="spectrum-Tabs-item" tabindex="0">
-        <span class="spectrum-Tabs-itemLabel">Tab 2</span>
-      </div>
-      <div class="spectrum-Tabs-item" tabindex="0">
-        <span class="spectrum-Tabs-itemLabel">Tab 3</span>
-      </div>
-      <div class="spectrum-Tabs-item" tabindex="0">
-        <span class="spectrum-Tabs-itemLabel">Tab 4</span>
-      </div>
-      <div class="spectrum-Tabs-selectionIndicator" style="width: 24px; left: 0px;"></div>
+    ${repeat(items, (item) => item.id, (item) => {
+      if (typeof item === "object") {
+        return html`
+          <div class=${classMap({
+            [`${rootClass}-item`]: true,
+            "is-selected": item.isSelected
+          })} tabindex="0">
+          <span class="${rootClass}-itemLabel">${item.label}</span>
+          </div>
+        `
+      } else return item;
+    })}
+      <div class="${rootClass}-selectionIndicator" style=${ifDefined(styleMap(style))}></div>
     </div>
   `;
 }
