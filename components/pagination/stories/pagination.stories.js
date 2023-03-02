851c5d2a075448b5d5e0df643097f917e988bc1a
mlogsdon18 <99203545+mlogsdon18@users.noreply.github.com>
2023-03-02 17:11:50 -0500

    chore: add pagination storybook (#1651)

---
+++ components/pagination/stories/pagination.stories.js
@@ -0,0 +1,68 @@
+// Import the component markup template
+import { Template } from "./template";
+
+import { default as ActionButton } from '@spectrum-css/actionbutton/stories/actionbutton.stories';
+
+export default {
+  title: "Pagination",
+  description: "The Pagination component displays numbered buttons or an input field to allow for navigation",
+  component: "Pagination",
+  argTypes: {
+    size: {
+      name: "Size",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      options: ["s", "m", "l", "xl"],
+      control: "select"
+    },
+    variant: {
+      table: { disable: true }
+    }
+  },
+  args: {
+    rootClass: "spectrum-Pagination",
+    size: "m",
+    variant: "listing"
+  },
+  parameters: {
+    actions: {
+      handles: [
+        ...ActionButton.parameters.actions.handles,
+      ]
+    },
+    status: {
+      type: process.env.MIGRATED_PACKAGES.includes('pagination') ? 'migrated' : undefined
+    }
+  }
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  items: [
+    {
+      id: 1,
+      label: "1",
+      isSelected: true
+    },
+    {
+      id: 2,
+      label: "2",
+    },
+    {
+      id: 3,
+      label: "...",
+    },
+    {
+      id: 10,
+      label: "10",
+    },
+  ]
+};
+
+export const Explicit = Template.bind({});
+Explicit.args = {
+  variant: "explicit"
+};
