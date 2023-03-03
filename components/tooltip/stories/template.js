0363b3fd1fb4b7031ac115da05d19c70d066d434
castastrophe <castastrophe@users.noreply.github.com>
2023-03-03 15:08:41 -0500

    chore: add storybook for tooltip

--- components/tooltip/stories/template.js
+++ components/tooltip/stories/template.js
@@ -1,12 +1,19 @@
 import { html } from 'lit-html';
-// import { classMap } from 'lit-html/directives/class-map.js';
-// import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { when } from 'lit-html/directives/when.js';
+
+import { Template as Icon } from '@spectrum-css/icon/stories/template.js'
 
 import "../index.css";
 
 export const Template = ({
   rootClass = "spectrum-Tooltip",
-  size = "m",
+  label,
+  variant = "neutral",
+  placement,
+  isOpen = true,
+  isFocused = false,
+  showOnHover = false,
   ...globals
 }) => {
   const { express } = globals;
@@ -18,10 +25,30 @@ export const Template = ({
     console.warn(e);
   }
 
+  let variantIcon;
+  if (variant === "info") {
+    variantIcon = "Info";
+  } else if (variant === "positive") {
+    variantIcon = "CheckmarkCircle";
+  } else if (variant === "negative") {
+    variantIcon = "Alert";
+  }
+
   return html`
-    <span class="spectrum-Tooltip spectrum-Tooltip--top is-open">
-  <span class="spectrum-Tooltip-label">Label</span>
-  <span class="spectrum-Tooltip-tip"></span>
-</span>
+    <span class=${classMap({
+      [rootClass]: true,
+      [`${rootClass}--${variant}`]: typeof variant !== "undefined" && variant !== "neutral",
+      [`${rootClass}--${placement}`]: typeof placement !== "undefined",
+      'is-open': isOpen,
+      'is-focused': isFocused,
+    })}>
+      ${when(variantIcon, () => Icon({
+        iconName: variantIcon,
+        size: "m",
+        customClasses: [`${rootClass}-typeIcon`]
+      }))}
+      ${when(label, () => html`<span class="${rootClass}-label">${label}</span>`)}
+      <span class="${rootClass}-tip"></span>
+    </span>
   `;
 }
