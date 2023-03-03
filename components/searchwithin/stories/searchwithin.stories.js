11b3388ccbeef589a85cb568f88a49605dd29f9f
castastrophe <castastrophe@users.noreply.github.com>
2023-03-03 16:17:10 -0500

    chore: search within story added

--- components/searchwithin/stories/searchwithin.stories.js
+++ components/searchwithin/stories/searchwithin.stories.js
@@ -1,25 +1,28 @@
 // Import the component markup template
 import { Template } from "./template";
 
+import { default as PickerStories } from '@spectrum-css/picker/stories/picker.stories.js';
+const ignoreProps = ['rootClass', 'position', 'isRounded'];
+
 export default {
-  title: "Searchwithin",
-  description: "The Searchwithin component is...",
-  component: "Searchwithin",
+  title: "Search within",
+  description: "The Search within component is...",
+  component: "SearchWithin",
   argTypes: {
-    size: {
-      name: "Size",
-      type: { name: "string", required: true },
-      table: {
-        type: { summary: "string" },
-        category: "Component",
-      },
-      options: ["s", "m", "l", "xl"],
-      control: "select"
-    },
+    ...Object.entries(PickerStories.argTypes).reduce((acc, [key,value]) => {
+      if (ignoreProps.includes(key)) return acc;
+      if (['size'].includes(key)) value.table = { ...value.table, category: "Shared settings" };
+      else value.table = { ...value.table, category: "Picker settings" };
+      return { ...acc, [key]: value };
+    }, {}),
   },
   args: {
-    rootClass: "spectrum-",
-    size: "m",
+    rootClass: "spectrum-SearchWithin",
+    isOpen: false,
+    isQuiet: false,
+    isDisabled: false,
+    size: 'm',
+    label: 'All',
   },
   parameters: {
     actions: {
--- components/searchwithin/stories/template.js
+++ components/searchwithin/stories/template.js
@@ -1,20 +1,80 @@
 import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
-// import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { styleMap } from 'lit-html/directives/style-map.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+
+import { Template as PickerButton } from '@spectrum-css/pickerbutton/stories/template.js';
+import { Template as Popover } from '@spectrum-css/popover/stories/template.js';
+import { Template as Textfield } from '@spectrum-css/textfield/stories/template.js';
+import { Template as ClearButton } from '@spectrum-css/clearbutton/stories/template.js';
+
+import { Template as Menu } from '@spectrum-css/menu/stories/template.js';
 
 import "../index.css";
 
 export const Template = ({
   rootClass = "spectrum-SearchWithin",
   customClasses = [],
-  size = "m",
-  // ...globals
+  customStyles = {},
+  isOpen = false,
+  size = 'm',
+  ...globals
 }) => {
   return html`
-    <div class=${classMap({
-      [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
-      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    <form
+      class=${classMap({
+        [rootClass]: true,
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}
+      style=${ifDefined(styleMap(customStyles))}
+    >
+    ${PickerButton({
+      ...globals,
+      size,
+      label: 'All',
+      isOpen,
+      position: 'left',
+      customClasses: [`${rootClass}-picker`],
+    })}
+    ${Textfield({
+      ...globals,
+      size,
+      autocomplete: false,
+      name: "search",
+      placeholder: "Search",
+      type: "search",
+      customInputClasses: [`${rootClass}-input`]
+    })}
+    ${ClearButton({
+      ...globals,
+      size,
+      customClasses: [`${rootClass}-clearButton`]
+    })}
+    ${Popover({
+      ...globals,
+      isOpen: isOpen,
+      withTip: false,
+      position: 'bottom',
+      customStyles: {
+        position: 'absolute',
+        top: '38px',
+        left: '0',
+      },
+      content: [
+        Menu({
+          ...globals,
+          items: [
+            { label: "Deselect" },
+            { label: "Select Inverse" },
+            { label: "Feather..." },
+            { label: "Select and Mask..." },
+            { type: "divider" },
+            { label: "Save Selection" },
+            { label: "Make Work Path", isDisabled: true },
+          ],
+        })
+      ]
+    })}
+  </form>
   `;
 }
