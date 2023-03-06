eef6609227f6b0704250a335902aa7a71da9f2f5
Megan Logsdon <megan@heysparkbox.com>
2023-03-06 13:18:57 -0500

    chore: add template for stories for SideNav component

--- components/sidenav/stories/template.js
+++ components/sidenav/stories/template.js
@@ -1,21 +1,72 @@
 import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
+import { repeat } from 'lit-html/directives/repeat.js';
+
 // import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import "../index.css";
 import "../skin.css";
 
+
+export const SideNavItem = ({
+  rootClass = "spectrum-SideNav",
+  link,
+  title,
+  isSelected,
+  isDisabled,
+  id,
+  customClasses
+}) => {
+  return html`
+    <li id=${id} class=${classMap({
+      [`${rootClass}-item`]: true,
+      "is-selected": isSelected,
+      "is-disabled": isDisabled,
+    })}>
+      <a href=${link} class="${rootClass}-itemLink">${title}</a>
+    </li>
+  `
+}
+
 export const Template = ({
   rootClass = "spectrum-SideNav",
   customClasses = [],
-  size = "m",
-  // ...globals
+  variant,
+  items = [],
+  ...globals
 }) => {
   return html`
-    <div class=${classMap({
-      [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
-      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    <nav>
+      <ul class=${classMap({
+        [rootClass]: true,
+        [`${rootClass}--${variant}`]: typeof variant !== "undefined",
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}>
+        ${repeat(items, (item) => item.id, (item) => {
+          if (typeof item.subitems !== "undefined") {
+            return html`
+              <li class=${classMap({
+                [`${rootClass}-item`]: true,
+                "is-selected": item.isSelected,
+                "is-disabled": item.isDisabled,
+              })}>
+                <a href=${item.link} class="${rootClass}-itemLink">${item.title}</a>
+                <ul class=${rootClass}>
+                  ${repeat(item.subitems, (item) => item.id, (item) => {
+                    return SideNavItem({
+                      ...item
+                    })
+                  })}
+                </ul>
+              </li>
+            `
+          } else {
+            return SideNavItem({
+              ...item
+            })
+          }
+        })}
+      </ul>
+  </nav>
   `;
 }
