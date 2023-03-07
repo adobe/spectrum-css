5465fb698f8fbc7ce0388f553fb8483e765592fa
castastrophe <castastrophe@users.noreply.github.com>
2023-03-07 14:11:30 -0500

    chore(splitview): write storybook + configure template

--- components/splitview/stories/template.js
+++ components/splitview/stories/template.js
@@ -1,5 +1,6 @@
 import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
+import { when } from "lit-html/directives/when.js";
 // import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import "../index.css";
@@ -8,14 +9,38 @@ import "../skin.css";
 export const Template = ({
   rootClass = "spectrum-SplitView",
   customClasses = [],
-  size = "m",
+  orientation = "horizontal",
+  isResizable = false,
+  isCollapsible = false,
+  collapsePosition,
+  panelLabels = [],
+  panelStyles = [],
+  componentHeight = "200px",
   // ...globals
 }) => {
+
+  const collapsible = isCollapsible;
+  const collapsibleStart = typeof collapsePosition !== "undefined" && collapsePosition === "left" || collapsePosition === "top";
+  const collapsibleEnd = typeof collapsePosition !== "undefined" && collapsePosition === "right" || collapsePosition === "bottom";
+  const collapsibleClassName = collapsible && collapsibleStart ? 'start' : collapsible && collapsibleEnd ? 'end' : '';
+
   return html`
-    <div class=${classMap({
+    <div
+      style="height: ${componentHeight};"
+      class=${classMap({
       [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+      [`${rootClass}--${orientation}`]: orientation,
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    })}>
+      <div class="spectrum-SplitView-pane" style="${panelStyles[0]}">${panelLabels[0]}</div>
+      <div class=${classMap({
+        [`${rootClass}-splitter`]: true,
+        ['is-draggable']: isResizable,
+        [`is-collapsed-${collapsibleClassName}`]: isCollapsible,
+      })}>
+        ${when(isResizable, () => html`<div class="spectrum-SplitView-gripper"></div>`)}
+      </div>
+      <div class="spectrum-SplitView-pane" style="${panelStyles[1]}">${panelLabels[1]}</div>
+    </div>
   `;
 }
