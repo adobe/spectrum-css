bd1d6c3a1574c6d487aaaf6bd2d89d6c2e4c0a9f
Patrick Fulton <360251+pfulton@users.noreply.github.com>
2023-03-07 14:51:57 -0500

    chore(splitview): write storybook + configure template (#1662)

--- components/splitview/stories/splitview.stories.js
+++ components/splitview/stories/splitview.stories.js
@@ -6,20 +6,68 @@ export default {
   description: "The Splitview component is...",
   component: "Splitview",
   argTypes: {
-    size: {
-      name: "Size",
-      type: { name: "string", required: true },
+    orientation: {
+      name: "Orientation",
+      type: { name: "string" },
       table: {
         type: { summary: "string" },
         category: "Component",
+        disable: true
       },
-      options: ["s", "m", "l", "xl"],
-      control: "select"
     },
+    isResizable: {
+      name: "Resizable",
+      type: "boolean",
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+        disable: true
+      },
+      control: "boolean"
+    },
+    isCollapsible: {
+      name: "Collapsible",
+      type: "boolean",
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+        disable: true
+      },
+    },
+    collapsePosition: {
+      name: "Collapse position",
+      type: "string",
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+        disable: true
+      },
+      control: "select",
+      options: ["left", "right", "top", "bottom"],
+      if: {
+        arg: 'isCollapsible',
+        truthy: true,
+      }
+    },
+    panelLabels: {
+      name: "Panel labels",
+      type: "string",
+      table: { disable: true },
+    },
+    panelStyles: {
+      name: "Panel labels",
+      type: "string",
+      table: { disable: true },
+    },
+    componentHeight: {
+      name: "Component height",
+      table: { disable: true }
+    }
   },
   args: {
-    rootClass: "spectrum-",
-    size: "m",
+    rootClass: "spectrum-SplitView",
+    isResizable: false,
+    componentHeight: "200px",
   },
   parameters: {
     actions: {
@@ -31,5 +79,78 @@ export default {
   }
 };
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  orientation: "horizontal",
+  isResizable: false,
+  isCollapsible: false,
+  panelLabels: ["Left", "Right"],
+  panelStyles: ["width: 304px;", "flex: 1;"]
+};
+
+export const HorizontallyResizable = Template.bind({});
+HorizontallyResizable.args = {
+  orientation: "horizontal",
+  isResizable: true,
+  isCollapsible: false,
+  panelLabels: ["Left", "Right"],
+  panelStyles: ["width: 304px;", "flex: 1;"]
+};
+
+export const HorizontalCollapsedLeft = Template.bind({});
+HorizontalCollapsedLeft.args = {
+  orientation: "horizontal",
+  isResizable: true,
+  isCollapsible: true,
+  collapsePosition: "left",
+  panelLabels: ["Left", "Right"],
+  panelStyles: ["width: 0;", "flex: 1;"]
+};
+
+export const HorizontalCollapsedRight = Template.bind({});
+HorizontalCollapsedRight.args = {
+  orientation: "horizontal",
+  isResizable: true,
+  isCollapsible: true,
+  collapsePosition: "right",
+  panelLabels: ["Left", "Right"],
+  panelStyles: ["flex: 1;", "width: 0;"]
+};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  orientation: "vertical",
+  isResizable: false,
+  isCollapsible: false,
+  panelLabels: ["Top", "Bottom"],
+  panelStyles: ["height: 50px;", "flex: 1;"]
+};
+
+export const VerticallyResizable = Template.bind({});
+VerticallyResizable.args = {
+  orientation: "vertical",
+  isResizable: true,
+  isCollapsible: false,
+  panelLabels: ["Top", "Bottom"],
+  panelStyles: ["height: 50px;", "flex: 1;"]
+};
+
+export const VerticalCollapsedTop = Template.bind({});
+VerticalCollapsedTop.args = {
+  orientation: "vertical",
+  isResizable: true,
+  isCollapsible: true,
+  collapsePosition: "top",
+  panelLabels: ["Top", "Bottom"],
+  panelStyles: ["height: 0;", "flex: 1;"]
+};
+
+export const VerticalCollapsedBottom = Template.bind({});
+VerticalCollapsedBottom.args = {
+  orientation: "vertical",
+  isResizable: true,
+  isCollapsible: true,
+  collapsePosition: "bottom",
+  panelLabels: ["Top", "Bottom"],
+  panelStyles: ["flex: 1;", "height: 0;"]
+};
