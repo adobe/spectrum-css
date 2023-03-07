38bf69237fdccc389d705752cf64711a0d266e6d
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2023-03-07 10:57:32 -0700

    chore(colorslider): add storybook (#1650)
    * chore(colorarea): refactor to use styleMap

---
+++ components/colorslider/stories/colorslider.stories.js
@@ -0,0 +1,51 @@
+// Import the component markup template
+import { Template } from "./template";
+
+export default {
+  title: "Color slider",
+  description: "The Color slider component lets users visually change an individual channel of a color.",
+  component: "ColorSlider",
+  argTypes: {
+    vertical: { table: { disable: true } },
+    isDisabled: {
+      name: "Disabled",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean"
+    },
+    isFocused: {
+      name: "Focused",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean",
+      if: { arg: 'isDisabled', truthy: false }
+    },
+  },
+  args: {
+    rootClass: "spectrum-ColorSlider",
+    isDisabled: false,
+    isFocused: false,
+  },
+  parameters: {
+    actions: {
+      handles: []
+    },
+    status: {
+      type: process.env.MIGRATED_PACKAGES.includes('colorslider') ? 'migrated' : undefined
+    }
+  }
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  vertical: true
+}
---
+++ components/colorslider/stories/template.js
@@ -0,0 +1,41 @@
+import { html } from 'lit-html';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { Template as ColorHandle } from "@spectrum-css/colorhandle/stories/template.js";
+
+
+import '../index.css';
+import '../skin.css';
+
+export const Template = ({
+  rootClass = "spectrum-ColorSlider",
+  customClasses = [],
+  isDisabled = false,
+  isFocused = false,
+  vertical,
+  colorHandleStyle = {
+    '--spectrum-picked-color': 'rgba(255, 0, 0)',
+  },
+  ...globals
+}) => {
+  return html`
+    <div class=${classMap({
+      [rootClass]: true,
+      [`${rootClass}--vertical`]: vertical,
+      'is-disabled': isDisabled,
+      'is-focused': isFocused,
+      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+  })}>
+  <div class="${rootClass}-checkerboard" role="presentation">
+    <div class="${rootClass}-gradient" role="presentation" style="background: linear-gradient(to ${vertical ? 'bottom' :'right'}, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%);"></div>
+  </div>
+  ${ColorHandle({
+    ...globals,
+    isDisabled,
+    customClasses: [`${rootClass}-handle`],
+    colorHandleStyle
+    })
+  }
+  <input type="range" class="${rootClass}-slider" min="0" max="100" step="1">
+  </div>
+  `;
+}
