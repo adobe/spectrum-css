c68b6ca0664a0f0c2da260e53a039eddacc46fc8
Josh Winn <965114+jawinn@users.noreply.github.com>
2023-03-29 15:11:14 -0400

    feat(illustratedmessage): stories support for accent color
    Add stories support for using the illustration accent color.
    Moved SVG into a function, in order to conditionally include the accent
    classes without duplicating the SVG.

--- components/illustratedmessage/stories/illustratedmessage.stories.js
+++ components/illustratedmessage/stories/illustratedmessage.stories.js
@@ -6,9 +6,18 @@ import { Template as Link } from "@spectrum-css/link/stories/template.js";
 
 export default {
   title: "Illustrated Message",
-  description: "The illustrated message component is used for status and errors. It is also used inside the Drop Zone component.",
+  description: "The Illustrated Message component is used for status and errors. It is also used for call to actions, such as within the Drop Zone component.",
   component: "IllustratedMessage",
   argTypes: {
+    useAccentColor: {
+      name: "Illustration Accent Color",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
     heading: {
       name: "Heading",
       type: { name: "string" },
@@ -43,12 +52,14 @@ export const Default = Template.bind({});
 Default.args = {
   heading: "Error 404: Page not found",
   description: "This page isn't available. Try checking the URL or visit a different page.",
+  useAccentColor: false,
 };
 
-export const CTA = Template.bind({});
-CTA.description = "The illustrated message component is used for instruction/call-to-action.";
-CTA.args = {
+export const AccentColor = Template.bind({});
+AccentColor.description = "An accent color class can be used on elements of the illustration SVG.";
+AccentColor.args = {
   ...Default.args,
   heading: "Drag and drop your file",
   description: html`${Link({ url: '#', text: 'Select a file' })} from your computer.`,
+  useAccentColor: true,
 };
