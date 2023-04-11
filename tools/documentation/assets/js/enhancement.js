aea2b4b20de629b68dae526d79799e60eaba65c2
Rajdeep Chandra <rajdeepc@adobe.com>
2023-04-11 19:44:04 +0530

    chore(documentation): todo on enhancement.js

--- tools/documentation/assets/js/enhancement.js
+++ tools/documentation/assets/js/enhancement.js
@@ -9,6 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+//TODO will remove this file once these are all tested
 
 // Load switch functionality from components/switch/metadata/enhancement.js
 // Load asset card functionality from components/assetcard/metadata/enhancement.js
