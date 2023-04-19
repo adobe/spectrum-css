3395838578bea56924ef78d4b2481de16f686ae4
Venkatesh Josyula <venkateshjo@adobe.com>
2023-04-19 15:46:21 +0530

    feat(accordion): init change accordion migration

--- components/accordion/stories/template.js
+++ components/accordion/stories/template.js
@@ -5,8 +5,7 @@ import { ifDefined } from "lit-html/directives/if-defined.js";
 
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
-import "../index.css";
-import "../skin.css";
+import '../index.css';
 
 export const AccordionItem = ({
 	heading,
