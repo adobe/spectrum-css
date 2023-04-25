709dc2ec28cd7dff124c0bfdd58e877a2d7b66be
castastrophe <castastrophe@users.noreply.github.com>
2023-04-25 09:23:59 -0400

    chore(swatch): add functionality to storybook

--- components/swatch/stories/template.js
+++ components/swatch/stories/template.js
@@ -2,18 +2,23 @@ import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 import { styleMap } from "lit/directives/style-map.js";
-import { Template as OpacityCheckerboard } from "@spectrum-css/opacitycheckerboard/stories/template.js";
+
+import { useArgs } from '@storybook/client-api';
+
 import "../index.css";
 
 export const Template = ({
 	rootClass = "spectrum-Swatch",
 	size = "m",
-	customClasses = [],
+  isSelected = false,
+  isDisabled = false,
+  customClasses = [],
 	styles = {"--spectrum-picked-color": "rgb(174, 216, 230)"},
 	id,
 	...globals
 }) => {
 	const { express } = globals;
+  const [_, updateArgs] = useArgs();
 
 	try {
 		if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
@@ -22,18 +27,28 @@ export const Template = ({
 		console.warn(e);
 	}
 
-	return html`
-		<div
-			class=${classMap({
-				[rootClass]: true,
-				[`${rootClass}--size${size?.toUpperCase()}`]:
-					typeof size !== "undefined",
-				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-			})}
-			id=${ifDefined(id)}
+  return html`
+    <div
+      class=${classMap({
+        [rootClass]: true,
+        [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+        'is-selected': !isDisabled && isSelected,
+        'is-disabled': isDisabled,
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}
+      ?disabled=${isDisabled}
+      id=${ifDefined(id)}
 			style=${styleMap(styles)}
 			tabindex="0"
-		>
+      @click=${(e) => {
+        updateArgs({ isSelected: !isSelected });
+      }}
+      @focusout=${() => updateArgs({ isSelected: false })}
+      @keypress=${(e) => {
+        if (e.key !== 'Enter' && e.key !== ' ') return;
+        updateArgs({ isSelected: !isSelected });
+      }}
+    >
 		${OpacityCheckerboard({
 			...globals,
 			componentOnly: true,
