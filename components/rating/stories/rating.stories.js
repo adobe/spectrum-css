9b1e94e82e31d19cb930abf9a27e1bb246616bcb
castastrophe <castastrophe@users.noreply.github.com>
2023-04-25 09:20:02 -0400

    chore(rating): update storybook with functionality

--- components/rating/stories/rating.stories.js
+++ components/rating/stories/rating.stories.js
@@ -7,9 +7,59 @@ export default {
     "A rating element is used to display or collect a user's rating of an item as represented by a number of stars.",
   component: "Rating",
   argTypes: {
+    isFocused: {
+      name: "Focused",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+        disable: true,
+      },
+      control: "boolean"
+    },
+    isDisabled: {
+      name: "Disabled",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean",
+    },
+    isReadOnly: {
+      name: "Read only",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
+    max: {
+      name: "Maximum value",
+      type: { name: "number" },
+      table: {
+        type: { summary: "number" },
+        category: "Content",
+      },
+      control: { type: "number" },
+    },
+    value: {
+      name: "Value",
+      type: { name: "number" },
+      table: {
+        type: { summary: "number" },
+        category: "Content",
+        disable: true,
+      },
+      control: { type: "number" },
+    },
   },
   args: {
     rootClass: "spectrum-Rating",
+    isDisabled: false,
+    max: 5,
+    value: 3
   },
   parameters: {
     actions: {
--- components/rating/stories/template.js
+++ components/rating/stories/template.js
@@ -3,6 +3,8 @@ import { classMap } from "lit-html/directives/class-map.js";
 import { repeat } from "lit-html/directives/repeat.js";
 import { ifDefined } from "lit-html/directives/if-defined.js";
 
+import { useArgs } from "@storybook/client-api";
+
 // Uncomment if you plan to include an icon
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
@@ -13,30 +15,65 @@ export const Template = ({
   rootClass = "spectrum-Rating",
   size = "m",
   max = 5,
+  value = 0,
+  isReadOnly = false,
+  isFocused = false,
+  isDisabled = true,
   customClasses = [],
   id,
   ...globals
 }) => {
+  const [, updateArgs] = useArgs();
+
   return html`
     <div
       class=${classMap({
         [rootClass]: true,
-        [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+        'is-disabled': isDisabled,
+        'is-readOnly': isReadOnly,
         ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
       })}
-      id=${ifDefined(id)}>
+      id=${ifDefined(id)}
+      @focusin=${() => {
+        updateArgs({ isFocused: !isFocused });
+      }}
+      @focusout=${() => {
+        updateArgs({ isFocused: !isFocused });
+      }}
+    >
       <input
         class="${rootClass}-input"
         type="range"
         min="0"
         max=${max}
-        value="0"
+        value=${value}
         aria-label="Rating"
+        ?readonly=${isReadOnly}
+        ?disabled=${isDisabled}
+        @change=${(e) => {
+          const rating = e.target.closest(`.${rootClass}`);
+          if (!rating) return;
+
+          const input = rating.closest(`.${rootClass}-input`);
+          if (!input) return;
+          if (!isReadOnly && !isDisabled) {
+            updateArgs({ value: parseInt(input.value, 10) });
+          }
+        }}
       />
       ${repeat(
         Array(max).fill(0),
-        () => html`
-          <span class="${rootClass}-icon">
+        (_, idx) => html`
+          <span
+            class=${classMap({
+              [`${rootClass}-icon`]: true,
+              'is-selected': !isDisabled && idx <= value - 1,
+              'is-currentValue': !isDisabled && !isReadOnly && idx === value - 1,
+            })}
+            @click=${(e) => {
+              updateArgs({ value: idx +1 });
+            }}
+          >
             ${Icon({
               ...globals,
               iconName: "Star",
