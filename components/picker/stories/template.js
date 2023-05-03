1097b23509ee92d6cfa1e9229a754803cf78eab9
mlogsdon18 <99203545+mlogsdon18@users.noreply.github.com>
2023-05-03 16:46:00 -0400

    fix(picker)!: update spacing tokens and storybook (#1804)
    BREAKING CHANGE: removes `--mod-picker-min-inline-size`
    
    Additionally:
    * chore(picker,pickerbutton): update stories - update stories to include label
    * chore(picker,pickerbutton): add loading state to stories
    * chore(picker): update icon size in stories
    * chore(picker): fixing label padding for story
    * style(picker): update spacing tokens
    * feat(picker): update spacing tokens
    * chore(picker): update storybook to match docs
    * fix(pickerbutton): undo changes to pickerbutton
    * chore(picker): get defaults set for storybook
    * chore(picker): updating stories to include express specific styling - adding express and spectrum specific styling to storybook - fixing loading spacing
    * chore(picker): update storybook - add invalid story - add click functionality
    * chore(picker): add focused story and fix icon focus color
    * docs(picker): update mods file
    
    ---------
    
    Co-authored-by: Patrick Fulton <pfulton@adobe.com>

--- components/picker/stories/template.js
+++ components/picker/stories/template.js
@@ -1,7 +1,12 @@
+import { html } from "lit-html";
 import { useArgs } from "@storybook/client-api";
+import { classMap } from "lit-html/directives/class-map.js";
 
+import { Template as FieldLabel } from "@spectrum-css/fieldlabel/stories/template.js";
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 import { Template as Popover } from "@spectrum-css/popover/stories/template.js";
-import { Template as PickerButton } from "@spectrum-css/pickerbutton/stories/template.js";
+import { Template as ProgressCircle } from "@spectrum-css/progresscircle/stories/template.js";
+import { Template as HelpText } from "@spectrum-css/helptext/stories/template.js";
 
 import "../index.css";
 
@@ -9,48 +14,103 @@ export const Template = ({
   rootClass = "spectrum-Picker",
   size = "m",
   label,
-  position,
-  iconName = "menu",
+  labelPosition = "top",
+  placeholder,
+  helpText,
+  isQuiet = false,
   isFocused = false,
-  isRounded = false,
   isOpen = false,
-  isValid = false,
   isInvalid = false,
+  isLoading = false,
+  isDisabled = false,
+  isReadOnly = false,
   customClasses = [],
   customStyles = {},
-  isQuiet = false,
-  isDisabled = false,
   customPopoverStyles = {},
   content = [],
   id,
   ...globals
 }) => {
   const [_, updateArgs] = useArgs();
+  
+  const { express } = globals;
+  try {
+    if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
+    else import(/* webpackPrefetch: true */ "../themes/express.css");
+  } catch (e) {
+    console.warn(e);
+  }
 
-  return [
-    PickerButton({
+  let iconName = "ChevronDown200";
+  switch(size){
+    case "s":
+      iconName = "ChevronDown75";
+      break;
+    case "m":
+      iconName = "ChevronDown100";
+      break;
+    case 'xl':
+      iconName = "ChevronDown300";
+      break;
+    default:
+      iconName = "ChevronDown200";
+  }
+
+  return html`
+  ${label ? 
+    FieldLabel({
       ...globals,
-      rootClass,
-      label,
-      iconName,
-      position,
       size,
+      label,
       isDisabled,
-      isFocused,
-      isOpen,
-      isValid,
-      isInvalid,
-      isRounded,
-      isQuiet,
-      customClasses,
-      customStyles,
-      id,
-      onclick: () => {
-        if (isDisabled) return;
+      alignment: labelPosition
+    })
+  : ""}
+    <button
+      class=${classMap({
+        [rootClass]: true,
+        [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+        [`${rootClass}--quiet`]: isQuiet,
+        [`is-invalid`]: isInvalid,
+        [`is-open`]: isOpen,
+        [`is-loading`]: isLoading,
+        [`is-focused`]: isFocused,
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}
+      ?disabled=${isDisabled}
+      aria-haspopup="listbox"
+      @click=${(e) => {
         updateArgs({ isOpen: !isOpen });
-      },
-    }),
-    Popover({
+      }}
+    >
+      <span class="${rootClass}-label is-placeholder">${placeholder}</span>
+      ${isLoading ? 
+        ProgressCircle({
+          size: "s",
+          isIndeterminate: true
+        })
+      : ""}
+      ${isInvalid && !isLoading ? 
+        Icon({
+          ...globals,
+          iconName: "Alert",
+          customClasses: [`${rootClass}-validationIcon`]
+        })
+      : ""}
+      ${Icon({
+        ...globals,
+        iconName,
+        customClasses: [`${rootClass}-menuIcon`]
+      })}
+    </button>
+    ${helpText ? 
+      HelpText({
+        text: helpText,
+        variant: isInvalid ? "negative" : "neutral",
+        hideIcon: true
+      })
+    : ""}
+    ${Popover({
       ...globals,
       isOpen: isOpen && !isDisabled,
       withTip: false,
@@ -58,6 +118,9 @@ export const Template = ({
       isQuiet,
       customStyles: customPopoverStyles,
       content,
-    }),
-  ];
+    })}
+  `;
+
+
+    
 };
