d02f55860e61b7d61d46d343016ad3754a4e9f3a
Megan Logsdon <megan@heysparkbox.com>
2023-05-05 11:27:10 -0400

    fix(dropzone): update naming and pointer events
    - rename classes from Dropzone to DropZone
    - fix pointer events for link
    - update filled story to only be available on dragged

--- components/dropzone/stories/dropzone.stories.js
+++ components/dropzone/stories/dropzone.stories.js
@@ -2,7 +2,7 @@
 import { Template } from "./template";
 
 export default {
-  title: "Drop zone",
+  title: "Drop Zone",
   description:
     'A drop zone is an area on the screen into a which an object can be dragged and dropped to accomplish a task. For example, a drop zone might be used in an upload workflow to enable the user to simply drop a file from their operating system into the drop zone, which is a more efficient and intuitive action, rather than utilize the standard "Choose File" dialog.',
   component: "DropZone",
@@ -24,10 +24,11 @@ export default {
         category: "State",
       },
       control: "boolean",
+      if: { arg: 'isDragged', truthy: true },
     },
   },
   args: {
-    rootClass: "spectrum-Dropzone",
+    rootClass: "spectrum-DropZone",
     isDragged: false,
     isFilled: false
   },
