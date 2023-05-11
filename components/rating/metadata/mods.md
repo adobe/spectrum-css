957c9b7db40e0df0d037c85564cdd2c58d3ea7a8
venkateshjo <125646446+venkateshjo@users.noreply.github.com>
2023-05-12 02:21:37 +0530

    feat(rating)!: token migration (#1642)
    BREAKING CHANGE: migrates the Rating component to use `@adobe/spectrum-tokens`.
    
    ---------
    
    Co-authored-by: Patrick Fulton <360251+pfulton@users.noreply.github.com>
    Co-authored-by: Patrick Fulton <pfulton@adobe.com>

---
+++ components/rating/metadata/mods.md
@@ -0,0 +1,22 @@
+| Modifiable Custom Properties |
+| --- |
+|`--mod-rating-focus-indicator-thickness`|
+|`--mod-rating-focus-indicator-color`|
+|`--mod-rating-icon-color-key-focus`|
+|`--mod-rating-icon-color-default`|
+|`--mod-rating-icon-color-hover`|
+|`--mod-rating-icon-color-down`|
+|`--mod-rating-icon-spacing`|
+|`--mod-rating-icon-width`|
+|`--mod-rating-icon-count`|
+|`--mod-rating-border-radius`|
+|`--mod-rating-focus-indicator-gap`|
+|`--mod-rating-icon-height`|
+|`--mod-rating-icon-spacing-vertical-top`|
+|`--mod-rating-indicator-border-radius`|
+|`--mod-rating-indicator-height`|
+|`--mod-rating-emphasized-icon-color-key-focus`|
+|`--mod-rating-emphasized-icon-color-default`|
+|`--mod-rating-emphasized-icon-color-hover`|
+|`--mod-rating-emphasized-icon-color-down`|
+|`--mod-rating-icon-color-disabled`|
--- components/rating/stories/template.js
+++ components/rating/stories/template.js
@@ -3,50 +3,93 @@ import { classMap } from "lit-html/directives/class-map.js";
 import { repeat } from "lit-html/directives/repeat.js";
 import { ifDefined } from "lit-html/directives/if-defined.js";
 
+import { useArgs } from "@storybook/client-api";
+
 // Uncomment if you plan to include an icon
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
 import "../index.css";
-import "../skin.css";
 
 export const Template = ({
   rootClass = "spectrum-Rating",
-  size = "m",
   max = 5,
+  value = 0,
+  isReadOnly = false,
+  isFocused = false,
+  isDisabled = true,
+  isEmphasized = false,
   customClasses = [],
   id,
   ...globals
 }) => {
+  const { express } = globals;
+  try {
+    if (express) import(/* webpackPrefetch: true */ "../themes/express.css");
+    else import(/* webpackPrefetch: true */ "../themes/spectrum.css");
+  } catch (e) {
+    console.warn(e);
+  }
+  
+  const [, updateArgs] = useArgs();
+  
   return html`
     <div
       class=${classMap({
         [rootClass]: true,
-        [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+        'is-disabled': isDisabled,
+        'is-readOnly': isReadOnly,
+        [`${rootClass}--emphasized`]: isEmphasized,
         ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
       })}
-      id=${ifDefined(id)}>
+      id=${ifDefined(id)}
+      @focusin=${() => {
+        updateArgs({ isFocused: true });
+      }}
+      @focusout=${() => {
+        updateArgs({ isFocused: false });
+      }}
+    >
       <input
         class="${rootClass}-input"
         type="range"
         min="0"
         max=${max}
-        value="0"
+        value=${value}
         aria-label="Rating"
+        ?readonly=${isReadOnly}
+        ?disabled=${isDisabled}
+        @change=${(e) => {
+          const rating = e.target.closest(`.${rootClass}`);
+          if (!rating) return;
+          
+          const input = rating.closest(`.${rootClass}-input`);
+          if (!input) return;
+          if (!isReadOnly && !isDisabled) {
+            updateArgs({ value: parseInt(input.value, 10) });
+          }
+        }}
       />
       ${repeat(
         Array(max).fill(0),
-        () => html`
-          <span class="${rootClass}-icon">
+        (_, idx) => html`
+          <span
+            class=${classMap({
+              [`${rootClass}-icon`]: true,
+              'is-selected': !isDisabled && idx <= value - 1,
+              'is-currentValue': !isDisabled && !isReadOnly && idx === value - 1,
+            })}
+            @click=${(e) => {
+              updateArgs({ value: idx + 1, isFocused: true });
+            }}
+          >
             ${Icon({
               ...globals,
               iconName: "Star",
-              size,
               customClasses: [`${rootClass}-starActive`],
             })}
             ${Icon({
               ...globals,
               iconName: "StarOutline",
-              size,
               customClasses: [`${rootClass}-starInactive`],
             })}
           </span>
