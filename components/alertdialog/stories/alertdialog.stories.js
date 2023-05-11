25a9fdbf9062dc0699b8d9d069a1ab09a1df9089
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-05-11 11:56:05 -0600

    feat(alertdialog): storybook variants

--- components/alertdialog/stories/alertdialog.stories.js
+++ components/alertdialog/stories/alertdialog.stories.js
@@ -1,39 +1,169 @@
+import { html } from "lit-html";
+
 // Import the component markup template
 import { Template } from "./template";
 
-// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
 export default {
-  title: "alertdialog",
-  description: "The alertdialog component is...",
-  component: "Alertdialog",
+  title: "Alert Dialog",
+  description:
+    "Alert dialogs display important information that users need to acknowledge. They appear over the interface and block further interactions until an action is selected.",
+  component: "AlertDialog",
   argTypes: {
-    size: {
-      name: "Size",
-      type: { name: "string", required: true },
-      defaultValue: "m",
+    heading: {
+      name: "Heading",
+      type: { name: "string" },
       table: {
         type: { summary: "string" },
         category: "Component",
-        defaultValue: { summary: "m" }
       },
-      options: ["s", "m", "l", "xl"],
-      control: "select"
+      control: { type: "text" },
+    },
+    content: { table: { disable: true } },
+    showModal: {
+      name: "Wrap the alert dialog in a modal",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
+    secondaryButton: {
+      name: 'Display 3rd button',
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
+    isOpen: {
+      name: "Open",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean",
     },
   },
-  // More on args: https://storybook.js.org/docs/web-components/writing-stories/args
   args: {
-    rootClass: "spectrum-Alertdialog",
-    size: "m",
+    rootClass: "spectrum-AlertDialog",
+    showModal: false,
+    isOpen: true,
+    secondaryButton: false,
   },
   parameters: {
     actions: {
-      handles: []
+      handles: [],
     },
     status: {
-      type: process.env.MIGRATED_PACKAGES.includes('alertdialog') ? 'migrated' : undefined
-    }
-  }
+      type: process.env.MIGRATED_PACKAGES.includes("alertdialog")
+        ? "migrated"
+        : undefined,
+    },
+  },
 };
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  variant: 'confirmation',
+  heading: "Enable Smart Filters?",
+  showModal: true,
+  buttons: [{
+    variant: "secondary",
+    treatment: "outline",
+    label: "Remind me later"
+  }, {
+    variant: "primary",
+    treatment: "fill",
+    label: "Enable",
+    variant: "accent"
+  }],
+  content: [
+   'Smart filters are nondestructive and will preserve your original images.',
+  ],
+};
+
+export const Information = Template.bind({});
+Information.args = {
+  variant: 'information',
+  heading: "Connect to wifi",
+  showModal: true,
+  buttons: [{
+    variant: "secondary",
+    treatment: "outline",
+    label: "Cancel"
+  }, {
+    variant: "primary",
+    treatment: "outline",
+    label: "Continue",
+    variant: "primary"
+  }],
+  content: [
+    'Please connect to wifi to sync your projects or go to Settings to change your prefernces.',
+  ],
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  variant: 'warning',
+  heading: "Unverified format",
+  showModal: true,
+  icon: true,
+  buttons: [{
+    variant: "secondary",
+    treatment: "outline",
+    label: "Cancel"
+  }, {
+    variant: "primary",
+    treatment: "outline",
+    label: "Continue",
+    variant: "primary"
+  }],
+  content: [
+    'This format has not been verified and may not be viewable for some users. Do you want to continue publishing?',
+  ],
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  variant: 'error',
+  heading: "Unable to share",
+  showModal: true,
+  icon: true,
+  buttons: [{
+    variant: "secondary",
+    treatment: "outline",
+    label: "Cancel"
+  }, {
+    variant: "primary",
+    treatment: "outline",
+    label: "Continue",
+    variant: "primary"
+  }],
+  content: [
+    'An error occured while sharing your project. Please verify the email address and try again.',
+  ],
+};
+
+export const Destructive = Template.bind({});
+Destructive.args = {
+  variant: 'destructive',
+  heading: "Delete 3 documents?",
+  showModal: true,
+  buttons: [{
+    variant: "secondary",
+    treatment: "outline",
+    label: "Cancel"
+  }, {
+    variant: "primary",
+    treatment: "fill",
+    label: "Delete",
+    variant: "negative"
+  }],
+  content: [
+    'Are you sure you want to delete the 3 selected documents?',
+  ],
+};
+
--- components/alertdialog/stories/template.js
+++ components/alertdialog/stories/template.js
@@ -1,24 +1,80 @@
-import { html } from 'lit-html';
-import { classMap } from 'lit-html/directives/class-map.js';
-import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { html } from "lit-html";
+import { classMap } from "lit-html/directives/class-map.js";
+import { ifDefined } from "lit-html/directives/if-defined.js";
+import { when } from 'lit-html/directives/when.js';
 
-import "../index.css";
+import { Template as Modal } from '@spectrum-css/modal/stories/template.js';
+import { Template as Divider } from "@spectrum-css/divider/stories/template.js";
+import { Template as ButtonGroup } from "@spectrum-css/buttongroup/stories/template.js";
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
-// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+import '../index.css';
+
 export const Template = ({
-  rootClass = "spectrum-Alertdialog",
-  size,
-  id,
+  rootClass = "spectrum-AlertDialog",
+  isOpen = true,
+  showModal = false,
+  heading = true,
+  content = [],
   customClasses = [],
+  buttons,
+  variant,
+  icon = false,
+  id,
   ...globals
 }) => {
-  return html`
-    <div class=${classMap({
-      [rootClass]: true,
-      [`${rootClass}--size${size.toUpperCase()}`]: true,
-      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-    })} id=${ifDefined(id)}>
-      <!-- Component mark-up goes here -->
+
+  const iconColor =
+  variant === "error"
+    ? "red"
+    : variant === "warning"
+    ? "orange"
+    : "";
+
+  const Dialog = html`
+    <div
+      class=${classMap({
+        [rootClass]: true,
+        [`${rootClass}-${variant}`]: true,
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}
+      id=${ifDefined(id)}
+      role="dialog"
+      tabindex="-1"
+      aria-modal="true"
+    >
+      <div class="${rootClass}-grid">
+      <div class="spectrum-AlertDialog-header">
+        <h1 class="${rootClass}-heading">${heading}</h1>
+        ${when(icon, () => Icon({
+          size: 'm',
+          iconName: "Alert",
+          customClasses: [`${rootClass}-icon`],
+          fill: iconColor,
+          ...globals,
+        })) }
+      </div>
+      ${Divider({
+            horizontal: true,
+            customClasses: [`${rootClass}-divider`],
+            ...globals,
+          })}
+      <section class="${rootClass}-content">${content}</section>
+      ${ButtonGroup({
+          items: buttons
+        })
+      }
+      </div>
     </div>
   `;
-}
+
+  if (showModal) {
+    return Modal({
+          ...globals,
+          isOpen,
+          content: Dialog,
+        });
+  } else {
+    return Dialog;
+  }
+};
