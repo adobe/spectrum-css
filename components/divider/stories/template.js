b3f4a5c573cdf35d1bf2819c56f46ae09de3b57d
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-05-16 13:29:31 -0600

    chore(alertbanner): remove unneeded import

--- components/divider/stories/template.js
+++ components/divider/stories/template.js
@@ -1,6 +1,6 @@
 import { html } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
-import { styleMap } from "lit-html/directives/style-map.js"
+
 import { upperCase, lowerCase, capitalize } from "lodash-es";
 
 import "../index.css";
