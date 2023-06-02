8c599afad640d16e3ba42726d911b806f3ca0731
Josh Winn <965114+jawinn@users.noreply.github.com>
2023-06-02 14:21:48 -0400

    fix(search): prettier and stylelint after merge
    Run prettier and stylelint on all search component files after a
    merge with --no-verify, to get everything up to date and passing the
    linter checks.

--- components/search/stories/template.js
+++ components/search/stories/template.js
@@ -4,53 +4,56 @@ import { classMap } from "lit-html/directives/class-map.js";
 import { Template as ClearButton } from "@spectrum-css/clearbutton/stories/template.js";
 import { Template as TextField } from "@spectrum-css/textfield/stories/template.js";
 
-import '../index.css';
+import "../index.css";
 
 export const Template = ({
-  rootClass = "spectrum-Search",
-  customClasses = [],
-  isDisabled = false,
-  isQuiet = false,
-  size,
-  ...globals
+	rootClass = "spectrum-Search",
+	customClasses = [],
+	isDisabled = false,
+	isQuiet = false,
+	size,
+	...globals
 }) => {
-  const { express } = globals;
+	const { express } = globals;
 
-  try {
-    if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
-    else import(/* webpackPrefetch: true */ "../themes/express.css");
-  } catch (e) {
-    console.warn(e);
-  }
+	try {
+		if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
+		else import(/* webpackPrefetch: true */ "../themes/express.css");
+	} catch (e) {
+		console.warn(e);
+	}
 
-  return html`
-    <form class=${classMap({
-      [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
-      [`${rootClass}--quiet`]: isQuiet,
-      'is-disabled': isDisabled,
-      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-    })}>
-      ${TextField({
-        ...globals,
-        isDisabled,
-        isQuiet,
-        size,
-        customClasses: [`${rootClass}-textfield`],
-        iconName: "Magnify",
-        type: "search",
-        placeholder: "Search",
-        name: "search",
-        customInputClasses: [`${rootClass}-input`],
-        customIconClasses: [`${rootClass}-icon`],
-        autocomplete: false,
-      })}
-      ${ClearButton({
-        ...globals,
-        isDisabled,
-        size,
-        customClasses: [`${rootClass}-clearButton`],
-      })}
-    </form>
-  `;
-}
+	return html`
+		<form
+			class=${classMap({
+				[rootClass]: true,
+				[`${rootClass}--size${size?.toUpperCase()}`]:
+					typeof size !== "undefined",
+				[`${rootClass}--quiet`]: isQuiet,
+				"is-disabled": isDisabled,
+				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+			})}
+		>
+			${TextField({
+				...globals,
+				isDisabled,
+				isQuiet,
+				size,
+				customClasses: [`${rootClass}-textfield`],
+				iconName: "Magnify",
+				type: "search",
+				placeholder: "Search",
+				name: "search",
+				customInputClasses: [`${rootClass}-input`],
+				customIconClasses: [`${rootClass}-icon`],
+				autocomplete: false,
+			})}
+			${ClearButton({
+				...globals,
+				isDisabled,
+				size,
+				customClasses: [`${rootClass}-clearButton`],
+			})}
+		</form>
+	`;
+};
