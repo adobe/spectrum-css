dbb63069387118d06f5424ecb27463cd8b07dafa
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-06-05 10:54:58 -0600

    feat(opacitycheckerboard): adds stories

--- components/opacitycheckerboard/stories/opacitycheckerboard.stories.js
+++ components/opacitycheckerboard/stories/opacitycheckerboard.stories.js
@@ -1,24 +1,42 @@
+import { Template } from "./template";
+
 export default {
 	title: "Opacity checkerboard",
 	description: "The Opacity checkerboard component is...",
 	component: "OpacityCheckerboard",
 	argTypes: {
-		size: {
-			name: "Size",
-			type: { name: "string", required: true },
-			defaultValue: "m",
+		hasColorOverlay: {
+			name: "Has Color Overlay",
+			type: { name: "boolean" },
+			defaultValue: false,
 			table: {
-				type: { summary: "string" },
 				category: "Component",
-				defaultValue: { summary: "m" },
 			},
-			options: ["s", "m", "l", "xl"],
-			control: "select",
+			control: "boolean",
+		},
+		overlayColor: {
+			name: "Overlay Color",
+			type: { name: "string" },
+			defaultValue: "rgba(255, 0, 0, 0.5)",
+			table: {
+				category: "Component",
+			},
+			control: "text",
+			if: { arg: "hasColorOverlay", truthy: true },
+		},
+		backgroundPosition: {
+			name: "Position",
+			type: { name: "string" },
+			defaultValue: "top left",
+			table: {
+				category: "Component",
+			},
+			control: "text",
+			description: "accepts any valid background-position property",
 		},
 	},
 	args: {
 		rootClass: "spectrum-OpacityCheckerboard",
-		size: "m",
 	},
 	parameters: {
 		actions: {
--- components/opacitycheckerboard/stories/template.js
+++ components/opacitycheckerboard/stories/template.js
@@ -1,23 +1,36 @@
 import { html } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
-import { ifDefined } from "lit-html/directives/if-defined.js";
+import { styleMap } from "lit-html/directives/style-map.js";
+import { when } from "lit-html/directives/when.js";
+
 import "../index.css";
 
 export const Template = ({
 	rootClass = "spectrum-OpacityCheckerboard",
-	size,
-	id,
+	hasColorOverlay,
+	overlayColor,
+	backgroundPosition,
 	customClasses = [],
+	style = {
+		"--mod-opacity-checkerboard-position": backgroundPosition,
+	},
 	...globals
 }) => {
 	return html`
+		<div class="${rootClass}-example">
 			<div
-				class="${classMap({
+				class=${classMap({
 					[rootClass]: true,
-					[`${rootClass}--size${size.toUpperCase()}`]: true,
-					...customClasses.reduce((a, c)({ ...a, [c]: true }), {}),
-				})} id=${ifDefined(id)}>
-				<!-- Component mark-up goes here -->
-			</div>
-			`;
+					...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+				})}
+				style=${styleMap(style)}
+			></div>
+			${when(hasColorOverlay, () => {
+				return html` <div
+					class="${rootClass}-example-color"
+					style="background-color:${overlayColor}"
+				></div>`;
+			})}
+		</div>
+	`;
 };
