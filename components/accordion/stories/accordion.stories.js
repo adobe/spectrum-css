5c04f0b4d4f4ff4f6789f15e9efc44e3ad5564c6
Venkatesh Josyula <venkateshjo@adobe.com>
2023-06-05 10:33:45 +0530

    chore(accordion): fixing the focus ring size

--- components/accordion/stories/accordion.stories.js
+++ components/accordion/stories/accordion.stories.js
@@ -2,59 +2,57 @@
 import { Template } from "./template";
 
 export default {
-  title: "Accordion",
-  description:
-    "The accordion element contains a list of items that can be expanded or collapsed to reveal additional content or information associated with each item. There can be zero expanded items, exactly one expanded item, or more than one item expanded at a time, depending on the configuration. This list of items is defined by child accordion item elements.",
-  component: "Accordion",
-  argTypes: {
-    items: { table: { disable: true } },
-    size: {
-      name: "Size",
-      type: { name: "string", required: true },
-      table: {
-        type: { summary: "string" },
-        category: "Component",
-      },
-      options: ["s", "m", "l", "xl"],
-      control: "select"
-    },
-    disableAll: {
-      name: "Disabled",
-      type: { name: "boolean" },
-      table: {
-        type: { summary: "boolean" },
-        category: "State",
-      },
-      control: "boolean",
-    },
-    density: {
-      name: "Density",
-      type: { name: "string", required: true },
-      table: {
-        type: { summary: "string" },
-        category: "Component",
-      },
-      options: ["compact", "regular", "spacious"],
-      control: "select"
-    },
-  },
-  args: {
-    rootClass: "spectrum-Accordion",
-    size: "m",
-    density: "regular",
-  },
-  parameters: {
-    actions: {
-      handles: [
-        "click .spectrum-Accordion-item",
-      ],
-    },
-    status: {
-      type: process.env.MIGRATED_PACKAGES.includes("accordion")
-        ? "migrated"
-        : undefined,
-    },
-  },
+	title: "Accordion",
+	description:
+		"The accordion element contains a list of items that can be expanded or collapsed to reveal additional content or information associated with each item. There can be zero expanded items, exactly one expanded item, or more than one item expanded at a time, depending on the configuration. This list of items is defined by child accordion item elements.",
+	component: "Accordion",
+	argTypes: {
+		items: { table: { disable: true } },
+		size: {
+			name: "Size",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["s", "m", "l", "xl"],
+			control: "select",
+		},
+		disableAll: {
+			name: "Disabled",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
+		density: {
+			name: "Density",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["compact", "regular", "spacious"],
+			control: "select",
+		},
+	},
+	args: {
+		rootClass: "spectrum-Accordion",
+		size: "m",
+		density: "regular",
+	},
+	parameters: {
+		actions: {
+			handles: ["click .spectrum-Accordion-item"],
+		},
+		status: {
+			type: process.env.MIGRATED_PACKAGES.includes("accordion")
+				? "migrated"
+				: undefined,
+		},
+	},
 };
 
 export const Default = Template.bind({});
