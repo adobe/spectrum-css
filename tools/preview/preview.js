eb79b6d9373061608a14ad96c63817ef4092784e
castastrophe <castastrophe@users.noreply.github.com>
2023-06-28 11:04:44 -0400

    chore: unlock vrt runs to all PRs

--- tools/preview/preview.js
+++ tools/preview/preview.js
@@ -1,3 +1,5 @@
+import isChromatic from "chromatic/isChromatic";
+
 import {
 	withContextWrapper,
 	withTextDirectionWrapper,
@@ -171,13 +173,23 @@ export const args = {
 	customClasses: [],
 };
 
+/** @type import('@storybook/types').StorybookParameters & import('@storybook/types').API_Layout */
 export const parameters = {
-	layout: "padded", // Valid: 'centered' | 'fullscreen' | 'padded' | 'none';
+	layout: "padded",
 	showNav: true,
+	showTabs: true,
 	showPanel: true,
 	panelPosition: "bottom",
-	showToolbar: false,
+	showToolbar: true,
 	isFullscreen: false,
+	//👇 Defines a list of viewport widths for a single story to be captured in Chromatic.
+	chromatic: isChromatic()
+		? {
+				// viewports: [320, 1200],
+				// forcedColors: 'active',
+				// prefersReducedMotion: 'reduce',
+		  }
+		: {},
 	controls: {
 		expanded: true,
 		hideNoControlsWarning: true,
@@ -223,6 +235,7 @@ export const decorators = [
 	withReducedMotionWrapper,
 	withContextWrapper,
 	withActions,
+	// ...[isChromatic() ? withSizingWrapper : false].filter(Boolean),
 ];
 
 export default {
