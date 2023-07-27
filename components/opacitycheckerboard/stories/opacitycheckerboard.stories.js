5a49afc3262b4e9268313a79f6b6e88d41a7a06e
Josh Winn <965114+jawinn@users.noreply.github.com>
2023-07-27 09:09:37 -0400

    feat(opacitycheckerboard): modify stories for use in components
    - Modify storybook template so that it can be imported and used by other
      components' stories. Allow passing in content, and excluding the
      storybook testing wrapper markup.
    - Moves defaultValue to args, as that syntax has been deprecated in
      newer versions of Storybook:
      https://storybook.js.org/docs/react/api/arg-types#defaultvalue
    - Adds a new story with a centered background position mod, and
      clarifies that the arg is changing the mod.

--- components/opacitycheckerboard/stories/opacitycheckerboard.stories.js
+++ components/opacitycheckerboard/stories/opacitycheckerboard.stories.js
@@ -9,7 +9,6 @@ export default {
 		hasColorOverlay: {
 			name: "Has Color Overlay",
 			type: { name: "boolean" },
-			defaultValue: false,
 			table: {
 				category: "Component",
 			},
@@ -18,7 +17,6 @@ export default {
 		overlayColor: {
 			name: "Overlay Color",
 			type: { name: "string" },
-			defaultValue: "rgba(255, 0, 0, 0.5)",
 			table: {
 				category: "Component",
 			},
@@ -28,16 +26,27 @@ export default {
 		backgroundPosition: {
 			name: "Position",
 			type: { name: "string" },
-			defaultValue: "top left",
 			table: {
 				category: "Component",
 			},
 			control: "text",
-			description: "Accepts any valid CSS background-position property value",
+			description: "Value for <code>--mod-opacity-checkerboard-position</code>. Accepts any valid CSS background-position property value.",
+		},
+		componentOnly: {
+			name: "Use Component Markup Only",
+			type: { name: "boolean" },
+			table: {
+				disable: true,
+			},
 		},
 	},
 	args: {
 		rootClass: "spectrum-OpacityCheckerboard",
+		hasColorOverlay: false,
+		overlayColor: "rgba(255, 0, 0, 0.5)",
+		backgroundPosition: "top left",
+		componentOnly: false,
+		content: '',
 	},
 	parameters: {
 		actions: {
@@ -52,3 +61,16 @@ export default {
 };
 export const Default = Template.bind({});
 Default.args = {};
+
+export const CheckerboardPosition = Template.bind({});
+CheckerboardPosition.args = {
+	backgroundPosition: 'center center',
+};
+CheckerboardPosition.parameters = {
+	docs: {
+		description: {
+			story:
+				"An example of using the <code>--mod-opacity-checkerboard-position</code> custom property to adjust the position of the checkerboard pattern.",
+		},
+	},
+};
