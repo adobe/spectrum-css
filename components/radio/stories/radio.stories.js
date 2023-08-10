7914b723a8c46e882e1ccd1e42475fa4db81e8a2
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2023-08-10 08:20:39 -0600

    fix(radio): update selected color tokens (#2045)
    * chore(radio): update selected color tokens
    
    * feat(radio): add variant to storybook
    
    * feat(radio): add onClick to storybook
    
    * fix(radio): add size select to controls

--- components/radio/stories/radio.stories.js
+++ components/radio/stories/radio.stories.js
@@ -7,6 +7,16 @@ export default {
 		"A radio selector allow users to select a single option from a list of mutually exclusive options. All possible options are exposed up front for users to compare.",
 	component: "Radio",
 	argTypes: {
+		size: {
+			name: "Size",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["s", "m", "l", "xl"],
+			control: "select",
+		},
 		label: {
 			name: "Label",
 			type: { name: "string" },
@@ -25,14 +35,56 @@ export default {
 			},
 			control: { type: "text" },
 		},
+		isEmphasized: {
+			name: "Emphasized styling",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: { type: "boolean" },
+		},
+		isChecked: {
+			name: "Radio selected",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: { type: "boolean" },
+		},
+		isDisabled: {
+			name: "Disabled",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
+		isReadOnly: {
+			name: "Read Only",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
 	},
 	args: {
 		rootClass: "spectrum-Radio",
-		size: "s",
+		size: "m",
+		label: "Label",
+		isEmphasized: false,
+		isChecked: false,
+		isDisabled: false,
+		isReadOnly: false,
 	},
 	parameters: {
 		actions: {
-			handles: [],
+			handles: ['click input[type="radio"]'],
+
 		},
 		status: {
 			type: process.env.MIGRATED_PACKAGES.includes("radio")
@@ -44,3 +96,22 @@ export default {
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Emphasized = Template.bind({});
+Emphasized.args = {
+	isEmphasized: true,
+	isChecked: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	isDisabled: true,
+};
+
+export const ReadOnly = Template.bind({});
+ReadOnly.args = {
+	isDisabled: true,
+	isReadOnly: true,
+};
+
+
