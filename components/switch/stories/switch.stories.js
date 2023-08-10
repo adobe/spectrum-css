255a47afc8684f392e18b849c2a80e62ebecde14
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2023-08-10 14:45:19 -0600

    fix(switch): update selected background color tokens (#2057)
    * fix(switch): expand storybook
    
    * fix(switch): use disabled content token
    
    * chore(switch): update color tokens
    
    * chore(switch): addresses PR feedback

--- components/switch/stories/switch.stories.js
+++ components/switch/stories/switch.stories.js
@@ -6,9 +6,61 @@ export default {
 	description:
 		"A switch is used to turn an option on or off. Switches allow users to select the state of a single option at a time.",
 	component: "Switch",
-	argTypes: {},
+	argTypes: {
+		size: {
+			name: "Size",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["s", "m", "l", "xl"],
+			control: "select",
+		},
+		isEmphasized: {
+			name: "Emphasized",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
+		isDisabled: {
+			name: "Disabled",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
+		isChecked: {
+			name: "Checked",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
+		label: {
+			name: "Label",
+			type: { name: "string" },
+			table: {
+				type: { summary: "string" },
+				category: "Content",
+			},
+			control: { type: "text" },
+		},
+	},
 	args: {
 		rootClass: "spectrum-Switch",
+		isDisabled: false,
+		isChecked: false,
+		isEmphasized: false,
+		label: "Switch label",
+		size: "m",
 	},
 	parameters: {
 		actions: {
@@ -24,3 +76,18 @@ export default {
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Emphasized = Template.bind({});
+Emphasized.args = {
+	isEmphasized: true
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+	isChecked: true
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	isDisabled: true
+};
