1b60120029c1d8541f8b3c5c72d1800462095b01
Megan Logsdon <megan@heysparkbox.com>
2023-08-22 15:47:03 -0400

    refactor(logicbutton): remove is-focused class and references in storybook

--- components/logicbutton/stories/logicbutton.stories.js
+++ components/logicbutton/stories/logicbutton.stories.js
@@ -26,21 +26,11 @@ export default {
 			},
 			control: "boolean",
 		},
-		isFocused: {
-			name: "Focused",
-			type: { name: "boolean" },
-			table: {
-				type: { summary: "boolean" },
-				category: "State",
-			},
-			control: "boolean",
-		},
 	},
 	args: {
 		rootClass: "spectrum-LogicButton",
 		variant: "and",
 		isDisabled: false,
-		isFocused: false,
 	},
 	parameters: {
 		actions: {
