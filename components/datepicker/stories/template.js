b716ba8bb7a754e1864af6e91414590c99ef2ffe
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-10-10 09:24:04 -0600

    chore(datepicker): add themes to template

--- components/datepicker/stories/template.js
+++ components/datepicker/stories/template.js
@@ -31,6 +31,16 @@ export const Template = ({
 	const [_, updateArgs] = useArgs();
 	const [{ lang }] = useGlobals();
 
+	const { express } = globals;
+
+	try {
+		if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
+		else import(/* webpackPrefetch: true */ "../themes/express.css");
+	} catch (e) {
+		console.warn(e);
+	}
+
+
 	return html`
 		<div
 			class=${classMap({
