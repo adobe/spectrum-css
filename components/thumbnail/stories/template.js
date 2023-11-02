01e3a06b69dd267fbadd2c07caf51eca3cd0d3f3
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-11-02 15:18:08 -0600

    feat(thumbnail): add isFocused on storybook

--- components/thumbnail/stories/template.js
+++ components/thumbnail/stories/template.js
@@ -27,6 +27,72 @@ export const Template = ({
 
 	const image = imageURL ? html`<img class="${rootClass}-image" src=${imageURL} alt=${ifDefined(altText)}/>` : svg ? html`${svg}` : "";
 
+	if (isLayer)
+		return html`
+			<div
+				class=${classMap({
+					[rootClass]: true,
+					[`${rootClass}--cover`]: isCover,
+					[`${rootClass}-layer`]: isLayer,
+					[`is-selected`]: isSelected,
+					[`is-disabled`]: isDisabled,
+					[`is-focused`]: isFocused,
+					[`${rootClass}--size${size}`]: typeof size !== "undefined",
+					...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+				})}
+				id=${ifDefined(id)}
+				@click=${onclick}
+			>
+				${OpacityCheckerboard({
+					...globals,
+					componentOnly: true,
+					customClasses: [`${rootClass}-layer-inner`],
+					content: checkerboardContentLayer,
+				})}
+			</div>
+		`;
+
+	if (backgroundColor)
+		return html`
+			<div
+				class=${classMap({
+					[rootClass]: true,
+					[`${rootClass}--cover`]: isCover,
+					[`${rootClass}-layer`]: isLayer,
+					[`is-selected`]: isSelected,
+					[`is-disabled`]: isDisabled,
+					[`is-focused`]: isFocused,
+					[`${rootClass}--size${size}`]: typeof size !== "undefined",
+					...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+				})}
+				id=${ifDefined(id)}
+				@click=${onclick}
+			>
+				<div class="${rootClass}-background" style=${styleMap(styles)}></div>
+				<div class="${rootClass}-image-wrapper">
+					${imageURL
+						? html`<img
+								class="${rootClass}-image"
+								src=${imageURL}
+								alt=${altText}
+						  />`
+						: ""}
+				</div>
+			</div>
+		`;
+
+	const checkerboardContent = html`
+			<div class="${rootClass}-image-wrapper">
+			${imageURL
+				? html`<img
+						class="${rootClass}-image"
+						src=${imageURL}
+						alt=${altText}
+					/>`
+				: ""}
+			${svg ? html`${svg}` : ""}
+		</div>
+	`
 	return html`
 		<div
 			class=${classMap({
