da245154b601e995665b40d86c746c990e0b35bf
Josh Johnson <jnjosh@adobe.com>
2023-11-08 09:42:42 -0500

    feat(dropindicator)!: migrate to spectrum tokens (#2198)
    BREAKING CHANGE: migrates DropIndicator to use `@adobe/spectrum-tokens`

---
+++ components/dropindicator/metadata/mods.md
@@ -0,0 +1,6 @@
+| Modifiable Custom Properties       |
+| ---------------------------------- |
+| `--mod-dropindicator-border-color` |
+| `--mod-dropindicator-border-size`  |
+| `--mod-dropindicator-circle-color` |
+| `--mod-dropindicator-circle-size`  |
--- components/dropindicator/stories/template.js
+++ components/dropindicator/stories/template.js
@@ -4,7 +4,6 @@ import { ifDefined } from "lit/directives/if-defined.js";
 import { styleMap } from "lit/directives/style-map.js";
 
 import "../index.css";
-import "../skin.css";
 
 export const Template = ({
 	rootClass = "spectrum-DropIndicator",
@@ -12,17 +11,29 @@ export const Template = ({
 	customStyles = {},
 	direction = "vertical",
 	size = "300px",
-}) => html`
-	<div
-		class=${classMap({
-			[rootClass]: true,
-			[`${rootClass}--${direction}`]: typeof direction !== "undefined",
-			...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-		})}
-		style=${ifDefined(styleMap({
-			blockSize: direction == "vertical" ? size : undefined,
-			inlineSize: direction == "horizontal" ? size : undefined,
-			...customStyles,
-		}))}
-	></div>
-`;
+	...globals
+}) => {
+	const { express } = globals;
+
+	try {
+		if (!express) import(/* webpackPrefetch: true */ "../themes/spectrum.css");
+		else import(/* webpackPrefetch: true */ "../themes/express.css");
+	} catch (e) {
+		console.warn(e);
+	}
+
+	return html`
+		<div
+			class=${classMap({
+				[rootClass]: true,
+				[`${rootClass}--${direction}`]: typeof direction !== "undefined",
+				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+			})}
+			style=${ifDefined(styleMap({
+				blockSize: direction == "vertical" ? size : undefined,
+				inlineSize: direction == "horizontal" ? size : undefined,
+				...customStyles,
+			}))}
+		></div>
+	`;
+};
