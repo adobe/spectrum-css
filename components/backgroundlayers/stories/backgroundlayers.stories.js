a973b69bab7c48abb0b1b41ff93f1f6061b9eeee
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-11-09 10:33:34 -0700

    docs(backgroundlayers): style changes

--- components/backgroundlayers/stories/backgroundlayers.stories.js
+++ components/backgroundlayers/stories/backgroundlayers.stories.js
@@ -2,7 +2,7 @@ import { Template } from "./template";
 import { html } from 'lit';
 
 export default {
-  title: "Elements/Background layers",
+  title: "UtilitityClasses/Background layers",
   description: "The background layers is a series of classes used to style background layers.",
   component: "BackgroundLayers",
   argTypes: {},
@@ -24,19 +24,19 @@ const EditingContext = ({
 	return html`
 		<div style="display: flex; justify-content: flex-start; position: relative; block-size: 150px;">
 			${Template({
-        style: "z-index: 4;",
+        style: "z-index: 4; inset-inline-start: 10px; inset-block-start: 10px;",
 				layer: 'elevated',
 			})}
 			${Template({
-        style: "z-index: 3; inset-inline-start: 15px; inset-block-start: 15px;",
+        style: "z-index: 3; inset-inline-start: 20px; inset-block-start: 20px;",
 				layer: 'layer2',
 			})}
 			${Template({
-        style: "z-index: 2; inset-inline-start: 25px; inset-block-start: 25px;",
+        style: "z-index: 2; inset-inline-start: 30px; inset-block-start: 30px;",
 				layer: 'layer1',
 			})}
 			${Template({
-        style: "z-index: 1; inset-inline-start: 35px; inset-block-start: 35px;",
+        style: "z-index: 1; inset-inline-start: 40px; inset-block-start: 40px;",
 				layer: "pasteboard",
 			})}
 		</div>
@@ -48,15 +48,15 @@ const BrowsingContext = ({
 	return html`
 		<div style="display: flex; justify-content: flex-start; position: relative; block-size: 150px;">
 			${Template({
-        style: "z-index: 3;",
+        style: "z-index: 3; inset-inline-start: 10px; inset-block-start: 10px;",
 				layer: 'elevated',
 			})}
 			${Template({
-        style: "z-index: 2; inset-inline-start: 15px; inset-block-start: 15px;",
+        style: "z-index: 2; inset-inline-start: 20px; inset-block-start: 20px;",
 				layer: 'layer1',
 			})}
 			${Template({
-        style: "z-index: 1; inset-inline-start: 25px; inset-block-start: 25px;",
+        style: "z-index: 1; inset-inline-start: 30px; inset-block-start: 30px;",
 				layer: "base",
 			})}
 		</div>
