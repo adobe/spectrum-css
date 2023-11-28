010dfe58d29a8cc2ee951e00cb778ccda38c0053
Josh Winn <965114+jawinn@users.noreply.github.com>
2023-11-28 12:28:28 -0500

    fix: mod-extractor support for multiple css files in root
    Fixes the fact that if there are multiple CSS files in the root, such
    as with the icon component (and commons), only the mods from the last
    file found will be included. During migration changes to Icon, some mods
    were being left out of the metadata when the utility was run.
    
    The utility now bundles all parsed mods per component directory into an
    array before exporting them. It also splits the utility into two
    functions as it was quite large and the functionality was very
    independent.

--- components/commons/metadata/mods.md
+++ components/commons/metadata/mods.md
@@ -1,4 +1,8 @@
 | Modifiable Custom Properties              |
 | ----------------------------------------- |
+| `--mod-animation-duration-100`            |
+| `--mod-focus-indicator-gap`               |
+| `--mod-line-height-100`                   |
 | `--mod-overlay-animation-distance`        |
 | `--mod-overlay-animation-duration-opened` |
+| `--mod-sans-font-family-stack`            |
--- tasks/mod-extractor.js
+++ tasks/mod-extractor.js
@@ -14,8 +14,16 @@ const { writeFile, readFile } = require("fs").promises;
 const path = require("path");
 const fg = require("fast-glob");
 
-async function main(dir = path.join(__dirname, "../components")) {
-	/* Loop over the directories in the components folder and find all the first-level css files */
+/**
+ * Parse first-level CSS files to extract "--mod" custom properties.
+ *
+ * @param {string} dir Parent components directory. 
+ * @returns Array of objects containing the component directory and the list of mod names.
+ */
+async function collectMods(dir = path.join(__dirname, "../components")) {
+	let modsPerPath = [];
+
+	// Loop over the directories in the components folder and find all the first-level CSS files.
 	for (const filepath of await fg("*/*.css", {
 		cwd: dir,
 		absolute: true,
@@ -37,53 +45,88 @@ async function main(dir = path.join(__dirname, "../components")) {
 		 */
 		const regex = /(--mod-(?:\w|-)+)(?!:|\w|-)/g;
 
-		/* Read the file and find all the matches */
+		// Read the file and find all the matches.
 		const matches = await readFile(filepath, "utf-8")
 			.then((content) => {
-				// assign the matches to an array through the spread operator and map the results to the first capture group
+				// Assign the matches to an array through the spread operator
+				// and map the results to the first capture group.
 				return [...content.matchAll(regex)].map((match) => match[1]);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 
-		// If there are no matches, skip this file and continue to the next one
+		// If there are no matches, skip this file and continue to the next one.
 		if (!matches || matches.length === 0) continue;
 
-		/* Remove duplicates using a Set and sort the results (default is alphabetical) */
-		const found = [...new Set(matches)].sort();
-		/* -- Markdown Output -- */
-		/* Output as a markdown table in the metadata folder for site rendering */
-		let destPath = `${path.dirname(filepath)}/metadata`;
-		// If the metadata folder doesn't exist, create it
-		if (!existsSync(destPath)) mkdirSync(destPath);
-
-		let formattedResults = [
-			"| Modifiable Custom Properties |\n| --- |",
-			...found.map((result) => `| \`${result}\` |`),
-		];
-
-		// Write the results to a markdown file in the metadata folder
-		await writeFile(
-			`${destPath}/mods.md`,
-			formattedResults.join("\n"),
-			(err) => {
-				if (err) throw err;
-			}
-		);
-
-		/* -- JSON Output -- */
-		destPath = `${path.dirname(filepath)}/dist`;
-		// If the dist folder doesn't exist yet, create it
-		if (!existsSync(destPath)) mkdirSync(destPath);
-
-		formattedResults = JSON.stringify({ mods: found }, null, 2);
-
-		// Write the JSON output to the dist folder
-		await writeFile(`${destPath}/mods.json`, formattedResults, (err) => {
-			if (err) throw err;
-		});
+		// Add matches to list of mods associated with this component (directory).
+		// Append to existing array or add new object to array.
+		const directory = path.dirname(filepath);
+		existingPathIndex = modsPerPath.findIndex(obj => obj.directory == directory);
+		if (existingPathIndex != -1){
+			modsPerPath[existingPathIndex].matches.push(...matches);
+		} else {
+			modsPerPath.push({ directory, matches });
+		}
 	}
+
+	return modsPerPath;
 }
 
-main();
+/**
+ * Output list of mods to a markdown file and a JSON file in the metadata directory.
+ * Sorts list and exludes any duplicates.
+ * 
+ * @param {String} directory Directory to write the metadata folder with mods files.
+ * @param {Array} mods Array of custom properties starting with "--mod".
+ */
+async function outputMods(directory, mods = []){
+	if (!mods || mods.length === 0 || !directory) return;
+
+	// Remove duplicates using a Set and sort the results (default is alphabetical).
+	const found = [...new Set(mods)].sort();
+
+	/* -- Markdown Output -- */
+	/* Output as a markdown table in the metadata folder for site rendering */
+	let destPath = `${directory}/metadata`;
+	// If the metadata folder doesn't exist, create it.
+	if (!existsSync(destPath)) mkdirSync(destPath);
+
+	let formattedResults = [
+		"| Modifiable Custom Properties |\n| --- |",
+		...found.map((result) => `| \`${result}\` |`),
+	];
+
+	// Write the results to a markdown file in the metadata folder.
+	await writeFile(
+		`${destPath}/mods.md`,
+		formattedResults.join("\n"),
+		(err) => {
+			if (err) throw err;
+		}
+	);
+
+	/* -- JSON Output -- */
+	destPath = `${directory}/dist`;
+	// If the dist folder doesn't exist yet, create it.
+	if (!existsSync(destPath)) mkdirSync(destPath);
+
+	formattedResults = JSON.stringify({ mods: found }, null, 2);
+
+	// Write the JSON output to the dist folder.
+	await writeFile(`${destPath}/mods.json`, formattedResults, (err) => {
+		if (err) throw err;
+	});
+}
+
+/**
+ * Parse CSS files to extract "--mod" custom properties, and export the list of mods
+ * to both a markdown file and a JSON file for each component.
+ */
+(async () => {
+	const modsPerPath = await collectMods();
+	// Output mods list for each component (directory).
+	modsPerPath.forEach(m => {
+		outputMods(m.directory, m.matches);
+	});
+})();
