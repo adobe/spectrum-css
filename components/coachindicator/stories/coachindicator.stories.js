8794dddac906c4fcde4f75f3fec81e35daa1537f
Patrick Fulton <pfulton@adobe.com>
2023-11-29 13:32:05 -0500

    feat(coachindicator): adds new component

---
+++ components/coachindicator/stories/coachindicator.stories.js
@@ -0,0 +1,68 @@
+import { html } from "lit";
+import { Template } from "./template";
+
+export default {
+	title: "components/Coach indicator",
+	description:
+		"The coach indicator component can be used to bring added attention to specific parts of a page.",
+	component: "CoachIndicator",
+	argTypes: {
+		// @todo: remove the disabling of reducedMotion once this global control is enabled, coach indicator does have reduced motion styling.
+		reducedMotion: { table: { disable: true } },
+		isQuiet: {
+			name: "Quiet styling",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Component",
+			},
+			control: "boolean",
+		},
+		variant: { table: { disable: true } },
+	},
+	args: {
+		rootClass: "spectrum-CoachIndicator",
+		isQuiet: false,
+		variant: "default",
+	},
+	parameters: {
+		actions: {
+			handles: [],
+		},
+		status: {
+			type: process.env.MIGRATED_PACKAGES.includes("coachindicator")
+				? "migrated"
+				: undefined,
+		},
+	},
+};
+
+const CustomCoachIndicator = (args) => {
+	return html`
+	<div
+		style="display: flex;">
+			${Template({
+				...args,
+				variant: "default"
+			})}
+			${Template({
+				...args,
+				variant: "dark"
+			})}
+			${Template({
+				...args,
+				variant: "light"
+			})}
+		<div>
+	`;
+};
+
+
+export const Default = CustomCoachIndicator.bind({});
+Default.args = {};
+
+export const Quiet = CustomCoachIndicator.bind({});
+Quiet.args = {
+	isQuiet: true
+};
+
