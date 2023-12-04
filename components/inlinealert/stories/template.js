60d997e3c8078507292ea448f42f1f92276c5344
Melissa Thompson <mthompson@heysparkbox.com>
2023-12-04 12:15:51 -0500

    docs(inlinealert): additional wrap examples only for chromatic

--- components/inlinealert/stories/template.js
+++ components/inlinealert/stories/template.js
@@ -1,15 +1,17 @@
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
-// import { ifDefined } from 'lit/directives/if-definedjs';
+import { ifDefined } from "lit/directives/if-defined.js";
+import { styleMap } from "lit/directives/style-map.js";
 
-import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 import { Template as Button } from "@spectrum-css/button/stories/template.js";
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
 import "../index.css";
 
 export const Template = ({
 	rootClass = "spectrum-InLineAlert",
 	customClasses = [],
+	customStyles = {},
 	headerText,
 	text,
 	variant = "neutral",
@@ -75,6 +77,8 @@ export const Template = ({
 				[`${rootClass}--${variant}`]: typeof variant !== "undefined",
 				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
 			})}
+			style=${ifDefined(styleMap(customStyles))}
+
 		>
 			<div class="${rootClass}-header">
 				${variant.charAt(0).toUpperCase() + variant.slice(1)} ${headerText}
