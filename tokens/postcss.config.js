22ee23aba5ebb39845ff2659b151cb535aea6906
castastrophe <castastrophe@users.noreply.github.com>
2024-02-07 12:37:26 -0500

    fix(tokens): support style-dictionary build in windows env

--- tokens/postcss.config.js
+++ tokens/postcss.config.js
@@ -11,7 +11,7 @@
  * governing permissions and limitations under the License.
  */
 
-module.exports = ({
+export default () => ({
 	resolveImports = true,
 	...options
 }) => require("../postcss.config.js")({
