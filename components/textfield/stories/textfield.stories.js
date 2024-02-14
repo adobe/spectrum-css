0c9e6c190b086de21e4dd4e85a73115b7889dc0c
Jenn Diaz <jenn.diaz@heysparkbox.com>
2024-02-14 15:03:00 -0700

    docs(textarea): default styling for text area in sb

--- components/textfield/stories/textfield.stories.js
+++ components/textfield/stories/textfield.stories.js
@@ -281,6 +281,6 @@ Default.args = {
 export const TextArea = TextAreaGroup.bind({});
 TextArea.args = {
 	multiline: true,
-	grows: true,
-	value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+	value:
+		"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
 };
