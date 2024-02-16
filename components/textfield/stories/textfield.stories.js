468b426180eaf10ff1eca7fc1e532b6c4fe21432
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2024-02-16 11:47:31 -0700

    fix(textfield): icon positioning with field label (#2380)

--- components/textfield/stories/textfield.stories.js
+++ components/textfield/stories/textfield.stories.js
@@ -1,5 +1,6 @@
 // Import the component markup template
 import { Template } from "./template";
+import { html } from "lit";
 
 export default {
 	title: "Components/Text field",
@@ -15,6 +16,36 @@ export default {
 				category: "State",
 			},
 			control: "boolean",
+		},
+		displayLabel: {
+			name: "Display field label",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Component",
+			},
+			control: "boolean",
+		},
+		labelPosition: {
+			name: "Label position",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Component",
+			},
+			options: ["top", "side"],
+			control: "select",
+			if: { arg: "displayLabel", truthy: true },
+		},
+		labelText: {
+			name: "Label text",
+			type: { name: "text" },
+			table: {
+				type: { summary: "text" },
+				category: "Component",
+			},
+			control: "text",
+			if: { arg: "displayLabel", truthy: true },
 		},
 		isInvalid: {
 			name: "Invalid",
@@ -146,6 +177,9 @@ export default {
 		isFocused: false,
 		isKeyboardFocused: false,
 		isLoading: false,
+		displayLabel: false,
+		labelPosition: "top",
+		labelText: "Username",
 		size: "m",
 		multiline: false,
 		grows: false,
@@ -163,13 +197,90 @@ export default {
 	},
 };
 
-export const Default = Template.bind({});
-Default.args = {};
+const TextFieldGroup = ({
+	...args
+}) => {
+	return html`
+		<div style="display: flex; flex-direction: column; gap: 2rem;">
+			${Template({
+				...args
+			})}
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username",
+				})
+				: null }
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username that is really long and wraps onto a second line",
+					isInvalid: true,
+				})
+				: null }
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username",
+					labelPosition: 'side',
+					isValid: true,
+					value: "username@reallylongemail.com"
+				})
+				: null }
+		</div>
+	`;
+};
 
-export const TextArea = Template.bind({});
+const TextAreaGroup = ({
+	...args
+}) => {
+	return html`
+		<div style="display: flex; flex-direction: column; gap: 2rem;">
+			${Template({
+				...args,
+				multiline: true,
+				value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+			})}
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username",
+					multiline: true,
+					value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+				})
+				: null }
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username that is really long and wraps onto a second line",
+					isInvalid: true,
+					multiline: true,
+					value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+				})
+				: null }
+			${window.isChromatic() ?
+				Template({
+					displayLabel: true,
+					labelText: "Username",
+					labelPosition: 'side',
+					isValid: true,
+					multiline: true,
+					value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+				})
+				: null }
+		</div>
+	`;
+};
+
+
+export const Default = TextFieldGroup.bind({});
+Default.args = {
+
+};
+
+export const TextArea = TextAreaGroup.bind({});
 TextArea.args = {
 	multiline: true,
 	grows: true,
-	value:
-		"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+	value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
 };
