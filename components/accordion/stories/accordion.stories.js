7fc6eb0bcbf12fdcac9025aef102aca2082c721c
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-03-04 13:37:12 -0500

    chore: minor accordion story updates (#2577)
    * chore: minor accordion story updates
    * docs: storybook template update
    * chore: storybook utilities imported

--- components/accordion/stories/accordion.stories.js
+++ components/accordion/stories/accordion.stories.js
@@ -1,12 +1,15 @@
 import { html } from "lit";
-import { when } from "lit/directives/when.js";
+import { styleMap } from "lit/directives/style-map.js";
 
-import { Template } from "./template";
+import { Template } from "@spectrum-css/accordion/stories/template.js";
+import { Template as Link } from "@spectrum-css/link/stories/template.js";
+import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
 
+/**
+ * The accordion element contains a list of items that can be expanded or collapsed to reveal additional content or information associated with each item. There can be zero expanded items, exactly one expanded item, or more than one item expanded at a time, depending on the configuration. This list of items is defined by child accordion item elements.
+ */
 export default {
 	title: "Components/Accordion",
-	description:
-		"The accordion element contains a list of items that can be expanded or collapsed to reveal additional content or information associated with each item. There can be zero expanded items, exactly one expanded item, or more than one item expanded at a time, depending on the configuration. This list of items is defined by child accordion item elements.",
 	component: "Accordion",
 	argTypes: {
 		items: { table: { disable: true } },
@@ -45,6 +48,94 @@ export default {
 		size: "m",
 		density: "regular",
 		disableAll: false,
+		/* Content sourced from: https://www.adobe.com/products/catalog.html#:~:text=Frequently%20asked%20questions. */
+		items: new Map([
+			[
+				"Are any Adobe products free?",
+				{
+					content:
+						"Yes, Adobe offers free products like Acrobat Reader, Aero, Fill & Sign, Photoshop Express, and Adobe Scan. You can also use Creative Cloud Express, Fresco, and Lightroom Mobile for free, with the option of making in-app purchases.",
+					isDisabled: true,
+				},
+			],
+			[
+				"Are Adobe products worth it?",
+				{
+					content: Typography({
+						semantics: "body",
+						content: [
+							"Adobe makes some of the most widely used software applications in the world, many of which are industry standard. Get started with free apps like Adobe Acrobat Reader, Aero, Fill & Sign, Photoshop Express, and Adobe Scan. Or consider Creative Cloud, with plans starting at just US$9.99/mo. Every Adobe Creative Cloud plan includes perks like free stock images and fonts, collaboration tools, and cloud storage as well as regular feature updates to deliver the latest technology.",
+							Link({
+								url: "https://www.adobe.com/creativecloud/plans.html",
+								text: "Learn more about Adobe Creative Cloud plans and pricing.",
+							}),
+						],
+					}),
+					isOpen: true,
+				},
+			],
+			[
+				"Which Adobe product is best for editing PDFs?",
+				{
+					content: Typography({
+						semantics: "body",
+						content: [
+							"You can edit PDFs with Adobe Acrobat, which is available in two editions: Standard and Pro. Acrobat Standard provides basic tools to create, edit, and sign PDFs on Windows devices. Acrobat Pro is the complete PDF solution with tools to edit, convert, and sign PDFs across web, mobile, and tablet, as well as on Windows and macOS computers. If you'd like to try before you buy, you can get a free 7-day trial of Acrobat Pro.",
+							Link({
+								url: "https://www.adobe.com/acrobat.html",
+								text: "Learn more about Acrobat.",
+							}),
+						],
+					}),
+				},
+			],
+			[
+				"How many products does Adobe have?",
+				{
+					content:
+						"Adobe offers nearly 100 products. Get creative with industry-standard apps like Adobe Photoshop, Illustrator InDesign, and Lightroom. Create, edit, and sign PDFs with Adobe Acrobat and Acrobat Sign. And deliver exceptional customer experiences with our marketing and commerce apps such as Adobe Experience Manager, Campaign, and Target.",
+					isOpen: true,
+				},
+			],
+			[
+				"How much do Adobe products cost?",
+				{
+					content: Typography({
+						semantics: "body",
+						content: [
+							"Creative Cloud plans start at US$9.99/mo. You can subscribe to specific Single App plans or get 20+ creative apps and services in the Creative Cloud All Apps plan.",
+							Link({
+								url: "https://www.adobe.com/creativecloud/plans.html",
+								text: "Explore Creative Cloud plans.",
+							}),
+						],
+					}),
+				},
+			],
+			[
+				"What are the most popular Adobe products?",
+				{
+					content:
+						"Adobe makes some of the most widely used software in the world, including popular apps like Acrobat Pro, Photoshop, Illustrator, InDesign, Lightroom, and Premiere Pro.",
+				},
+			],
+			[
+				"How can I get a student discount on Adobe products?",
+				{
+					content: Typography({
+						semantics: "body",
+						content: [
+							`Students who provide a valid school-issued email address at purchase are eligible to save over 60% on Creative Cloud All Apps, which includes 20+ apps such as Photoshop, Illustrator, InDesign, Acrobat Pro, and more. ${Link(
+								{
+									url: "https://www.adobe.com/creativecloud/buy/students.html",
+									text: "Learn more about Creative Cloud for students.",
+								}
+							)}`,
+						],
+					}),
+				},
+			]
+		]),
 	},
 	parameters: {
 		actions: {
@@ -53,133 +144,32 @@ export default {
 		status: {
 			type: process.env.MIGRATED_PACKAGES.includes("accordion")
 				? "migrated"
-				: undefined,
+				: "legacy",
 		},
 	},
 };
 
-const AccordionGroup = ({
-	customStyles = {},
-	...args
-}) => {
-	return html`
-		<div style="display: flex; gap: 2rem;">
+const AccordionGroup = (args) => html`
+	${window.isChromatic() ? html`
+		<div style=${styleMap({
+			display: "flex",
+			flexWrap: "wrap",
+			gap: "2em"
+		})}>
+			${Template(args)}
 			${Template({
 				...args,
-				items: new Map([
-				[
-					"Recent",
-					{
-						content: "Item 1",
-						isOpen: true,
-						isDisabled: false,
-					},
-				],
-				[
-					"Architecture",
-					{
-						content: "Item 2",
-						isOpen: false,
-						isDisabled: true,
-					},
-				],
-				[
-					"Nature",
-					{
-						content: "Item 3",
-						isOpen: false,
-						isDisabled: false,
-					},
-				],
-				[
-					"Really Long Accordion Item that should wrap",
-					{
-						content: "Really long content that should wrap when component has a max or fixed width",
-						isOpen: true,
-						isDisabled: false,
-					},
-				],
-			]),
+				customStyles: {
+					maxInlineSize: "300px",
+				},
 			})}
-			${when(window.isChromatic(), () =>
-				Template({
-					...args,
-					customStyles: { "max-inline-size": "300px"},
-					items: new Map([
-					[
-						"Recent",
-						{
-							content: "Item 1",
-							isOpen: true,
-							isDisabled: false,
-						},
-					],
-					[
-						"Architecture",
-						{
-							content: "Item 2",
-							isOpen: false,
-							isDisabled: true,
-						},
-					],
-					[
-						"Nature",
-						{
-							content: "Item 3",
-							isOpen: false,
-							isDisabled: false,
-						},
-					],
-					[
-						"Really Long Accordion Item that should wrap",
-						{
-							content: "Really long content that should wrap when component has a max or fixed width",
-							isOpen: true,
-							isDisabled: false,
-						},
-					],
-				]),
-				})
-			)}
-			${when(window.isChromatic(), () =>
-				Template({
-					...args,
-					disableAll: true,
-					items: new Map([
-					[
-						"Recent",
-						{
-							content: "Item 1",
-							isOpen: true,
-						},
-					],
-					[
-						"Architecture",
-						{
-							content: "Item 2",
-							isOpen: false,
-						},
-					],
-					[
-						"Nature",
-						{
-							content: "Item 3",
-							isOpen: false,
-						},
-					],
-					[
-						"Really Long Accordion Item that should wrap",
-						{
-							content: "Really long content that should wrap when component has a max or fixed width",
-							isOpen: true,
-						},
-					],
-				]),
-				})
-			)}
+			${Template({
+				...args,
+				disableAll: true,
+			})}
 		</div>
-	`;
-};
+	` : Template(args)}
+`;
 
 export const Default = AccordionGroup.bind({});
 Default.args = {};
--- components/accordion/stories/template.js
+++ components/accordion/stories/template.js
@@ -1,12 +1,14 @@
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
-import { styleMap } from "lit/directives/style-map.js";
-import { repeat } from "lit/directives/repeat.js";
 import { ifDefined } from "lit/directives/if-defined.js";
+import { repeat } from "lit/directives/repeat.js";
+import { styleMap } from "lit/directives/style-map.js";
+
+import { useArgs } from "@storybook/client-api";
 
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
-import "../index.css";
+import "@spectrum-css/accordion/index.css";
 
 export const AccordionItem = ({
 	heading,
@@ -17,27 +19,23 @@ export const AccordionItem = ({
 	isDisabled = false,
 	isOpen = false,
 	iconSize = "m",
-	disableAll = false,
 	customStyles = {},
-	// customClasses = [],
+	customClasses = [],
+	onclick,
 	...globals
 }) => {
 	return html`
 		<div
 			class=${classMap({
-				[rootClass]: true,
-				"is-open": isOpen,
-				"is-disabled": isDisabled || disableAll,
-			})}
+		[rootClass]: true,
+		"is-open": isOpen && !isDisabled,
+		"is-disabled": isDisabled,
+		...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+	})}
 			id=${ifDefined(id)}
 			style=${ifDefined(styleMap(customStyles))}
 			role="presentation"
-			@click=${(evt) => {
-				if (disableAll || isDisabled || !evt || !evt.target) return;
-				const closest = evt.target.closest(`.${rootClass}`);
-				if (!closest) return;
-				closest.classList.toggle("is-open");
-			}}
+			@click=${onclick}
 		>
 			<!-- WAI-ARIA 1.1: Item header is a <button> wrapped within a <h3> element, rather than a <div> element with role="tab" -->
 			<h3 class="${rootClass}Heading">
@@ -45,7 +43,7 @@ export const AccordionItem = ({
 				<button
 					class="${rootClass}Header"
 					type="button"
-					?disabled=${isDisabled || disableAll}
+					?disabled=${isDisabled}
 					id="spectrum-accordion-item-${idx}-header"
 					aria-controls="spectrum-accordion-item-${idx}-content"
 					aria-expanded="${open ? "true" : "false"}"
@@ -54,7 +52,8 @@ export const AccordionItem = ({
 				</button>
 				<span class="${rootClass}IconContainer">
 					${Icon({
-						iconName: "ChevronRight",
+						iconName: !isOpen ? "ChevronRight" : "ChevronDown",
+						setName: "ui",
 						size: iconSize,
 						customClasses: [`${rootClass}Indicator`],
 						...globals,
@@ -80,11 +79,15 @@ export const Template = ({
 	density = "regular",
 	items,
 	id,
-	disableAll,
+	disableAll = false,
 	customClasses = [],
+	customStyles = {},
 	...globals
 }) => {
+	const [_, updateArgs] = useArgs();
+
 	if (!items || !items.size) return html``;
+
 	return html`
 		<div
 			class="${classMap({
@@ -97,17 +100,29 @@ export const Template = ({
 			})}"
 			id=${ifDefined(id)}
 			role="region"
+			style=${ifDefined(styleMap(customStyles))}
 		>
 			${repeat(Array.from(items.keys()), (heading, idx) => {
 				const item = items.get(heading);
 				return AccordionItem({
+					...globals,
 					rootClass: `${rootClass}-item`,
 					heading,
 					idx,
 					iconSize: `${size}`,
+					isDisabled: item.isDisabled || disableAll,
 					...item,
-					...globals,
-					disableAll,
+					onclick: () => {
+						if (item.isDisabled) return;
+
+						// Update the args
+						const newItems = new Map(items);
+						newItems.set(heading, {
+							...item,
+							isOpen: !item.isOpen,
+						});
+						updateArgs({ items: newItems });
+					},
 				});
 			})}
 		</div>
