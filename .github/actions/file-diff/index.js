7ca664efcbde3ae0498423373a326081351404d8
castastrophe <castastrophe@users.noreply.github.com>
2024-03-25 10:37:31 -0400

    ci: remove summary table from file diff

--- .github/actions/file-diff/index.js
+++ .github/actions/file-diff/index.js
@@ -12,14 +12,21 @@
  */
 
 const { existsSync } = require("fs");
-const { join, sep } = require("path");
+const { join } = require("path");
 
 const core = require("@actions/core");
 
 const {
-	fetchFilesAndSizes,
+	fetchFileData,
 	bytesToSize,
 	addComment,
+	splitDataByPackage,
+	makeTable,
+	difference,
+	isRemoved,
+	isNew,
+	printChange,
+	printPercentChange,
 } = require("./utilities.js");
 
 async function run() {
@@ -37,42 +44,30 @@ async function run() {
 
 		// --------------- Evaluate compiled assets  ---------------
 		/** @type Map<string, number> */
-		const headOutput = await fetchFilesAndSizes(headPath, fileGlobPattern, {
-			core,
-		});
+		const headFileStats = await fetchFileData(headPath, fileGlobPattern);
+
 		/**
 		 * If a diff path is provided, get the diff files and their sizes
 		 * @type Map<string, number>
 		 **/
-		const baseOutput = await fetchFilesAndSizes(basePath, fileGlobPattern, {
-			core,
-		});
+		const baseFileStats = await fetchFileData(basePath, fileGlobPattern);
+
 		/**
 		 * Indicates that there are files we're comparing against
 		 * and not just reporting on the overall size of the compiled assets
 		 * @type boolean
 		 */
-		const hasBase = baseOutput.size > 0;
+		const shouldCompare = baseFileStats.size > 0;
 		// --------------- End evaluation  ---------------
 
 		/** Split the data by component package */
-		const { filePath, PACKAGES } = splitDataByPackage(headOutput, headPath, baseOutput);
+		const { filePath, PACKAGES } = splitDataByPackage(headFileStats, headPath, baseFileStats);
+
+		if (PACKAGES.size === 0) {
+			throw new Error(`No packages could be identified in the provided workspace.`);
+		}
+
 		const sections = makeTable(PACKAGES, filePath, headPath);
-
-		/** Calculate the total size of the pull request's assets */
-		const overallHeadSize = [...headOutput.values()].reduce(
-			(acc, size) => acc + size,
-			0
-		);
-
-		/** Calculate the overall size of the base branch's assets */
-		const overallBaseSize = hasBase
-			? [...baseOutput.values()].reduce((acc, size) => acc + size, 0)
-			: undefined;
-
-		const hasChange = overallHeadSize !== overallBaseSize;
-
-		/** If no diff map data provided, we're going to report on the overall size */
 
 		/**
 		 * If the updated assets are the same as the original,
@@ -82,48 +77,24 @@ async function run() {
 		const markdown = [];
 		const summary = [
 			"### Summary",
-			`**Total size**: ${bytesToSize(overallHeadSize)}<sup>*</sup>`,
 		];
 
-		let summaryTable = [];
-
 		if (sections.length === 0) {
+			// @TODO why would the sections come back empty?
 			summary.push(...["", " 🎉 No changes detected in any packages"]);
 		} else {
-			/**
-			 * Calculate the change in size
-			 * PR - base / base = change
-			 */
-			let changeSummary = "";
-			if (baseOutput.size > 0 && hasBase && hasChange) {
-				changeSummary = `**Total change (Δ)**: ${printChange(overallHeadSize, overallBaseSize)} (${printPercentChange(overallHeadSize, overallBaseSize)})`;
-			} else if (baseOutput.size > 0 && hasBase && !hasChange) {
-				changeSummary = `No change in file sizes`;
-			}
+			let hasChange = false;
 
-			if (changeSummary !== "") {
-				summary.push(
-					changeSummary,
-					"",
-					"<small><em>Table reports on changes to a package's main file. Other changes can be found in the collapsed <a href=\"#details\">Details</a> section below.</em></small>",
-					""
-				);
-			}
+			/* Push a line break before and after the section details */
+			markdown.push("");
 
-			markdown.push(
-				"<a name=\"details\"></a>",
-				`<details>`,
-				`<summary><b>Details</b></summary>`,
-				""
-			);
+			sections.map(({ name, filePath, hasChange, mainFile, fileMap }) => {
+				if (shouldCompare && !hasChange) return;
 
-			sections.map(({ name, filePath, headMainSize, baseMainSize, hasChange, mainFile, fileMap }) => {
-				if (!hasChange) return;
-
-				const data = [];
+				const detail = [];
 
 				/** We only evaluate changes if there is a diff branch being used and this is the main file for the package */
-				if (hasBase) {
+				if (shouldCompare) {
 					/**
 					 * If: the component folder exists in the original branch but not the PR
 					 * Or: the pull request file size is 0 or empty but the original branch has a size
@@ -141,28 +112,20 @@ async function run() {
 					if (
 						(existsSync(join(basePath, filePath, name)) && !existsSync(join(headPath, filePath, name)))
 					) {
-						data.push("🚨 deleted, moved, or renamed");
+						detail.push("🚨 deleted, moved, or renamed");
 					} else if (
 						(existsSync(join(headPath, filePath, name)) && !existsSync(join(basePath, filePath, name)))
 					) {
-						data.push("🎉 new");
-					} else if (
-						((Math.abs(difference(headMainSize, baseMainSize))) / 1000) >= 0.001
-					) {
-						data.push(printChange(headMainSize, baseMainSize));
-					}
-
-					if (data.length > 0) {
-						summaryTable.push([name, bytesToSize(headMainSize), data]);
+						detail.push("🎉 new");
 					}
 				}
 
 
-				const md = ["", `#### ${name}`, ""];
+				const md = ["", `#### ${name}`, ...detail, ""];
 				md.push(
 					...[
-						["File", "Head", ...(hasBase ? ["Base", "Δ"] : [])],
-						[" - ", " - ", ...(hasBase ? [" - ", " - "] : [])],
+						["File", "Head", ...(shouldCompare ? ["Base", "Δ"] : [])],
+						[" - ", " - ", ...(shouldCompare ? [" - ", " - "] : [])],
 					].map((row) => `| ${row.join(" | ")} |`),
 					...[...fileMap.entries()]
 						.reduce(
@@ -178,9 +141,9 @@ async function run() {
 										readableFilename === mainFile ? `**${readableFilename}**` : readableFilename,
 										// If the file was removed, note it's absense with a dash; otherwise, note it's size
 										isRemoved(headByteSize, baseByteSize) ? " - " : bytesToSize(headByteSize),
-										...(hasBase ? [
+										...(shouldCompare ? [
 											bytesToSize(baseByteSize),
-											isRemoved(headByteSize, baseByteSize) ? "🚨 deleted, moved, or renamed" : isNew(headByteSize, baseByteSize) ? "🎉 **new**" : `${printChange(headByteSize, baseByteSize)}${difference(headByteSize, baseByteSize) !== 0 ? ` (${printPercentChange(headByteSize , baseByteSize)})` : ""}`,
+											isRemoved(headByteSize, baseByteSize) ? "🚨 deleted, moved, or renamed" : isNew(headByteSize, baseByteSize) ? "🎉 **new**" : `${printChange(headByteSize, baseByteSize)}${difference(baseByteSize, headByteSize) !== 0 ? ` (${printPercentChange(headByteSize , baseByteSize)})` : ""}`,
 										] : []),
 									]
 								];
@@ -193,25 +156,31 @@ async function run() {
 				markdown.push(...md);
 			});
 
-			markdown.push("", `</details>`);
-		}
+			/**
+			 * Calculate the change in size
+			 * PR - base / base = change
+			 */
+			let changeSummary = "";
+			if (baseFileStats.size > 0 && shouldCompare && hasChange) {
+				changeSummary = `**Total change (Δ)**: ${printChange(overallBaseSize, overallHeadSize)} (${printPercentChange(overallHeadSize, overallBaseSize)})`;
+			} else if (baseFileStats.size > 0 && shouldCompare && !hasChange) {
+				changeSummary = `No change in file sizes`;
+			}
 
-		if (summaryTable.length > 0) {
-			// Add the headings to the summary table if it contains data
-			summaryTable = [
-				["Package", "Size", ...(hasBase ? ["Δ"] : [])],
-				["-", "-", ...(hasBase ? ["-"] : [])],
-				...summaryTable,
-			];
+			if (changeSummary !== "") {
+				summary.push(
+					changeSummary,
+					"",
+				);
+			}
 
-			summary.push(...summaryTable.map((row) => `| ${row.join(" | ")} |`));
+			markdown.push("");
 		}
 
 		markdown.push(
 			"",
 			"<small>",
-			"* <em>Size determined by adding together the size of the main file for all packages in the library.</em><br/>",
-			"* <em>Results are not gzipped or minified.</em><br/>",
+			"* <em>Represents the sum of all <strong>main</strong> files for packages in the library (i.e., @spectrum-css/accordion/dist/index.css).</em><br/>",
 			"* <em>An ASCII character in UTF-8 is 8 bits or 1 byte.</em>",
 			"</small>"
 		);
@@ -237,22 +206,22 @@ async function run() {
 		core.summary = summary.join("\n");
 
 		// --------------- Set output variables  ---------------
-		if (headOutput.size > 0) {
-			const headMainSize = [...headOutput.entries()].reduce(
+		if (headFileStats.size > 0) {
+			const headMainSize = [...headFileStats.entries()].reduce(
 				(acc, [_, size]) => acc + size,
 				0
 			);
 			core.setOutput("total-size", headMainSize);
 
-			if (hasBase) {
-				const baseMainSize = [...baseOutput.entries()].reduce(
+			if (shouldCompare) {
+				const baseMainSize = [...baseFileStats.entries()].reduce(
 					(acc, [_, size]) => acc + size,
 					0
 				);
 
 				core.setOutput(
 					"has-changed",
-					hasBase && headMainSize !== baseMainSize ? "true" : "false"
+					shouldCompare && headMainSize !== baseMainSize ? "true" : "false"
 				);
 			}
 		} else {
@@ -265,163 +234,3 @@ async function run() {
 }
 
 run();
-
-/** A few helpful utility functions; v1 == PR (change); v0 == base (initial) */
-const difference = (v1, v0) => v1 - v0;
-const isRemoved = (v1, v0) => (!v1 || v1 === 0) && (v0 && v0 > 0);
-const isNew = (v1, v0) => (v1 && v1 > 0) && (!v0 || v0 === 0);
-
-/**
- * Convert the provided difference between file sizes into a human
- * readable representation of the change.
- * @param {number} difference
- * @returns {string}
- */
-const printChange = function (v1, v0) {
-	/** Calculate the change in size: v1 - v0 = change */
-	const d = difference(v1, v0);
-	return d === 0
-		? `-`
-		: `${d > 0 ? "⬆" : "⬇"} ${bytesToSize(Math.abs(d))}`;
-};
-
-/**
- * Convert the provided difference between file sizes into a percent
- * value of the change.
- * @param {number} delta
- * @param {number} original
- * @returns {string}
- */
-const printPercentChange = function (v1, v0) {
-	const delta = ((v1 - v0) / v0) * 100;
-	if (delta === 0) return `no change`;
-	return `${delta.toFixed(2)}%`;
-};
-
-/**
- *
- * @param {Map<string, Map<string, { headByteSize: number, baseByteSize: number }>>} PACKAGES
- * @param {string} filePath - The path to the component's dist folder from the root of the repo
- * @param {string} path - The path from the github workspace to the root of the repo
- * @returns {Array<{ name: string, filePath: string, headMainSize: number, baseMainSize: number, hasChange: boolean, fileMap: Map<string, { headByteSize: number, baseByteSize: number }>}>}
- */
-const makeTable = function (PACKAGES, filePath, path) {
-	const sections = [];
-
-	/** Next convert that component data into a detailed object for reporting */
-	PACKAGES.forEach((fileMap, packageName) => {
-		// Read in the main asset file from the package.json
-		const packagePath = join(path, filePath, packageName, "package.json");
-
-		let mainFile = "index.css";
-		if (existsSync(packagePath)) {
-			const { main } = require(packagePath) ?? {};
-			if (main) mainFile = main.replace(/^.*\/dist\//, "");
-		}
-
-		const mainFileOnly = [...fileMap.keys()].filter((file) => file.endsWith(mainFile));
-		const headMainSize = mainFileOnly.reduce(
-			(acc, filename) => {
-				const { headByteSize = 0 } = fileMap.get(filename);
-				return acc + headByteSize;
-			},
-			0
-		);
-
-		const baseMainSize = mainFileOnly.reduce(
-			(acc, filename) => {
-				const { baseByteSize = 0 } = fileMap.get(filename);
-				return acc + baseByteSize;
-			},
-			0
-		);
-
-		const hasChange = fileMap.size > 0 && [...fileMap.values()].some(({ headByteSize, baseByteSize }) => headByteSize !== baseByteSize);
-
-		/**
-		 * We don't need to report on components that haven't changed unless they're new or removed
-		 */
-		if (headMainSize === baseMainSize) return;
-
-		sections.push({
-			name: packageName,
-			filePath,
-			headMainSize,
-			baseMainSize,
-			hasChange,
-			mainFile: mainFileOnly?.[0],
-			fileMap
-		});
-	});
-
-	return sections;
-};
-
-/**
- * Split out the data indexed by filename into groups by component
- * @param {Map<string, number>} dataMap
- * @param {string} path
- * @param {Map<string, number>} baseMap
- * @returns {{ filePath: string, PACKAGES: Map<string, Map<string, { headByteSize: number, baseByteSize: number }>>}}
- */
-const splitDataByPackage = function (dataMap, path, baseMap = new Map()) {
-	const PACKAGES = new Map();
-
-	let filePath;
-	[...dataMap.entries()].forEach(([file, headByteSize]) => {
-		// Determine the name of the component
-		const parts = file.split(sep);
-		const componentIdx = parts.findIndex((part) => part === "dist") - 1;
-		const packageName = parts[componentIdx];
-
-		if (!filePath) {
-			filePath = `${file.replace(path, "")}/${parts.slice(componentIdx + 1, -1).join(sep)}`;
-		}
-
-		const readableFilename = file.replace(/^.*\/dist\//, "");
-
-		const fileMap = PACKAGES.has(packageName)
-			? PACKAGES.get(packageName)
-			: new Map();
-
-		if (!fileMap.has(readableFilename)) {
-			fileMap.set(readableFilename, {
-				headByteSize,
-				baseByteSize: baseMap.get(file),
-			});
-		}
-
-		/** Update the component's table data */
-		PACKAGES.set(packageName, fileMap);
-	});
-
-	// Look for any base files not present in the head
-	[...baseMap.entries()].forEach(([file, baseByteSize]) => {
-		// Determine the name of the component
-		const parts = file.split(sep);
-		const componentIdx = parts.findIndex((part) => part === "dist") - 1;
-		const packageName = parts[componentIdx];
-
-		if (!filePath) {
-			filePath = `${file.replace(path, "")}/${parts.slice(componentIdx + 1, -1).join(sep)}`;
-		}
-
-		const readableFilename = file.replace(/^.*\/dist\//, "");
-
-		const fileMap = PACKAGES.has(packageName)
-			? PACKAGES.get(packageName)
-			: new Map();
-
-		if (!fileMap.has(readableFilename)) {
-			fileMap.set(readableFilename, {
-				headByteSize: dataMap.get(file),
-				baseByteSize,
-			});
-		}
-
-		/** Update the component's table data */
-		PACKAGES.set(packageName, fileMap);
-	});
-
-	return { filePath, PACKAGES };
-};
--- .github/actions/file-diff/utilities.js
+++ .github/actions/file-diff/utilities.js
@@ -11,11 +11,47 @@
  * governing permissions and limitations under the License.
  */
 
-const { statSync, existsSync, readdirSync } = require("fs");
-const { join, relative } = require("path");
+const { statSync, existsSync, readdirSync, readFileSync } = require("fs");
+const { readFile } = require("fs").promises;
+const { join, relative, dirname, sep, extname } = require("path");
 
 const github = require("@actions/github");
 const glob = require("@actions/glob");
+const core = require("@actions/core");
+
+const Diff = require("diff");
+
+/** A few helpful utility functions; v1 == PR (change); v0 == base (initial) */
+exports.difference = (v1, v0) => v1 - v0;
+exports.isRemoved = (v1, v0) => (!v1 || v1 === 0) && (v0 && v0 > 0);
+exports.isNew = (v1, v0) => (v1 && v1 > 0) && (!v0 || v0 === 0);
+
+/**
+ * Convert the provided difference between file sizes into a human
+ * readable representation of the change.
+ * @param {number} difference
+ * @returns {string}
+ */
+exports.printChange = function (v1, v0) {
+	/** Calculate the change in size: v1 - v0 = change */
+	const d = difference(v1, v0);
+	return d === 0
+		? `-`
+		: `${d > 0 ? "⬆" : "⬇"} ${bytesToSize(Math.abs(d))}`;
+};
+
+/**
+ * Convert the provided difference between file sizes into a percent
+ * value of the change.
+ * @param {number} delta
+ * @param {number} original
+ * @returns {string}
+ */
+exports.printPercentChange = function (v1, v0) {
+	const delta = ((v1 - v0) / v0) * 100;
+	if (delta === 0) return `no change`;
+	return `${delta.toFixed(2)}%`;
+};
 
 /**
  * List all files in the directory to help with debugging
@@ -23,7 +59,7 @@ const glob = require("@actions/glob");
  * @param {string[]} pattern
  * @returns {void}
  */
-function debugEmptyDirectory(path, pattern, { core }) {
+function debugEmptyDirectory(path, pattern) {
     if (!existsSync(path)) {
         throw new Error(`The provided path does not exist in the workspace`);
     }
@@ -31,10 +67,10 @@ function debugEmptyDirectory(path, pattern, { core }) {
     /**
      * Recursively report on the files in the provided directory for debugging
      * @param {string} path
-     * @param {{ core: import('@actions/core'), rootDir: string }} configuration
+     * @param {{ rootDir: string }} configuration
      * @returns {void}
      */
-    function reportOnDirectoryFiles(path, { core, rootDir = path }) {
+    function reportOnDirectoryFiles(path, { rootDir = path }) {
         readdirSync(path, { withFileTypes: true }).forEach((dirent) => {
             if (dirent.isFile()) {
                 const file = join(path, dirent.name);
@@ -44,13 +80,13 @@ function debugEmptyDirectory(path, pattern, { core }) {
                 const dir = join(path, dirent.name);
                 if (dirent.name.startsWith(".") || dirent.name === "node_modules") return;
                 core.info(`${relative(rootDir, dir)}/`);
-                reportOnDirectoryFiles(dir, { core, rootDir });
+                reportOnDirectoryFiles(dir, { rootDir });
             }
         });
     }
 
     core.startGroup(`All files in ${path}`);
-    reportOnDirectoryFiles(path, { core });
+    reportOnDirectoryFiles(path);
     core.endGroup();
 
     throw new Error(`No files found matching the glob pattern: ${pattern.map((p) => join(path, p)).join(", ")}`);
@@ -144,14 +180,29 @@ exports.addComment = async function ({ search, content, token }) {
 };
 
 /**
+ * @typedef {Object} FileData
+ * @property {number} size - a numeric representation of the file size via stat
+ * @property {string} content - a raw read-in of the file content
+ */
+
+/**
  * Use the provided glob pattern to fetch the files and their sizes from the
  * filesystem and return a Map of the files and their sizes.
  * @param {string} rootPath
  * @param {string[]} patterns
- * @returns {Promise<Map<string, number>>}
+ * @returns {Promise<Map<string, FileData>>}
  */
-exports.fetchFilesAndSizes = async function (rootPath, patterns = [], { core }) {
-    if (!existsSync(rootPath)) return new Map();
+exports.fetchFileData = async function (rootPath, patterns = []) {
+    // If no rootPath is provided
+    if (typeof rootPath === "undefined") {
+        return new Map();
+    }
+
+        // Log a warning that the provided path does not exist in the workspace
+    if (!existsSync(rootPath)) {
+        core.warning(`Provided path (${rootPath}) could not be found in the workspace.`);
+        return new Map();
+    }
 
     /** @type import('@actions/glob').Globber */
     const globber = await glob.create(patterns.map((f) => join(rootPath, f)).join("\n"));
@@ -161,7 +212,7 @@ exports.fetchFilesAndSizes = async function (rootPath, patterns = [], { core }) 
 
     // If no files are found, fail the action with a helpful message
     if (files.length === 0) {
-        debugEmptyDirectory(rootPath, patterns, { core });
+        debugEmptyDirectory(rootPath, patterns);
         return new Map();
     }
 
@@ -173,9 +224,167 @@ exports.fetchFilesAndSizes = async function (rootPath, patterns = [], { core }) 
             .map((f) => {
                 const relativePath = relative(rootPath, f);
                 const stat = statSync(f);
+                // Only report on files, not on directories
                 if (!stat || stat.isDirectory()) return;
-                return [relativePath, stat.size];
+
+                const content = readFileSync(f, { encoding: "utf-8" });
+                return [relativePath, {
+                    size: stat.size,
+                    content,
+                }];
             })
             .filter(Boolean),
     );
+};
+
+/**
+ * Split out the data indexed by filename into groups by component
+ * @param {Map<string, FileData>} dataMap
+ * @param {string} path
+ * @param {Map<string, FileData>} baseMap
+ * @returns {{ filePath: string, PACKAGES: Map<string, Map<string, FileData>>}}
+ */
+exports.splitDataByPackage = function (dataMap, path, baseMap = new Map()) {
+	const PACKAGES = new Map();
+
+	let filePath;
+	[...dataMap.entries()].forEach(([file, headData]) => {
+		// Determine the name of the component
+		const parts = file.split(sep);
+		const componentIdx = parts.findIndex((part) => part === "dist") - 1;
+		const packageName = parts[componentIdx];
+
+		if (!filePath) {
+			filePath = `${file.replace(path, "")}/${parts.slice(componentIdx + 1, -1).join(sep)}`;
+		}
+
+		const readableFilename = file.replace(/^.*\/dist\//, "");
+
+		const fileMap = PACKAGES.has(packageName)
+			? PACKAGES.get(packageName)
+			: new Map();
+
+        if (!fileMap.has(readableFilename)) {
+			fileMap.set(readableFilename, {
+                headData,
+                baseData: baseMap.get(file),
+			});
+		}
+
+		/** Update the component's table data */
+		PACKAGES.set(packageName, fileMap);
+	});
+
+	// Look for any base files not present in the head
+	[...baseMap.entries()].forEach(([file, baseByteSize]) => {
+		// Determine the name of the component
+		const parts = file.split(sep);
+		const componentIdx = parts.findIndex((part) => part === "dist") - 1;
+		const packageName = parts[componentIdx];
+
+		if (!filePath) {
+			filePath = `${file.replace(path, "")}/${parts.slice(componentIdx + 1, -1).join(sep)}`;
+		}
+
+		const readableFilename = file.replace(/^.*\/dist\//, "");
+
+		const fileMap = PACKAGES.has(packageName)
+			? PACKAGES.get(packageName)
+			: new Map();
+
+		if (!fileMap.has(readableFilename)) {
+			fileMap.set(readableFilename, {
+				headByteSize: dataMap.get(file),
+				baseByteSize,
+			});
+		}
+
+		/** Update the component's table data */
+		PACKAGES.set(packageName, fileMap);
+	});
+
+	return { filePath, PACKAGES };
+};
+
+/**
+ * Sort the provided table into a printable dataset
+ * @param {Map<string, Map<string, FileData>>} PACKAGES
+ * @param {string} filePath - The path to the component's dist folder from the root of the repo
+ * @param {string} path - The path from the github workspace to the root of the repo
+ * @returns {Array<{ name: string, filePath: string, hasChange: boolean, fileMap: Map<string, FileData>}>}
+ */
+exports.makeTable = function (PACKAGES, filePath, path) {
+	const sections = [];
+
+	/** Next convert that component data into a detailed object for reporting */
+	PACKAGES.forEach((fileMap, packageName) => {
+		// Read in the main asset file from the package.json
+		const packagePath = join(path, filePath, packageName, "package.json");
+
+		let mainFile;
+		if (existsSync(packagePath)) {
+			const { main } = require(packagePath) ?? {};
+			if (main) {
+				mainFile = main.replace(/^.*\/dist\//, "");
+
+				// Check if a minified output of this file exists
+				if (existsSync(join(dirname(packagePath), main.replace(/\.css$/, ".min.css")))) {
+					mainFile = mainFile.replace(/\.css$/, ".min.css");
+				}
+			}
+		}
+
+		const hasChange = fileMap.size > 0 && [...fileMap.values()].some(({ headByteSize, baseByteSize }) => headByteSize !== baseByteSize);
+
+		/**
+		 * We don't need to report on components that haven't changed unless they're new or removed
+		 */
+		// if (headMainSize === baseMainSize) return;
+
+		sections.push({
+			name: packageName,
+			filePath,
+			hasChange,
+			mainFile,
+			fileMap
+		});
+	});
+
+	return sections;
+};
+
+/**
+ * Read in file content from head and base, determine if the content has any differences
+ * returns false if the content is the same, else return the diff object
+ * @param {string} filePath - The path to the component's dist folder from the root of the repo
+ * @param {Object} options
+ * @param {string} options.headPath
+ * @param {string} options.basePath
+ * @returns {boolean|import("diff").Change[]|Error}
+ */
+exports.fetchDiff = async function (filepath, { headPath, basePath }) {
+    const filetype = extname(filepath);
+
+    let headContent;
+    let baseContent;
+
+    // Read in the file content in parallel
+    await Promise.all([
+        await readFile(join(headPath, filepath)).then(content => headContent = content),
+        await readFile(join(basePath, filepath)).then(content => baseContent = content),
+    ]).catch(err => {
+        return err;
+    });
+
+    let diff;
+    if (filetype === ".css") {
+        diff = Diff.diffCss(baseContent, headContent);
+    } else if (filetype === ".json") {
+        diff = Diff.diffJson(baseContent, headContent);
+    } else {
+        diff = Diff.diffWordsWithSpace(baseContent, headContent);
+    }
+
+    if (diff.length === 0) return false;
+    else return Diff.createPatch(filepath, baseContent, headContent);
 };
