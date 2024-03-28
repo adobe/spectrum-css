8ec900822dcd579a17691a7e1ecbdb88c442b93e
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2024-03-28 13:12:20 -0600

    feat(cornerrounding): add custom token and foundations documentation (#2559)
    * feat(cornerrounding): documentation, custom tokens
    
    - feat(cornerrounding): add custom token
    - chore(cornerrounding): adds dist
    - docs(cornerrounding): add file strucutre for foundations
    - feat(cornerrounding): override token value
      - Sets custom var to override token spectrum-corner-radius-full
    - chore(cornerrounding): add override to dist
      - Adds custom var to override token value to dist
    - chore(cornerrounding): adjust file structure
      - Also renames file
    - feat(cornerrounding): add Foundations story for corner-rounding
    - feat(cornerrounding): add action buttons and checkboxes
      - Adds action buttons and checkboxes stories to demonstrate corner rounding
    - docs(cornerrounding): update corner-rounding docs
      - better utilize storybook features
      - clean up checkbox and action button stories
      - hide stories in corner-rounding directory
      - improve table styling and css token use
      - improve corner rounding documentation
      - add tables for component size tokens
    - fix(cornerrounding): remove mdx for tables
    - fix(cornerrounding): use sentence case
    - feat(cornerrounding): use custom token on close button
    - docs: add alias note
    - docs: remove reference to non-alias tokens
    - feat(actionbutton): partial migration for corner rounding only
    - feat(checkbox): partial migration for corner rounding only
    - chore(closebutton): update custom var post-refactor
    - fix: token rebase issue
    - chore(actionbutton,checkbox): update package version
    - chore: use corner-radius alias token
    - chore: update tokens version
    - chore: revert unneeded component version change
    - docs: design requested updates, show token with example
    - docs: move tables to stories, inline spacing for tables
    
    * fix: includes error
    
    ---------
    
    Co-authored-by: Melissa Thompson <mthompson@heysparkbox.com>

---
+++ .storybook/foundations/corner-rounding/action-button-corner-rounding.stories.js
@@ -0,0 +1,103 @@
+import { html } from "lit";
+import { Template } from "../../../components/actionbutton/stories/template";
+
+export default {
+	title: "Corner rounding",
+	description:
+		"The action button component represents an action a user can take.",
+	component: "ActionButton",
+	args: {
+		rootClass: "spectrum-ActionButton",
+	},
+	parameters: {
+		actions: {
+			handles: ["click .spectrum-ActionButton:not([disabled])"],
+		},
+	},
+	tags: ['foundation'],
+};
+
+const ActionButton = (args, context) => {
+	return html`
+		<div style="padding: 1rem 0;">
+			${Template({
+				...args,
+				iconName: undefined,
+			}, context)}
+		</div>
+	`;
+};
+
+const ActionButtonTable = (args, context) => {
+	return html`
+		<table class="spectrum-Foundations-Example-CornerRounding-table">
+			<thead>
+				<tr>
+					<th scope="col">Token</th>
+					<th scope="col" style="padding: 0 2rem;">Value</th>
+					<th scope="col">Medium example</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>--spectrum-corner-radius-medium-size-extra-small</td>
+					<td style="padding: 0 2rem;">6px</td>
+					<td>
+						${ActionButton({
+							...args,
+							label: "Extra Small",
+							size: "xs"
+						}, context)}
+					</td>
+				</tr>
+				<tr>
+					<td>--spectrum-corner-radius-medium-size-small</td>
+					<td style="padding: 0 2rem;">7px</td>
+					<td>
+						${ActionButton({
+							...args,
+							label: "Small",
+							size: "s"
+						}, context)}
+					</td>
+				</tr>
+				<tr>
+					<td>--spectrum-corner-radius-medium-size-medium</td>
+					<td style="padding: 0 2rem;">8px</td>
+					<td>
+						${ActionButton({
+							...args,
+							label: "Medium",
+							size: "m"
+						}, context)}
+					</td>
+				</tr>
+				<tr>
+					<td>--spectrum-corner-radius-medium-size-large</td>
+					<td style="padding: 0 2rem;">9px</td>
+					<td>
+						${ActionButton({
+							...args,
+							label: "Large",
+							size: "l"
+						}, context)}
+					</td>
+				</tr>
+				<tr>
+					<td>--spectrum-corner-radius-medium-size-extra-large</td>
+					<td style="padding: 0 2rem;">10px</td>
+					<td>
+						${ActionButton({
+							...args,
+							label: "Extra Large",
+							size: "xl"
+						}, context)}
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	`;
+};
+
+export const ActionButtonExamples = ActionButtonTable.bind({});
+ActionButtonExamples.args = {};
