3d4292e1b748c0feb9cbabb74fce43fba7933a74
Jennifer Grenier Diaz <jenn.diaz@heysparkbox.com>
2024-03-28 13:12:20 -0600

    feat(cornerrounding): add custom token and foundations documentation (#2559)
    * feat(cornerrounding): documentation, custom tokens
    
    - feat(cornerrounding): add custom token
    - chore(cornerrounding): adds dist
    - docs(cornerrounding): add file strucutre for foundations
    - feat(cornerrounding): override token value
      - Sets custom var to override token spectrum-corner-radius-full
    - chore(cornerrounding): add override to dist
      - Adds custom var to override token value to dist
    - chore(cornerrounding): adjust file structure
      - Also renames file
    - feat(cornerrounding): add Foundations story for corner-rounding
    - feat(cornerrounding): add action buttons and checkboxes
      - Adds action buttons and checkboxes stories to demonstrate corner rounding
    - docs(cornerrounding): update corner-rounding docs
      - better utilize storybook features
      - clean up checkbox and action button stories
      - hide stories in corner-rounding directory
      - improve table styling and css token use
      - improve corner rounding documentation
      - add tables for component size tokens
    - fix(cornerrounding): remove mdx for tables
    - fix(cornerrounding): use sentence case
    - feat(cornerrounding): use custom token on close button
    - docs: add alias note
    - docs: remove reference to non-alias tokens
    - feat(actionbutton): partial migration for corner rounding only
    - feat(checkbox): partial migration for corner rounding only
    - chore(closebutton): update custom var post-refactor
    - fix: token rebase issue
    - chore(actionbutton,checkbox): update package version
    - chore: use corner-radius alias token
    - chore: update tokens version
    - chore: revert unneeded component version change
    - docs: design requested updates, show token with example
    - docs: move tables to stories, inline spacing for tables
    
    * fix: includes error
    
    ---------
    
    Co-authored-by: Melissa Thompson <mthompson@heysparkbox.com>

--- .storybook/foundations/down-state/checkbox-down-state.stories.js
+++ .storybook/foundations/down-state/checkbox-down-state.stories.js
@@ -1,7 +1,7 @@
 import { Template } from "../../../components/checkbox/stories/template";
 
 export default {
-	title: "Foundations/Down state",
+	title: "Down state",
 	description:
 		"Checkboxes allow users to select multiple items from a list of individual items, or mark one individual item as selected.",
 	component: "Checkbox",
