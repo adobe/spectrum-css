f2251c72aa735ec79300fef146dcae7272981d4b
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-04-03 12:30:47 -0400

    feat(alertbanner): start migration to spectrum two
    Migrates the component to spectrum two.
    - Theme files are removed, and their custom properties integrated into
      the main index.css.
    - Renames misspelled mod custom property (noted in docs)
    - Simplifies background color styles by changing a single custom
      property per variant, as done on other components.

--- components/alertbanner/stories/alertbanner.stories.js
+++ components/alertbanner/stories/alertbanner.stories.js
@@ -1,6 +1,6 @@
 import { html } from "lit";
 import { styleMap } from "lit/directives/style-map.js";
-
+import { when } from "lit/directives/when.js";
 import { Template } from "./template";
 
 /**
@@ -9,9 +9,11 @@ import { Template } from "./template";
 export default {
 	title: "Alert banner",
 	component: "AlertBanner",
+	subtitle: "Testing",
 	argTypes: {
 		isOpen: {
 			name: "Open",
+			description: "Determines whether the banner is visible or hidden.",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
@@ -21,39 +23,42 @@ export default {
 		},
 		text: {
 			name: "Text",
+			description: "Text of the message displayed within the alert banner.",
 			type: { name: "string", required: true },
 			table: {
 				type: { summary: "string" },
 				disable: false,
-				category: "Component",
+				category: "Content",
 			},
 			control: { type: "text" },
 		},
 		variant: {
-			name: "Background color variants",
+			name: "Semantic variants",
 			type: { name: "string" },
 			table: {
 				type: { summary: "string" },
 				category: "Component",
+				defaultValue: { summary: "neutral" },
 			},
 			options: ["neutral", "info", "negative"],
 			control: "radio",
 		},
-		hasActionButton: {
-			name: "Display action button",
-			type: { name: "boolean" },
+		actionButtonText: {
+			name: "Action button text",
+			description: "Label text for the optional action button. When empty, the action button does not display.",
+			type: { name: "string" },
 			table: {
-				type: { summary: "boolean" },
-				category: "State",
+				type: { summary: "string" },
+				category: "Content",
 			},
-			control: "boolean",
+			control: "text",
 		},
 	},
 	args: {
 		rootClass: "spectrum-AlertBanner",
 		isOpen: true,
 		variant: "neutral",
-		hasActionButton: true,
+		actionButtonText: "Action",
 		text: "Your trial has expired",
 	},
 	parameters: {
@@ -66,34 +71,31 @@ export default {
 	},
 };
 
-const AlertBannerGroup = (args) => html`
-	<div
-		style=${styleMap({
-			"display": "flex",
-			"flex-direction": "column",
-			"gap": "16px",
-		})}
-	>
-		${Template({
-			...args,
-		})}
-		${window.isChromatic() ?
-		Template({
-			...args,
-			hasActionButton: true,
-			variant: "info",
-			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purcahsed the software."
-		}): null }
-		${window.isChromatic() ?
-				Template({
+const AlertBannerGroup = (args) => {
+	return html`
+		<div
+			style=${styleMap({
+				"display": "flex",
+				"flex-direction": "column",
+				"gap": "16px",
+			})}
+		>
+			${Template(args)}
+			${when(window.isChromatic(), () => html`
+				${Template({
 					...args,
-			hasActionButton: true,
-			variant: "negative",
-			text: "Connection interupted. Check your network to continue."
-		})
-		: null }
-	</div>
-`;
+					variant: "info",
+					text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purcahsed the software."
+				})}
+				${Template({
+					...args,
+					variant: "negative",
+					text: "Connection interupted. Check your network to continue."
+				})}
+			`)}
+		</div>
+	`;
+};
 
 export const Default = AlertBannerGroup.bind({});
 Default.args = {};
