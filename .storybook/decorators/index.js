e3f89746d0a9e76ddbb42d163de45d5cb7380364
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-04-30 17:32:00 -0400

    fix: remove custom storybook keyboard shortcuts for text direction
    When pressing the "r" or "n" key, the document direction would change
    due to this custom keydown code. This was causing problems such as
    typing into a text input and the document direction would flip if you
    typed anything with the letter "r". This also conflicted with other
    shortcuts, such as Cmd+Shift+R to reload the page.
    
    The text direction selector is available as a toolbar control and it is
    not a repeatedly used feature that requires a custom keyboard shortcut
    that can cause conflicts with existing keyboard shortcuts.

--- .storybook/decorators/index.js
+++ .storybook/decorators/index.js
@@ -6,27 +6,15 @@ export { withTestingPreviewWrapper } from "./withTestingPreviewWrapper.js";
 
 /**
  * @type import('@storybook/csf').DecoratorFunction<import('@storybook/web-components').WebComponentsFramework>
- * @description Rendered as controls; these properties are assigned to the document root element
+ * @description Sets the text direction of the document, using the global set with a toolbar control. These properties are assigned to the document root element.
  **/
 export const withTextDirectionWrapper = makeDecorator({
 	name: "withTextDirectionWrapper",
 	parameterName: "textDecoration",
 	wrapper: (StoryFn, context) => {
 		const { globals, parameters } = context;
-    const defaultDirection = "ltr"
+		const defaultDirection = "ltr"
 		const textDirection = parameters.textDirection || globals.textDirection || defaultDirection;
-
-		// Shortkeys for the global types
-		document.addEventListener("keydown", (e) => {
-			switch (e.key || e.keyCode) {
-				case "r":
-					document.documentElement.dir = "rtl";
-					break;
-				case "n":
-					document.documentElement.dir = defaultDirection;
-					break;
-			}
-		});
 
 		useEffect(() => {
 			if (textDirection) document.documentElement.dir = textDirection;
