c6a6f6016858460e17cdea0c152e400da5802983
Melissa Thompson <mthompson@heysparkbox.com>
2024-05-01 18:17:21 -0400

    feat(helptext): s2 migration

---
+++ .changeset/great-cows-cheat.md
@@ -0,0 +1,7 @@
+---
+"@spectrum-css/helptext": major
+---
+
+feat(helptext): s2 migration
+
+Help text now uses Spectrum 2 tokens and specifications. This includes updates to heights and spacing, icons and icon sizes, colors, and fonts.
--- components/helptext/stories/helptext.stories.js
+++ components/helptext/stories/helptext.stories.js
@@ -1,4 +1,7 @@
-// Import the component markup template
+import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
+import { html } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
+import { when } from "lit/directives/when.js";
 import { Template } from "./template";
 
 export default {
@@ -85,16 +88,166 @@ export default {
 			type: "migrated",
 		},
 	},
+	decorators: [
+		(Story, context) => html`
+			<style>
+				.spectrum-Detail { display: inline-block; }
+				.spectrum-Typography > div {
+					border: 1px solid var(--spectrum-gray-200);
+					border-radius: 4px;
+					padding: 0 1em 1em;
+					/* Why seafoam? Because it separates it from the component styles. */
+					--mod-detail-font-color: var(--spectrum-seafoam-900);
+				}
+			</style>
+			<div
+				style=${styleMap({
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "flex-start",
+					gap: "1rem",
+					"--mod-detail-margin-end": ".3rem",
+				})}
+			>
+				${Story(context)}
+			</div>
+		`,
+	],
 };
 
-export const Default = Template.bind({});
-Default.storyName = "Neutral";
+const States = (args) =>
+	html` <div>
+			${Typography({
+				semantics: "detail",
+				size: "s",
+				content: ["Default"],
+			})}
+			${Template(args)}
+		</div>
+		${when(args.variant === "negative", () => html`
+			<div>
+				${Typography({
+					semantics: "detail",
+					size: "s",
+					content: ["Option for No Icon"],
+				})}
+				${Template({
+					...args,
+					variant: "negative",
+					hideIcon: true,
+				})}
+			</div>
+		`)}
+		<div>
+			${Typography({
+				semantics: "detail",
+				size: "s",
+				content: ["Disabled"],
+			})}
+			${Template({
+				...args,
+				isDisabled: true,
+			})}
+		</div>
+	`;
+
+const Sizes = (args) =>
+	html` ${["s", "m", "l", "xl"].map((size) => {
+		return html` <div>
+			${Typography({
+				semantics: "detail",
+				size: "s",
+				content: [
+					{
+						s: "Small",
+						m: "Medium",
+						l: "Large",
+						xl: "Extra-large",
+					}[size],
+				],
+			})}
+			${Template({
+				...args,
+				size,
+			})}
+		</div>`;
+	})}`;
+
+const Variants = (args) =>
+	html` ${window.isChromatic()
+		? html` <div class="spectrum-Typography">
+					${Typography({
+						semantics: "detail",
+						size: "l",
+						content: ["Neutral"],
+					})}
+					<div
+						style=${styleMap({
+							display: "flex",
+							flexDirection: "column",
+							gap: ".3rem",
+						})}
+					>
+						${States(args)}
+					</div>
+				</div>
+				<div class="spectrum-Typography">
+					${Typography({
+						semantics: "detail",
+						size: "l",
+						content: ["Negative"],
+					})}
+					<div
+						style=${styleMap({
+							display: "flex",
+							flexDirection: "column",
+							gap: ".3rem",
+						})}
+					>
+						${States({
+							...args,
+							variant: "negative",
+							text: "This is an example with wrapping text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+							customStyles: {"max-width": "350px"},
+						})}
+					</div>
+				</div>
+				<div class="spectrum-Typography">
+					${Typography({
+						semantics: "detail",
+						size: "l",
+						content: ["Sizing"],
+					})}
+					<div
+						style=${styleMap({
+							display: "flex",
+							flexDirection: "column",
+							gap: ".3rem",
+						})}
+					>
+						${Sizes(args)}
+					</div>
+				</div>
+				<div class="spectrum-Typography">
+					${Typography({
+						semantics: "detail",
+						size: "l",
+						content: ["Sizing - Negative"],
+					})}
+					<div
+						style=${styleMap({
+							display: "flex",
+							flexDirection: "column",
+							gap: ".3rem",
+						})}
+					>
+						${Sizes({
+							...args,
+							variant: "negative",
+						})}
+					</div>
+				</div>`
+		: Template(args)}`;
+
+export const Default = Variants.bind({});
 Default.args = {};
-
-export const Negative = Template.bind({});
-Negative.storyName = "Negative";
-Negative.args = {
-	variant: "negative",
-	text: "This is an example with wrapping text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	customStyles: {"max-width": "350px"},
-};
