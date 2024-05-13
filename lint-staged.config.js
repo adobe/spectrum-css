37f5a0b4e41277c1e909b27338d70ec34e4afa24
castastrophe <castastrophe@users.noreply.github.com>
2024-05-13 18:07:04 -0400

    chore: fix loglevel error message when running prettier commands

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"components/*/*.css": [
-		"prettier --no-error-on-unmatched-pattern --ignore-unknown --loglevel silent --write",
+		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write",
 		"stylelint --fix --cache --allow-empty-input --quiet"
 	],
 	"*.json": [
@@ -13,15 +13,15 @@ module.exports = {
 		"eslint --fix --cache --no-error-on-unmatched-pattern --quiet"
 	],
 	"dist/*.css": [
-		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --loglevel silent --write"
+		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write"
 	],
 	"components/*/metadata/*.{yml,yaml}": (files) => {
 		return [
 			...(files.map(file => `pajv test --valid -s ./schemas/documentation.schema.json -d "${file}"`) ?? []),
-            `prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --loglevel silent --write ${files.join(" ")}`
+            `prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write ${files.join(" ")}`
 		];
 	},
 	"*.md": [
-		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --loglevel silent --write"
+		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write"
 	]
 };
