8efab7a53d45f9da0e421b11c69dc3a706e4d2f0
Melissa Thompson <mthompson@heysparkbox.com>
2024-05-13 13:02:26 -0400

    docs(popover): increase chromatic coverage for tip position

--- components/popover/stories/popover.stories.js
+++ components/popover/stories/popover.stories.js
@@ -1,7 +1,9 @@
+import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
 import { userEvent, within } from "@storybook/testing-library";
 import { html } from "lit";
+import { when } from "lit/directives/when.js";
 
-import { Template } from "./template";
+import { SourcelessTemplate, Template } from "./template";
 
 import { Template as ActionButton } from "@spectrum-css/actionbutton/stories/template.js";
 import { Template as Menu } from "@spectrum-css/menu/stories/template.js";
@@ -125,9 +127,56 @@ export default {
 		},
 	},
 	decorators: [
-		(Story, context) => html`<div style="padding: 16px">${Story(context)}</div>`
+		(Story, context) => html`
+			<style>
+				.spectrum-Detail { display: inline-block; }
+				.spectrum-Typography > div {
+					/* Why seafoam? Because it separates it from the component styles. */
+					--mod-detail-font-color: var(--spectrum-seafoam-900);
+				}
+			</style>
+			<div style="padding: 16px">${Story(context)}</div>
+		`,
 	],
 };
+
+
+const ChromaticTipPlacementVariants = (args) => html`
+	${placementOptions.map(option => {
+		const optionDescription = () => {
+			if (option.startsWith("start") || option.startsWith("end"))
+				return "Changes side with text direction (like a logical property)";
+			if (option.startsWith("left") || option.startsWith("right"))
+				return "Text direction does not effect the position";
+			return null;
+		};
+
+		return html`
+			<div class="spectrum-Typography" style="padding-bottom: 12rem;">
+				${Typography({
+					semantics: "detail",
+					size: "l",
+					content: [`${option}`],
+				})}
+				<div>
+					${when(optionDescription() !== null, () => html`
+						${Typography({
+							semantics: "detail",
+							size: "s",
+							content: [`${optionDescription()}`],
+						})}
+					`)}
+					</div>
+				<div style="padding-top: 2rem">
+					${SourcelessTemplate({
+						...args,
+						position: option,
+					})}
+				</div>
+			</div>
+		`;
+	})}
+`;
 
 export const Default = Template.bind({});
 Default.play = async ({ canvasElement }) => {
@@ -136,7 +185,9 @@ Default.play = async ({ canvasElement }) => {
 };
 Default.args = {};
 
-export const WithTip = Template.bind({});
+export const WithTip = (args) => html`
+	${window.isChromatic() ? ChromaticTipPlacementVariants(args) : Template(args)}
+`;
 WithTip.play = async ({ canvasElement }) => {
 	const canvas = within(canvasElement);
 	await userEvent.click(canvas.getByRole("button"));
