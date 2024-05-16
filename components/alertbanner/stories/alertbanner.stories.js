8449f42ef3ba7376f7addf7130a352e8f6ceeb7b
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-05-16 17:07:53 -0400

    docs(alertbanner): custom storybook docs page and template updates
    Creates a custom MDX docs page in Storybook for the Alert banner
    component. And reworks the stories and chromatic-only templates a little
    to facilitate and increase coverage.
    
    Includes Storybook VRT coverage for the max width changes in PR #2762 .
    Tests the display of the alert banner when in a container larger than
    and smaller than its max-inline-size.

--- components/alertbanner/stories/alertbanner.stories.js
+++ components/alertbanner/stories/alertbanner.stories.js
@@ -5,7 +5,7 @@ import { Template as Typography } from "@spectrum-css/typography/stories/templat
 import { Template } from "./template";
 
 /**
- * The alert banner show pressing and high-signal messages, such as system alerts. They're meant to be noticed and prompt users to take action.
+ * The alert banner shows pressing and high-signal messages, such as system alerts. It is meant to be noticed and prompt users to take action.
  */
 export default {
 	title: "Alert banner",
@@ -84,9 +84,8 @@ export default {
 				style=${styleMap({
 					display: "flex",
 					flexDirection: "column",
-					alignItems: "flex-start",
 					gap: "1.0rem",
-					"--mod-detail-margin-end": ".3rem",
+					"--mod-detail-margin-end": "5px",
 				})}
 			>
 				${Story(context)}
@@ -96,43 +95,68 @@ export default {
 };
 
 const AlertBannerGroup = (args) => {
+	// Wrapper styles for testing that it resizes to fit its container width, up until its max width. 
+	const largerThanMaxWidth = { inlineSize: "calc(var(--spectrum-alert-banner-width) + 100px)" };
+	const smallerThanMaxWidth = { inlineSize: "calc(var(--spectrum-alert-banner-width) - 200px)" };
+
 	return !window.isChromatic() ? Template(args) : html`
-		<div class="spectrum-Typography">
-			${Typography({
-				semantics: "detail",
-				size: "l",
-				content: ["All variants with button, including wrapping"],
-			})}
-			<div
-				style=${styleMap({
-					display: "flex",
-					flexDirection: "column",
-					gap: "12px",
-				})}
+		${["neutral", "info", "negative"].map((variant) => html`
+			<div 
+				class="spectrum-Typography"
+				style=${styleMap(largerThanMaxWidth)}
 			>
-				${Template(args)}
-				${Template({
-					...args,
-					variant: "info",
-					text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software.",
+				${Typography({
+					semantics: "detail",
+					size: "l",
+					content: [`Variant: ${variant}`],
 				})}
-				${Template({
-					...args,
-					variant: "negative",
-					text: "Connection interupted. Check your network to continue.",
-				})}
-				${Template({
-					...args,
-					isOpen: false,
-					text: "This alert banner should never be displayed in VRTs. This is to test that it is not displayed when there is no is-open class applied.",
-				})}
+				<div
+					style=${styleMap({
+						display: "flex",
+						flexDirection: "column",
+						gap: "12px",
+					})}
+				>
+					${Template({
+						...args,
+						variant,
+						text: "Example with short text and both buttons.",
+					})}
+					${Template({
+						...args,
+						variant,
+						text: "Example with wrapping text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+					})}
+					${Template({
+						...args,
+						variant,
+						actionButtonText: "",
+						text: "Example without the contextual action button.",
+					})}
+					${Template({
+						...args,
+						variant,
+						actionButtonText: "",
+						text: "Example without the contextual action button, and with wrapping text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+					})}
+					${Template({
+						...args,
+						isOpen: false,
+						variant,
+						text: "This alert banner should never be displayed in VRTs. This is to test that it is not displayed when there is no is-open class applied.",
+					})}
+				</div>
 			</div>
-		</div>
-		<div class="spectrum-Typography">
+		`)}
+
+		<div 
+			class="spectrum-Typography"
+			style=${styleMap(smallerThanMaxWidth)}
+		>
 			${Typography({
 				semantics: "detail",
 				size: "l",
-				content: ["All variants without button"],
+				content: ["Container smaller than max-width"],
 			})}
 			<div
 				style=${styleMap({
@@ -143,24 +167,116 @@ const AlertBannerGroup = (args) => {
 			>
 				${Template({
 					...args,
-					actionButtonText: "",
-				})}
-				${Template({
-					...args,
 					variant: "info",
 					text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software.",
-					actionButtonText: "",
-				})}
-				${Template({
-					...args,
-					variant: "negative",
-					text: "Connection interupted. Check your network to continue.",
-					actionButtonText: "",
 				})}
 			</div>
 		</div>
 	`;
 };
 
+/**
+ * Examples of all text overflow (wrapping) scenarios.
+ */
+const TextOverflowTemplate = (args) => html`
+	<div
+		style=${styleMap({
+			display: "flex",
+			flexDirection: "column",
+			gap: "12px",
+		})}
+	>
+		${Template({
+			...args,
+			variant: "info",
+			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software.",
+		})}
+		${Template({
+			...args,
+			variant: "neutral",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			actionButtonText: "",
+		})}
+	</div>
+`;
+
+/**
+ * Examples of options for contextual action button and close button.
+ */
+const ActionableOptionsTemplate = (args) => html`
+	<div
+		style=${styleMap({
+			display: "flex",
+			flexDirection: "column",
+			gap: "12px",
+		})}
+	>
+		${Template({
+			...args,
+			variant: "neutral",
+			text: "Your trial has expired.",
+			actionButtonText: "Buy now",
+		})}
+		${Template({
+			...args,
+			variant: "neutral",
+			text: "Your trial has expired",
+			actionButtonText: "",
+		})}
+	</div>
+`;
+
+/**
+ * Stories
+ */
 export const Default = AlertBannerGroup.bind({});
 Default.args = {};
+
+/**
+ * Stories for the MDX "Docs" only.
+ * Based off of the base `Template` which does not have conditional Chromatic-only markup.
+ */
+export const Neutral = Template.bind({});
+Neutral.tags = ["is-hidden-story"];
+Neutral.args = {
+	variant: "neutral",
+};
+Neutral.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const Informative = Template.bind({});
+Informative.tags = ["is-hidden-story"];
+Informative.args = {
+	variant: "info",
+};
+Informative.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const Negative = Template.bind({});
+Negative.tags = ["is-hidden-story"];
+Negative.args = {
+	variant: "negative",
+};
+Negative.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const ActionableOptions = ActionableOptionsTemplate.bind({});
+ActionableOptions.tags = ["is-hidden-story"];
+ActionableOptions.args = {
+	variant: "negative",
+};
+ActionableOptions.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const TextOverflow = TextOverflowTemplate.bind({});
+TextOverflow.tags = ["is-hidden-story"];
+TextOverflow.args = {
+	variant: "negative",
+};
+TextOverflow.parameters = {
+	chromatic: { disableSnapshot: true },
+};
