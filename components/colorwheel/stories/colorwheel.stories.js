bd481401de2bd2fc5651032e93f98fc747dd6d36
Melissa Thompson <mthompson@heysparkbox.com>
2024-05-24 11:52:27 -0400

    docs(colorwheel): site docs to storybook

--- components/colorwheel/stories/colorwheel.stories.js
+++ components/colorwheel/stories/colorwheel.stories.js
@@ -26,11 +26,21 @@ export default {
 			control: "boolean",
 			if: { arg: "isDisabled", truthy: false },
 		},
+		isWithColorArea: {
+			name: "With Color Area",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
 	},
 	args: {
 		rootClass: "spectrum-ColorWheel",
 		isDisabled: false,
 		isFocused: false,
+		isWithColorArea: false,
 	},
 	parameters: {
 		actions: {
@@ -44,3 +54,24 @@ export default {
 
 export const Default = Template.bind({});
 Default.args = {};
+
+/**
+ * Stories for the MDX "Docs" only.
+ */
+export const Disabled = Template.bind({});
+Disabled.args = {
+	isDisabled: true,
+};
+Disabled.tags = ["docs-only"];
+Disabled.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const WithColorArea = Template.bind({});
+WithColorArea.args = {
+	isWithColorArea: true,
+};
+WithColorArea.tags = ["docs-only"];
+WithColorArea.parameters = {
+	chromatic: { disableSnapshot: true },
+};
--- components/colorwheel/stories/template.js
+++ components/colorwheel/stories/template.js
@@ -1,6 +1,9 @@
-import { Template as ColorHandle } from "@spectrum-css/colorhandle/stories/template.js";
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
+import { when } from "lit/directives/when.js";
+
+import { Template as ColorArea } from "@spectrum-css/colorarea/stories/template.js";
+import { Template as ColorHandle } from "@spectrum-css/colorhandle/stories/template.js";
 
 import "../index.css";
 
@@ -9,6 +12,7 @@ export const Template = ({
 	customClasses = [],
 	isDisabled = false,
 	isFocused = false,
+	isWithColorArea = false,
 	colorHandleStyle = {
 		"--spectrum-picked-color": "rgb(255, 0, 0)",
 	},
@@ -21,7 +25,18 @@ export const Template = ({
 		...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
 	})}>
 		<div class="${rootClass}-inner">
-			<div class="${rootClass}-colorarea-container"></div>
+			<div class="${rootClass}-colorarea-container">
+			${when(isWithColorArea, () => html`
+				${ColorArea({
+					...globals,
+					isDisabled,
+					customStyles: {
+						"--mod-colorarea-width": "80px",
+						"--mod-colorarea-height": "80px",
+					},
+				})}
+			`)}
+			</div>
 		</div>
 		<div class=${classMap({
 			[`${rootClass}-border`]: true,
