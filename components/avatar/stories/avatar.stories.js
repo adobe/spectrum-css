255311ee0b2dc7f0497689700b32ebf53e77ab8e
Melissa Thompson <mthompson@heysparkbox.com>
2024-05-24 15:37:22 -0400

    docs(asset,assetcard,avatar,badge): site docs to storybook  (#2782)
    * docs(asset): site docs to storybook
    
    * docs(assetcard): site docs to storybook
    
    * docs(avatar): site docs to storybook
    
    * docs(badge): site docs to storybook
    
    * docs: show argtypes instead of controls on docs page
    
    * docs(avatar): different example avatar

--- components/avatar/stories/avatar.stories.js
+++ components/avatar/stories/avatar.stories.js
@@ -1,7 +1,18 @@
+import { html } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
+
+import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
 import { Template } from "./template";
 
+const sizeOptions = ["50", "75", "100", "200", "300", "400", "500", "600", "700"];
+
 /**
- * An image representing a user.
+ * An image representing a user. Note that a div wrapper is required for avatar:
+ * ```
+ * <div class="spectrum-Avatar spectrum-Avatar--size50">
+ *   <img class="spectrum-Avatar-image" src="img/example-ava.jpg" alt="Avatar">
+ * </div>
+ * ```
  */
 export default {
 	title: "Components/Avatar",
@@ -15,7 +26,7 @@ export default {
 				type: { summary: "string" },
 				category: "Component",
 			},
-			options: ["50", "75", "100", "200", "300", "400", "500", "600", "700"],
+			options: sizeOptions,
 			control: "select",
 		},
 		image: {
@@ -59,7 +70,7 @@ export default {
 	args: {
 		rootClass: "spectrum-Avatar",
 		size: "700",
-		image: "example-ava.png",
+		image: "example-ava@2x.png",
 		altText: "Avatar",
 		isDisabled: false,
 		hasLink: true,
@@ -73,3 +84,58 @@ export default {
 
 export const Default = Template.bind({});
 Default.args = {};
+
+/**
+ * Stories for the MDX "Docs" only.
+ */
+const AvatarSizes = (args) => html`
+	<div
+		style=${styleMap({
+			"display": "flex",
+			"gap": "16px",
+		})}
+	>
+		${sizeOptions.map((size) => (html`
+			<div
+				style=${styleMap({
+					"display": "flex",
+					"gap": "16px",
+					"flex-direction": "column",
+					"align-items": "center",
+				})}
+			>
+				${Template({...args, size})}
+				${Typography({
+					semantics: "detail",
+					size: "s",
+					content: [size],
+				})}
+			</div>
+		`))}
+	</div>
+`;
+
+export const SizeOptions = AvatarSizes.bind({});
+SizeOptions.tags = ["docs-only"];
+SizeOptions.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const NoLink = Template.bind({});
+NoLink.tags = ["docs-only"];
+NoLink.args = {
+	hasLink: false,
+};
+NoLink.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const Disabled = Template.bind({});
+Disabled.tags = ["docs-only"];
+Disabled.args = {
+	hasLink: false,
+	isDisabled: true,
+};
+Disabled.parameters = {
+	chromatic: { disableSnapshot: true },
+};
--- components/badge/stories/badge.stories.js
+++ components/badge/stories/badge.stories.js
@@ -1,10 +1,19 @@
 import { html } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
+
 import { Template } from "./template";
 
 import { default as IconStories } from "@spectrum-css/icon/stories/icon.stories.js";
 
+const semanticOptions = ["neutral", "accent", "informative", "positive", "negative"];
+const nonSemanticOptions = ["gray", "red", "orange", "yellow", "chartreuse", "celery", "green", "seafoam", "cyan", "blue", "indigo", "purple", "fuchsia", "magenta"];
+const fixedOptions = ["none", "fixed-inline-start", "fixed-inline-end", "fixed-block-start", "fixed-block-end"];
 /**
- * A badge element displays a small amount of color-categorized metadata; ideal for getting a user's attention.
+ * A badge element displays a small amount of color-categorized metadata; ideal for getting a user's attention. Some notes about badge:
+ * - Badge t-shirt sizes correspond to icon sizes
+ * - Label and icon elements must be nested inside a parent container of class .spectrum-Badge in order to achieve the correct layout and wrapping behavior.
+ * - Layout of Badge is applied with a display of `inline-flex`, allowing badge to display as inline while the child elements for the label and icon utilize flexbox for layout.
+ * - Fixed positioning impacts the border radius of the badge component
  */
 export default {
 	title: "Components/Badge",
@@ -50,13 +59,7 @@ export default {
 				type: { summary: "string" },
 				category: "Advanced",
 			},
-			options: [
-				"none",
-				"fixed-inline-start",
-				"fixed-inline-end",
-				"fixed-block-start",
-				"fixed-block-end",
-			],
+			options: fixedOptions,
 			control: "select",
 		},
 	},
@@ -94,9 +97,58 @@ const BadgeGroup = (args) => html`
 	${Template({
 		...args,
 		label: "24 days left in trial",
-		customStyles: { "max-inline-size": "100px" },
+		customStyles: { "max-inline-size": "120px" },
 	})}
 `;
 
 export const Default = BadgeGroup.bind({});
 Default.args = {};
+
+
+/**
+ * Stories for the MDX "Docs" only.
+ */
+const Variants = (args, variants) => html`
+	<div
+		style=${styleMap({
+			"display": "flex",
+			"gap": "16px",
+			"flex-wrap": "wrap"
+		})}
+	>
+		${variants.map((variant) => {
+			const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+			const label = capitalizeFirstLetter(variant);
+			return ( html`
+				<div
+					style=${styleMap({
+						"display": "flex",
+						"gap": "16px",
+						"flex-direction": "column",
+						"align-items": "center",
+					})}
+				>
+					${Template({...args, variant, label})}
+				</div>
+			`);
+	})}
+	</div>
+`;
+
+export const SemanticVariants = (args) => Variants(args, semanticOptions);
+SemanticVariants.tags = ["docs-only"];
+SemanticVariants.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const NonSemanticVariants = (args) => Variants(args, nonSemanticOptions);
+NonSemanticVariants.tags = ["docs-only"];
+NonSemanticVariants.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const FixedVariants = (args) => Variants(args, fixedOptions);
+FixedVariants.tags = ["docs-only"];
+FixedVariants.parameters = {
+	chromatic: { disableSnapshot: true },
+};
