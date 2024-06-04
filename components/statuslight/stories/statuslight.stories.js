a11ab69d4ad7d64b9132447fc131df03afa064f7
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-06-04 15:02:20 -0400

    feat(statuslight): s2 migration
    * refactor: remove --sizeM modifier and make "medium" default size
    
    * refactor:remove accent semantic variant
    
    * refactor: add/update new colors/tokens
    
    * refactor: update typography styles
    
    * refactor: generate new mod properties
    
    * docs: add migration notes/changes to yml
    
    * feat: add chromatic vr coverage
    
    * fix: high contrast changes (utilizes changes from PR#2771 to clean up
    legacy vendor prefixes)
    
    * docs: add changeset and update yml

--- components/statuslight/stories/statuslight.stories.js
+++ components/statuslight/stories/statuslight.stories.js
@@ -1,7 +1,13 @@
 import { html } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
+import { when } from "lit/directives/when.js";
+import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
 
 import { Template } from "./template";
 
+/**
+ * Status lights describe the condition of an entity. They can be used to convey semantic meaning, such as statuses and categories.
+ */
 export default {
 	title: "Components/Status light",
 	component: "Statuslight",
@@ -27,32 +33,37 @@ export default {
 		},
 		variant: {
 			name: "Variant",
+			description: "Changes the color of the status dot. The variant list includes both semantic and non-semantic options.",
 			type: { name: "string", required: true },
 			table: {
 				type: { summary: "string" },
 				category: "Component",
 			},
 			options: [
-				"accent",
 				"info",
 				"neutral",
 				"positive",
 				"notice",
 				"negative",
-				"gray",
-				"red",
-				"orange",
 				"yellow",
 				"chartreuse",
 				"celery",
-				"green",
 				"seafoam",
 				"cyan",
-				"blue",
 				"indigo",
 				"purple",
 				"fuchsia",
 				"magenta",
+				"gray",
+				"red",
+				"orange",
+				"green",
+				"blue",
+				"pink",
+				"turquoise",
+				"cinnamon",
+				"brown",
+				"silver",
 			],
 			control: "select",
 		},
@@ -71,22 +82,192 @@ export default {
 			type: "migrated",
 		},
 	},
+	decorators: [
+		(Story, context) => {
+			if (!window.isChromatic()) return Story(context);
+			return html`
+				<style>
+					.spectrum-Detail { display: inline-block; }
+					.spectrum-Typography > div {
+						border: 1px solid var(--spectrum-gray-200);
+						border-radius: 4px;
+						padding: 0 1em 1em;
+						/* Why seafoam? Because it separates it from the component styles. */
+						--mod-detail-font-color: var(--spectrum-seafoam-900);
+					}
+				</style>
+				<div
+					style=${styleMap({
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "flex-start",
+						gap: "1rem",
+						"--mod-detail-margin-end": ".3rem",
+					})}
+				>
+					${Story(context)}
+				</div>
+			`;
+		}
+	],
 };
 
-export const Default = (args) => html`
-	<div>
-		${Template({
-			...args
-		})}
+const Sizes = (args) =>
+	html` ${["s", "m", "l", "xl"].map((size) => {
+		return html`
+			<div>
+				${Typography({
+					semantics: "detail",
+					size: "s",
+					content: [
+						{
+							s: "Small",
+							m: "Medium",
+							l: "Large",
+							xl: "Extra-large",
+						}[size],
+					],
+				})}
+				${Template({
+					...args,
+					size
+				})}
+				${when(window.isChromatic(), () =>
+					Template({
+						...args,
+						size,
+						label: "Status light label that is long and wraps to the next line",
+						customStyles: {"max-width": "150px"}
+					})
+				)}
+			</div>
+		`;
+	})}`;
 
-		${
-			window.isChromatic() ?
-			Template({
+const SemanticColors = (args) =>
+	html` ${["info", "neutral", "positive", "notice", "negative",].map((variant) => {
+		return html`
+			<div>
+			${Typography({
+				semantics: "detail",
+				variant: "info",
+				content: [
+					{
+						info: "Informative",
+						neutral: "Neutral",
+						positive: "Positive",
+						notice: "Notice",
+						negative: "Negative",
+					}[variant],
+				],
+			})}
+			${Template({
 				...args,
-				label: "Status light label that is long and wraps to the next line",
-				customStyles: {"max-width": "150px"}
-			})
-		: null
-	}
-	</div>
-`;
+				variant,
+			})}
+			</div>
+		`;
+	})}`;
+
+const NonSemanticColors = (args) =>
+	html` ${[
+		"yellow",
+		"chartreuse",
+		"celery",
+		"seafoam",
+		"cyan",
+		"indigo",
+		"purple",
+		"fuchsia",
+		"magenta",
+		"gray",
+		"red",
+		"orange",
+		"green",
+		"blue",
+		"pink",
+		"turquoise",
+		"cinnamon",
+		"brown",
+		"silver",
+	].map((variant) => {
+		return html`
+		<div>
+			${Typography({
+				semantics: "detail",
+				variant: "yellow",
+				content: [
+					{
+						yellow: "Yellow",
+						chartreuse: "Chartreuse",
+						celery: "Celery",
+						seafoam: "Seafoam",
+						cyan: "Cyan",
+						indigo: "Indigo",
+						purple: "Purple",
+						fuchsia: "Fuchsia",
+						magenta: "Magenta",
+						gray: "Gray",
+						red: "Red",
+						orange: "Orange",
+						green: "Green",
+						blue: "Blue",
+						pink: "Pink",
+						turquoise: "Turquoise",
+						cinnamon: "Cinnamon",
+						brown: "Brown",
+						silver: "Silver",
+					}[variant],
+				],
+			})}
+			${Template({
+				...args,
+				variant,
+			})}
+			</div>
+		`;
+	})}`;
+
+const ChromaticVariants = (args) => {
+	const sectionData = [
+		{
+			sectionName: "Sizes",
+			componentMarkup: Sizes({
+				...args,
+			}),
+		},
+		{
+			sectionName: "Semantic colors",
+			componentMarkup: SemanticColors({
+				...args,
+			}),
+		},
+		{
+			sectionName: "Non-semantic colors",
+			componentMarkup: NonSemanticColors({
+				...args,
+			}),
+		},
+	];
+
+	return sectionData.map((data) => html`
+		<div class="spectrum-Typography">
+			${Typography({
+				semantics: "detail",
+				size: "l",
+				content: [data.sectionName],
+			})}
+			<div
+				style=${styleMap({
+					display: "grid",
+					gap: "1.5rem",
+					gridTemplateColumns: "repeat(4, 1fr)",
+				})}
+			>
+				${data.componentMarkup}
+			</div>
+		</div>
+	`);
+};
+
+export const Default = (args) => window.isChromatic() ? ChromaticVariants(args) : Template(args);
