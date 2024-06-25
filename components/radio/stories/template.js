0607e41c9aaa6e7a0838d40f8f6a5af76353c8e4
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-06-25 12:40:23 -0400

    docs(radio): migrate docs to storybook
    Migrate docs page variants and relevant notes to Storybook Docs, for the
    Radio component.
    
    Separates disabled, read-only, and emphasized options out into their own
    Docs stories for the user's view, as single radios would not have those
    options mixed in a group of radios.

--- components/radio/stories/template.js
+++ components/radio/stories/template.js
@@ -1,4 +1,4 @@
-import { getRandomId } from "@spectrum-css/preview/decorators";
+import { Container, getRandomId } from "@spectrum-css/preview/decorators";
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
@@ -21,6 +21,12 @@ export const Template = ({
 } = {}, context = {}) => {
 	const { updateArgs } = context;
 
+	// Create a unique ID for the input field and its associated label.
+	// Tack onto any existing id or name arg being set.
+	const inputId = typeof id !== "undefined" ? id += "-input"
+		: typeof name !== "undefined" ? id = name + "-input"
+			: "radio-0";
+
 	return html`
 		<div
 			class=${classMap({
@@ -38,8 +44,7 @@ export const Template = ({
 				type="radio"
 				name=${name}
 				class="${rootClass}-input"
-				id="radio-0"
-				?readOnly=${isReadOnly}
+				id=${inputId}
 				?checked=${isChecked}
 				?disabled=${isDisabled}
 				@change=${function() {
@@ -48,9 +53,39 @@ export const Template = ({
 				}}
 			/>
 			<span class="${rootClass}-button ${rootClass}-button--sizeS"></span>
-			<label class="${rootClass}-label ${rootClass}-label--sizeS" for="radio-0"
+			<label class="${rootClass}-label ${rootClass}-label--sizeS" for=${inputId}
 				>${label}</label
 			>
 		</div>
 	`;
 };
+
+/**
+ * Displays two radios; one unselected, and one selected.
+ */
+export const BasicGroupTemplate = (args, context) => Container({
+	withBorder: false,
+	direction: "column",
+	wrapperStyles: {
+		rowGap: "0px",
+		alignItems: "flex-start",
+	},
+	content: html`
+		${Template({
+			...args,
+			label: "Example label",
+			id: "radio-1-" + (args?.id ?? "default"),
+			name: "radio-example-" + (args?.name ?? "default"),
+		}, context)}
+		${Template({
+			...args,
+			isChecked: true,
+			label: "Initially selected radio button that has wrapping label text",
+			customStyles: {
+				"max-width": "220px",
+			},
+			id: "radio-2-" + (args?.id ?? "default"),
+			name: "radio-example-" + (args?.name ?? "default"),
+		}, context)}
+	`,
+});
