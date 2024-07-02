9d7fbf7db3664ab6bf47695c6a2b1219df4f669c
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-07-02 10:48:48 -0400

    docs(link): add missing variants

--- components/link/stories/link.stories.js
+++ components/link/stories/link.stories.js
@@ -3,7 +3,7 @@ import { version } from "../package.json";
 import { Template } from "./template";
 
 /**
- * A link allow users to navigate to a different location. They can be presented in-line inside a paragraph or as a standalone text.
+ * A link allows users to navigate to a different location. They can be presented in-line inside a paragraph or as standalone text.
  */
 export default {
 	title: "Link",
@@ -16,7 +16,7 @@ export default {
 				type: { summary: "string" },
 				category: "Content",
 			},
-			control: "url",
+			control: "text",
 		},
 		text: {
 			name: "Text",
@@ -34,7 +34,7 @@ export default {
 				type: { summary: "string" },
 				category: "Component",
 			},
-			options: ["secondary"],
+			options: ["primary", "secondary"],
 			control: "select",
 		},
 		staticColor: {
@@ -60,6 +60,7 @@ export default {
 	args: {
 		rootClass: "spectrum-Link",
 		isQuiet: false,
+		variant: "primary",
 	},
 	parameters: {
 		actions: {
@@ -70,10 +71,45 @@ export default {
 };
 
 export const Default = Template.bind({});
+Default.storyName = "Primary";
 Default.args = {
 	url: "https://www.adobe.com",
 	text: "Learn more about Adobe",
 };
+
+// ********* DOCS ONLY ********* //
+export const Secondary = Template.bind({});
+Secondary.args = {
+	variant: "secondary",
+	text: "Link using spectrum-Link--secondary"
+};
+Secondary.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+Secondary.tags = ["autodocs", "!dev"];
+
+export const QuietPrimary = Template.bind({});
+QuietPrimary.storyName = "Quiet (Primary)";
+QuietPrimary.args = {
+	isQuiet: true,
+	text: "Link using spectrum-Link--quiet"
+};
+QuietPrimary.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+QuietPrimary.tags = ["autodocs", "!dev"];
+
+export const QuietSecondary = Template.bind({});
+QuietSecondary.storyName = "Quiet (Secondary)";
+QuietSecondary.args = {
+	isQuiet: true,
+	variant: "secondary",
+	text: "Link using spectrum-Link--quiet and spectrum-Link--secondary"
+};
+QuietSecondary.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+QuietSecondary.tags = ["autodocs", "!dev"];
 
 // ********* VRT ONLY ********* //
 export const WithForcedColors = Default.bind({});
@@ -85,3 +121,36 @@ WithForcedColors.parameters = {
 		modes: disableDefaultModes
 	},
 };
+
+export const StaticWhite = Default.bind({});
+StaticWhite.args = {
+	staticColor: "white",
+	text: "Link using spectrum-Link--staticWhite"
+};
+StaticWhite.tags = ["!autodocs", "!dev", "test"];
+
+export const StaticBlack = Default.bind({});
+StaticBlack.args = {
+	staticColor: "black",
+	text: "Link using spectrum-Link--staticBlack"
+};
+StaticBlack.tags = ["!autodocs", "!dev", "test"];
+
+export const QuietStaticWhite = Default.bind({});
+QuietStaticWhite.storyName = "Quiet (static white)";
+QuietStaticWhite.args = {
+	isQuiet: true,
+	staticColor: "white",
+	text: "Link using spectrum-Link--staticWhite and spectrum-Link--quiet"
+};
+QuietStaticWhite.tags = ["!autodocs", "!dev", "test"];
+
+export const QuietStaticBlack = Default.bind({});
+QuietStaticBlack.storyName = "Quiet (static black)";
+QuietStaticBlack.args = {
+	isQuiet: true,
+	staticColor: "black",
+	text: "Link using spectrum-Link--staticBlack and spectrum-Link--quiet"
+};
+QuietStaticBlack.tags = ["!autodocs", "!dev", "test"];
+
