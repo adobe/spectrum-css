bd8e64b14d8900774c2b66bd93a833e9a51a6170
Rise Erpelding <rise@heysparkbox.com>
2024-07-19 14:59:31 -0500

    docs(textfield): adjustments to make chromatic match main

--- components/textfield/stories/template.js
+++ components/textfield/stories/template.js
@@ -232,7 +232,7 @@ export const TextFieldGroup = Variants({
 		testHeading: "With value",
 		displayLabel: true,
 		labelText: "Username",
-		value: "username@reallylongemail.com"
+		value: "UsernameWiderThanInput@ReallyLongEmail.com"
 	},
 	{
 		testHeading: "With placeholder",
@@ -269,41 +269,34 @@ export const TextFieldGroup = Variants({
 		hasCharacterCount: true,
 		characterCount: 400,
 		value: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-	}
-	],
-	stateData: [
-		{
-			testHeading: "Invalid",
-			isInvalid: true,
-		}, 
-		{
-			testHeading: "Valid",
-			isValid: true,
-		}, 
-		{
-			testHeading: "Focused",
-			isFocused: true,
-		}, 
-		{
-			testHeading: "Keyboard focused",
-			isKeyboardFocused: true,
-		}, 
-		{
-			testHeading: "Disabled",
-			isDisabled: true,
-		}, 
-		{
-			testHeading: "Required",
-			isRequired: true,
-		}, 
-		{
-			testHeading: "Read-only",
-			value: "username@reallylongemail.com",
-			isReadOnly: true,
-		}, 
-		{
-			testHeading: "Loading",
-			isLoading: true,
-		}
-	],
+	}],
+	stateData: [{
+		testHeading: "Invalid",
+		isInvalid: true,
+	},
+	{
+		testHeading: "Valid",
+		isValid: true,
+	},
+	{
+		testHeading: "Focused",
+		isFocused: true,
+	},
+	{
+		testHeading: "Keyboard focused",
+		isKeyboardFocused: true,
+	},
+	{
+		testHeading: "Disabled",
+		isDisabled: true,
+	},
+	{
+		testHeading: "Required",
+		isRequired: true,
+	},
+	{
+		testHeading: "Read-only",
+		value: "username@reallylongemail.com",
+		isReadOnly: true,
+	}]
 });
