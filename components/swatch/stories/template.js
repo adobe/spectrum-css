c896c3dac41c528b3977df9f63d9816e29e05691
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-07-22 15:36:44 -0400

    docs(swatch): enhance template coverage
    - templates for sizes, border, rectangle, disabled, image, gradient,
    corner rounding, nothing/empty

--- components/swatch/stories/template.js
+++ components/swatch/stories/template.js
@@ -1,5 +1,6 @@
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 import { Template as OpacityCheckerboard } from "@spectrum-css/opacitycheckerboard/stories/template.js";
+import { Template as Typography} from "@spectrum-css/typography/stories/template.js";
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
 import { ifDefined } from "lit/directives/if-defined.js";
@@ -11,11 +12,18 @@ import "../index.css";
 export const Template = ({
 	rootClass = "spectrum-Swatch",
 	size = "m",
+	withBorder = "default",
+	isRectangle = false,
+	isImage = false,
+	imageUrl,
+	isGradient = false,
+	isMixedValue = false,
 	isSelected = false,
 	isDisabled = false,
 	rounding = "regular",
 	customClasses = [],
 	swatchColor,
+	gradient,
 	customStyles = {},
 	id,
 } = {}, context = {}) => {
@@ -30,8 +38,13 @@ export const Template = ({
 				[`${rootClass}--rounding${capitalize(
 							lowerCase(rounding)
 						)}`]: typeof rounding !== "undefined" && rounding !== "regular",
+				[`${rootClass}--${withBorder}`]: typeof withBorder !== "undefined" && withBorder !== "default",
 				"is-selected": !isDisabled && isSelected,
 				"is-disabled": isDisabled,
+				"is-image": (isImage || isGradient)
+				&& (typeof gradient !== "undefined" || gradient !== "transparent" || imageUrl !== "undefined"),
+				"is-mixedValue": !isDisabled && isMixedValue,
+				[`${rootClass}--rectangle`]: typeof isRectangle !== "undefined" && isRectangle !== false,
 				"is-nothing": !isDisabled && (typeof swatchColor === "undefined" || swatchColor === "transparent"),
 				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
 			})}
@@ -39,6 +52,8 @@ export const Template = ({
 			id=${ifDefined(id)}
 			style=${ifDefined(styleMap({
 				"--spectrum-picked-color": swatchColor,
+				"--spectrum-gradient": gradient,
+				"--spectrum-background": `url(${imageUrl})`,
 				...customStyles,
 			}))}
 			tabindex="0"
@@ -60,6 +75,11 @@ export const Template = ({
 						customClasses: [`${rootClass}-disabledIcon`],
 						setName: "workflow",
 						iconName: "Cancel",
+					}, context)] : []),
+					...(isMixedValue ? [Icon({
+						customClasses: [`${rootClass}-mixedValueIcon`],
+						setName: "ui",
+						iconName: "Dash",
 					}, context)] : []),
 				]
 			}, context)}
@@ -101,4 +121,174 @@ export const States = (args, context) => html`
 		${SwatchGroup({ ...args, isDisabled: true }, context)}
 		${SwatchGroup({ ...args, isSelected: true }, context)}
 	</div>
+`;
+
+export const SizingGroup = (args, context) => html`
+	${["xs", "s", "m", "l"].map((size) => html`
+		<div>
+			${Typography({
+				semantics: "detail",
+				size: "s",
+				content: [
+					{
+						xs: "Extra-small",
+						s: "Small",
+						m: "Medium (default)",
+						l: "Large",
+					}[size]
+				],
+				customClasses: ["chromatic-ignore"],
+			})}
+			<div style="display: flex; gap: 8px; padding: 8px 0;">
+				${Template({
+					...args,
+					size},
+				context)}
+				${Template({
+					...args,
+					size,
+					swatchColor: "rgba(174, 216, 230, 0.3)",
+					}, context)}
+			</div>
+		</div>
+	`)}
+`;
+
+export const RoundingGroup = (args, context) => html`
+	<div style=${styleMap({
+		"display": "flex",
+		"flex-direction": "column",
+		"flex-wrap": "wrap",
+		"gap": "8px",
+	})}>
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: ["Default"],
+			customClasses: ["chromatic-ignore"],
+		})}
+		${Template({
+			...args,
+			rounding: "default",
+		}, context)}
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: ["Full"],
+			customClasses: ["chromatic-ignore"],
+		})}
+		${Template({
+			...args,
+			rounding: "full",
+		}, context)}
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: ["None"],
+			customClasses: ["chromatic-ignore"],
+		})}
+		${Template({
+			...args,
+			rounding: "none",
+		}, context)}
+`;
+
+export const BorderGroup = (args, context) => html `
+	<div style="display: flex; gap: 8px; padding: 8px 0;">
+		<div>
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: ["Default"],
+			customClasses: ["chromatic-ignore"],
+		})}
+		${Template({
+			...args,
+			}, context)}
+		</div>
+		<div>
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: ["None"],
+			customClasses: ["chromatic-ignore"],
+		})}
+		${Template({
+			...args,
+			withBorder: "noBorder",
+			}, context)}
+		</div>
+	</div>
+`;
+
+export const NothingGroup = (args, context) => html`
+${["xs", "s", "m", "l"].map((size) => html`
+	<div>
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: [
+				{
+					xs: "Extra-small",
+					s: "Small",
+					m: "Medium (default)",
+					l: "Large",
+				}[size]
+			],
+			customClasses: ["chromatic-ignore"],
+		})}
+		<div style="display: flex; gap: 8px; padding: 8px 0;">
+			${Template({
+				...args,
+				size,
+				swatchColor: "transparent",
+				}, context)}
+			${Template({
+				...args,
+				size,
+				rounding: "full",
+				swatchColor: "transparent",
+				}, context)}
+				${Template({
+				...args,
+				size,
+				swatchColor: "transparent",
+				isRectangle: true,
+				}, context)}
+		</div>
+	</div>
+`)}
+`;
+
+export const DisabledGroup = (args, context) => html`
+${["xs", "s", "m", "l"].map((size) => html`
+	<div>
+		${Typography({
+			semantics: "detail",
+			size: "s",
+			content: [
+				{
+					xs: "Extra-small",
+					s: "Small",
+					m: "Medium (default)",
+					l: "Large",
+				}[size]
+			],
+			customClasses: ["chromatic-ignore"],
+		})}
+		<div style="display: flex; gap: 8px; padding: 8px 0;">
+			${Template({
+				...args,
+				size,
+				isDisabled: true, 
+				}, context)}
+			${Template({
+				...args,
+				size,
+				isDisabled: true,
+				rounding: "full",
+				}, context)}
+		</div>
+	</div>
+`)}
 `;
