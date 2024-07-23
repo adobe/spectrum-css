a7036245cf8f448e156e2cf3f82191d17502b930
castastrophe <castastrophe@users.noreply.github.com>
2024-07-23 16:49:21 -0400

    chore: storybook coverage enhancements

--- .storybook/blocks/component-details.jsx
+++ .storybook/blocks/component-details.jsx
@@ -1,16 +1,15 @@
-import { useOf } from '@storybook/blocks';
 import React from "react";
-
+import { useOf } from '@storybook/blocks';
 import "./component-details.css";
 
 /**
  * Displays the current version number of the component. The version is read from
  * the component's parameters, where it was sourced from the package.json file.
- *
+ * 
  * Also displays a component status of "deprecated" if it is set in the story's
  * parameters.
- *
- * Usage of this doc block within MDX template(s):
+ * 
+ * Usage of this doc block within MDX template(s): 
  *  <ComponentDetails />
  */
 const ComponentDetails = () => {
@@ -22,7 +21,7 @@ const ComponentDetails = () => {
 			<dt>Current version:</dt>
 			<dd>{versionNumber}</dd>
 
-			{ storyMeta?.csfFile?.meta?.parameters?.status?.type == "deprecated"
+			{ storyMeta?.csfFile?.meta?.parameters?.status?.type == "deprecated" 
 				?	<>
 						<dt>Component status:</dt>
 						<dd>Deprecated</dd>
