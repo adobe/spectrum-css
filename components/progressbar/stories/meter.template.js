660dc581207724bc2c672dfd77a66681652377fb
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-07-24 13:19:31 -0400

    docs(meter): add gradient example to chromatic preview

--- components/progressbar/stories/meter.template.js
+++ components/progressbar/stories/meter.template.js
@@ -17,3 +17,74 @@ export const Template = ({
 	size,
 	...item,
 }, context);
+
+export const MeterGroup = (args, context) => html`
+	<div style=${styleMap({
+		"display": window.isChromatic() ? "none" : "contents"
+	})}>
+		${Template(args, context)}
+	</div>
+	<div style=${styleMap({
+		"display": window.isChromatic() ? "flex" : "none",
+		"flex-direction": "column",
+		"align-items": "flex-start",
+		"gap": "32px",
+	})}>
+		${[{},
+		{
+			heading: "Large",
+			size: "l",
+		},
+		{
+			heading: "Positive",
+			fill: "positive",
+		},
+		{
+			heading: "Negative",
+			fill: "negative",
+		},
+		{
+			heading: "Notice",
+			fill: "notice",
+		},
+		{
+			heading: "Text overflow",
+			fill: "notice",
+			label: "Storage space remaining for XYZ user"
+		}].map(({ heading, ...item }) => html`
+			<div>
+				${Typography({
+					semantics: "heading",
+					size: "xs",
+					content: [heading],
+				}, context)}
+				${Template({
+					...args,
+					...item,
+				}, context)}
+			</div>
+		`)}
+
+		<!-- The gradient story below supports linear-gradients used by Express. For use cases that require a custom linear-gradient for any --mod-*-{fill} properties, set those custom properties in CSS. -->
+		 <div>
+			${Typography({
+				semantics: "heading",
+				size: "s",
+				content: ["Gradient support"],
+				customClasses: ["chromatic-ignore"],
+			}, context)}
+			<div style=${styleMap({
+				"display": window.isChromatic() ? "flex" : "none",
+				"flex-direction": "column",
+				"align-items": "flex-start",
+				"gap": "32px",
+			})}>
+				${Template({
+					...args,
+					trackFill: "linear-gradient(to right, hotpink, orange)",
+					progressBarFill: "linear-gradient(to left, teal, purple)",
+				}, context)}
+			</div>
+		</div>
+	</div>
+`;
