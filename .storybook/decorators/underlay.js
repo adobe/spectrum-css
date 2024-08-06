134194f9f7ed55293323a443fae21660a2064455
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-08-06 14:55:01 -0400

    chore: add additional test case to link
    Co-authored-by: Josh Winn <965114+jawinn@users.noreply.github.com>

--- .storybook/decorators/underlay.js
+++ .storybook/decorators/underlay.js
@@ -1,7 +1,6 @@
 import { Template as Underlay } from "@spectrum-css/underlay/stories/template.js";
 import { makeDecorator } from "@storybook/preview-api";
 import { html } from "lit";
-import { styleMap } from "lit/directives/style-map.js";
 
 /**
  * @type import('@storybook/csf').DecoratorFunction<import('@storybook/web-components').WebComponentsFramework>
@@ -20,19 +19,26 @@ export const withUnderlayWrapper = makeDecorator({
 			} = {},
 		} = context;
 
+		if (testingPreview) {
+			document.addEventListener("DOMContentLoaded", () => {
+				// Ensure the body is positioned relative to allow the underlay to
+				// be positioned absolutely within the body
+				document.body.style.position = "relative";
+			});
+		}
+
 		// Expand the underlay to fill the entire screen when testing previews
 		// to ensure the underlay is always visible in snapshots
 		return html`
-			<div style=${styleMap({ "position": "relative", "display": testingPreview ? "block" : "contents" })}>
-				${Underlay({
-					isOpen,
-					customStyles: {
-						"block-size": testingPreview ? "100%" : undefined,
-						"inline-size": testingPreview ? "100%" : undefined,
-					},
-				}, context)}
-				${StoryFn(context)}
-			</div>
+			${Underlay({
+				isOpen,
+				customStyles: {
+					"position": testingPreview ? "absolute" : undefined,
+					"block-size": testingPreview ? "100%" : undefined,
+					"inline-size": testingPreview ? "100%" : undefined,
+				},
+			}, context)}
+			${StoryFn(context)}
         `;
 	},
 });
