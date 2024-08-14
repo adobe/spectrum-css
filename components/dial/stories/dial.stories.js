8505ca345ad84f6f6da9fbc37c1d12b01e4961bb
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-08-14 17:04:06 -0400

    docs(dial): add missing stories and chromatic coverage

--- components/dial/stories/dial.stories.js
+++ components/dial/stories/dial.stories.js
@@ -1,7 +1,7 @@
 import { disableDefaultModes } from "@spectrum-css/preview/modes";
-import { isFocused } from "@spectrum-css/preview/types";
 import { version } from "../package.json";
 import { DialGroup } from "./dial.test.js";
+import { Template } from "./template.js";
 
 /**
  * A dial is an input control used for selecting a value within a range, similar to a slider. It's often used in audio and video mixing and editing applications, where horizontal space is limited.
@@ -28,7 +28,15 @@ export default {
 			},
 			control: "text",
 		},
-		isFocused,
+		isFocusVisible: {
+			name: "Focused",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "State",
+			},
+			control: "boolean",
+		},
 		isDragged: {
 			name: "Dragged",
 			type: { name: "boolean" },
@@ -51,7 +59,7 @@ export default {
 	args: {
 		rootClass: "spectrum-Dial",
 		size: "m",
-		isFocused: false,
+		isFocusVisible: false,
 		isDragged: false,
 		isDisabled: false,
 	},
@@ -62,6 +70,33 @@ export default {
 
 export const Default = DialGroup.bind();
 Default.args = {};
+
+export const Small = Template.bind();
+Small.args = {
+	size: "s",
+};
+Small.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+Small.tags = ["!dev"];
+
+export const WithLabel = Template.bind();
+WithLabel.args = {
+	label: "Volume",
+};
+WithLabel.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+WithLabel.tags = ["!dev"];
+
+export const Disabled = Template.bind();
+Disabled.args = {
+	isDisabled: true,
+};
+Disabled.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+Disabled.tags = ["!dev"];
 
 // ********* VRT ONLY ********* //
 export const WithForcedColors = DialGroup.bind({});
