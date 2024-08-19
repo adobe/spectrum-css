d9318ee6ca6af23e75a01c66a3a01541342cfb28
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-08-19 17:22:13 -0400

    docs(meter,progressbar): rebase fixes

--- components/progressbar/stories/meter.template.js
+++ components/progressbar/stories/meter.template.js
@@ -4,7 +4,6 @@ import "../index.css";
 
 export const Template = ({
 	customClasses = [],
-	customStyles = {},
 	fill,
 	size = "s",
 	...item
@@ -16,61 +15,5 @@ export const Template = ({
 		typeof fill !== "undefined" ? `is-${fill}` : null,
 	].filter(Boolean),
 	size,
-	customStyles,
 	...item,
 }, context);
-
-/* The gradient story below supports linear-gradients used by Express. For use cases that require a custom linear-gradient for any --mod-*-{fill} properties, set those custom properties in CSS. */
-export const MeterGroup = (args, context) => html`
-	<div style=${styleMap({
-		"display": window.isChromatic() ? "none" : "contents"
-	})}>
-		${Template(args, context)}
-	</div>
-	<div style=${styleMap({
-		"display": window.isChromatic() ? "flex" : "none",
-		"flex-direction": "column",
-		"align-items": "flex-start",
-		"gap": "32px",
-	})}>
-		${[{},
-		{
-			heading: "Large",
-			size: "l",
-		},
-		{
-			heading: "Positive",
-			fill: "positive",
-		},
-		{
-			heading: "Negative",
-			fill: "negative",
-		},
-		{
-			heading: "Notice",
-			fill: "notice",
-		},
-		{
-			heading: "Text overflow",
-			fill: "notice",
-			label: "Storage space remaining for XYZ user"
-		},
-		{
-			heading: "Gradient support", 
-			trackFill: "linear-gradient(to right, hotpink, orange)",
-			progressBarFill: "linear-gradient(to left, teal, purple)",
-		}].map(({ heading, ...item }) => html`
-			<div>
-				${Typography({
-					semantics: "heading",
-					size: "xs",
-					content: [heading],
-				}, context)}
-				${Template({
-					...args,
-					...item,
-				}, context)}
-			</div>
-		`)}
-	</div>
-`;
--- components/progressbar/stories/progressbar.test.js
+++ components/progressbar/stories/progressbar.test.js
@@ -15,11 +15,19 @@ export const ProgressBarGroup = Variants({
 			testHeading: "Text overflow",
 			label: "Storage space remaining for XYZ user"
 		},
+		/* The gradient story below supports linear-gradients used by Express. For use cases that require a custom 
+		linear-gradient for any --mod-*-{fill} properties, set those custom properties in CSS.
+		*/
+		{
+			testHeading: "Gradient support",
+			trackFill: "linear-gradient(to right, hotpink, orange)",
+			progressBarFill: "linear-gradient(to left, teal, purple)",
+		},
 	],
 	stateData: [
 		{
 			testHeading: "Indeterminate",
-			indeterminate: true,
+			isIndeterminate: true,
 			value: undefined,
 		}
 	],
