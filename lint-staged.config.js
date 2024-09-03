8cb31b671d0c23002ac09fe1db31179682cad295
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-09-03 12:44:50 -0400

    chore: metadata schema validation (#3059)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	"*.css": [
-		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write"
+	"*.css,!plugins/*/{expected,fixtures}/*.css": [
+		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write",
 	],
 	"*.{js,json}": [
 		"eslint --fix --cache --no-error-on-unmatched-pattern --quiet --ignore-pattern \"!.storybook/\""
@@ -12,6 +12,12 @@ module.exports = {
 		return [
 			...(files.map(file => `pajv test --valid -s ./schemas/documentation.schema.json -d "${file}"`) ?? []),
             `prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write ${files.join(" ")}`
+		];
+	},
+	"components/*/metadata/metadata.json": (files) => {
+		return [
+			...(files.map(file => `pajv test --valid -s ./schemas/metadata.schema.json -d "${file}"`) ?? []),
+			`prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write ${files.join(" ")}`
 		];
 	},
 	".storybook/deprecated/*/*.yml": (files) => {
