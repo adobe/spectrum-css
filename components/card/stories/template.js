fc46362aa62ea99201dece3788bb6ede0006d394
Cory Dransfeldt <cdransfeldt@adobe.com>
2024-09-05 13:08:01 -0700

    chore(card): hide footer if isQuiet is true

--- components/card/stories/template.js
+++ components/card/stories/template.js
@@ -180,7 +180,7 @@ export const Template = ({
           </div>`
       )}
       ${when(
-        footer,
+        footer && !isQuiet,
         () => html`
           <div
             class=${classMap({
