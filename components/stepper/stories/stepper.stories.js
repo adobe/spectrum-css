11777f450f75af6c6bb8ef8bf3ae6742f2fa1b72
castastrophe <castastrophe@users.noreply.github.com>
2024-09-17 10:57:36 -0400

    chore(stepper): update vrt snpashots

--- components/stepper/stories/stepper.stories.js
+++ components/stepper/stories/stepper.stories.js
@@ -1,9 +1,9 @@
-import { disableDefaultModes } from "@spectrum-css/preview/modes";
-import { isDisabled, isFocused, isInvalid, isKeyboardFocused, isQuiet, size } from "@spectrum-css/preview/types";
 import { Sizes } from "@spectrum-css/preview/decorators";
+import { disableDefaultModes } from "@spectrum-css/preview/modes";
+import { isDisabled, isFocused, isHovered, isInvalid, isKeyboardFocused, isQuiet, size } from "@spectrum-css/preview/types";
 import pkgJson from "../package.json";
 import { StepperGroup } from "./stepper.test.js";
-import { Template, DisabledVariantsGroup, AllDefaultVariantsGroup } from "./template";
+import { AllDefaultVariantsGroup, DisabledVariantsGroup, Template } from "./template";
 
 /**
  * A stepper can be used to increment or decrement a value by a specified amount via an up/down button. An input field displays the current value.
@@ -26,6 +26,7 @@ export default {
 		isDisabled,
 		isInvalid,
 		isFocused,
+		isHovered,
 		isKeyboardFocused,
 	},
 	args: {
@@ -33,6 +34,7 @@ export default {
 		size: "m",
 		isQuiet: false,
 		isFocused: false,
+		isHovered: false,
 		isKeyboardFocused: false,
 		isInvalid: false,
 		isDisabled: false,
