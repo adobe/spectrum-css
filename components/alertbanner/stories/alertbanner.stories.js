c9176c14271aa560fa18541c7b1f1fb0a0ac2722
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-09-18 10:45:31 -0400

    docs(alertbanner): migrate docs to storybook (#3118)
    Create docs-only stories for alert banner in Storybook to represent
    what was on the docs site.
    
    Modifies the alert banner's action button control to allow the text of
    the button to be editable.
    
    Adds test cases and examples for options without close button and
    without close button or action button.

--- components/alertbanner/stories/alertbanner.stories.js
+++ components/alertbanner/stories/alertbanner.stories.js
@@ -2,9 +2,11 @@ import { disableDefaultModes } from "@spectrum-css/preview/modes";
 import { isOpen } from "@spectrum-css/preview/types";
 import pkgJson from "../package.json";
 import { AlertBannerGroup } from "./alertbanner.test.js";
+import { ActionableOptionsTemplate, Template, TextOverflowTemplate } from "./template.js";
 
 /**
- * The alert banner show pressing and high-signal messages, such as system alerts. They're meant to be noticed and prompt users to take action.
+ * The alert banner shows pressing and high-signal messages, such as system alerts. It is meant to be noticed and prompt users to take action.
+ * It should occupy all of the available horizontal space until it reaches its maximum allowed width.
  */
 export default {
 	title: "Alert banner",
@@ -13,6 +15,7 @@ export default {
 		isOpen,
 		text: {
 			name: "Text",
+			description: "Text of the message displayed within the alert banner.",
 			type: { name: "string", required: true },
 			table: {
 				type: { summary: "string" },
@@ -22,29 +25,43 @@ export default {
 			control: { type: "text" },
 		},
 		variant: {
-			name: "Background color variants",
+			name: "Semantic variants",
 			type: { name: "string" },
 			table: {
 				type: { summary: "string" },
 				category: "Component",
+				defaultValue: { summary: "neutral" },
 			},
 			options: ["neutral", "info", "negative"],
 			control: "radio",
 		},
-		hasActionButton: {
-			name: "Display action button",
+		actionButtonText: {
+			name: "Action button text",
+			description: "Label text for the optional action button. When empty, the action button does not display.",
+			type: { name: "string" },
+			table: {
+				type: { summary: "string" },
+				category: "Content",
+			},
+			control: "text",
+		},
+		showCloseButton: {
+			name: "Show close button",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
-				category: "Content",
+				category: "Component",
 			},
 			control: "boolean",
-		},
+		}
 	},
 	args: {
 		rootClass: "spectrum-AlertBanner",
 		isOpen: false,
 		variant: "neutral",
+		actionButtonText: "Action",
+		text: "Your trial has expired",
+		showCloseButton: true,
 	},
 	parameters: {
 		actions: {
@@ -55,13 +72,9 @@ export default {
 };
 
 export const Default = AlertBannerGroup.bind({});
+Default.tags = ["!autodocs"];
 Default.args = {
 	isOpen: true,
-	hasActionButton: true,
-	text: "Your trial has expired",
-	customStyles: {
-		"inline-size": "600px",
-	}
 };
 
 // ********* VRT ONLY ********* //
@@ -73,4 +86,78 @@ WithForcedColors.parameters = {
 		forcedColors: "active",
 		modes: disableDefaultModes
 	},
+};
+
+// ********* DOCS ONLY ********* //
+
+/**
+ * Neutral is the default semantic variant.
+ */
+export const Neutral = Template.bind({});
+Neutral.storyName = "Variant: neutral";
+Neutral.tags = ["!dev"];
+Neutral.args = {
+	isOpen: true,
+	variant: "neutral",
+	text: "Your trial has expired",
+};
+Neutral.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const Informative = Template.bind({});
+Informative.storyName = "Variant: informative";
+Informative.tags = ["!dev"];
+Informative.args = {
+	isOpen: true,
+	variant: "info",
+	text: "Your trial will expire in 3 days",
+};
+Informative.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const Negative = Template.bind({});
+Negative.storyName = "Variant: negative";
+Negative.tags = ["!dev"];
+Negative.args = {
+	isOpen: true,
+	variant: "negative",
+	text: "Connection interupted. Check your network to continue.",
+};
+Negative.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+/**
+ * The alert banner component can contain both an icon-only close button and a button with a contextual action to
+ * take. Whenever possible, include the in-line action button if there's a way for a user to quickly address the issue
+ * associated with an alert. There should never be more than one button with a contextual action in an alert banner.
+ * 
+ * The close button is optional, depending on context. Consider adding one to let a user easily dismiss the alert.
+ */
+export const ActionableOptions = ActionableOptionsTemplate.bind({});
+ActionableOptions.storyName = "Actionable and dismissable";
+ActionableOptions.tags = ["!dev"];
+ActionableOptions.args = {
+	isOpen: true,
+	variant: "negative",
+};
+ActionableOptions.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+/**
+ * When the text is too long for the available horizontal space, it wraps to form another line. In
+ * actionable alert banners, the button moves below the text.
+ */
+export const TextOverflow = TextOverflowTemplate.bind({});
+TextOverflow.storyName = "Text overflow behavior";
+TextOverflow.tags = ["!dev"];
+TextOverflow.args = {
+	isOpen: true,
+	variant: "negative",
+};
+TextOverflow.parameters = {
+	chromatic: { disableSnapshot: true },
 };
--- components/alertbanner/stories/alertbanner.test.js
+++ components/alertbanner/stories/alertbanner.test.js
@@ -3,25 +3,56 @@ import { Template } from "./template.js";
 
 export const AlertBannerGroup = Variants({
 	Template,
+	stateDirection: "column",
 	testData: [
 		{
-			testHeading: "Neutral",
+			testHeading: "Default (neutral)",
 		},
 		{
-			testHeading: "Informational",
-			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purcahsed the software.",
+			testHeading: "Default, with wrapping text",
+			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		},
+		{
+			testHeading: "Informative",
 			variant: "info",
-			hasActionButton: false,
 		},
 		{
-			testHeading: "Warning",
-			text: "Connection interupted. Check your network to continue.",
+			testHeading: "Informative, with wrapping text",
+			variant: "info",
+			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		},
+		{
+			testHeading: "Negative",
 			variant: "negative",
-			hasActionButton: true,
+		},
+		{
+			testHeading: "Negative, with wrapping text",
+			variant: "negative",
+			text: "Your trial will expire in 3 days. Once it expires your files will be saved and ready for you to open again once you have purchased the software Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 		},
 		{
 			testHeading: "Closed",
 			isOpen: false,
+		},
+	],
+	stateData: [
+		{
+			testHeading: "Action button only",
+			showCloseButton: false,
+			actionButtonText: "Take action",
+			ignore: ["Closed"],
+		},
+		{
+			testHeading: "Close button only",
+			showCloseButton: true,
+			actionButtonText: "",
+			ignore: ["Closed"],
+		},
+		{
+			testHeading: "Text only",
+			showCloseButton: false,
+			actionButtonText: "",
+			ignore: ["Closed"],
 		},
 	],
 });
