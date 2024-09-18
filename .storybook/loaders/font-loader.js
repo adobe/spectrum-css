d684f5169b7a5072812dd1efd57432e0715a1707
castastrophe <castastrophe@users.noreply.github.com>
2024-09-18 09:05:55 -0400

    chore(storybook): typekit and react error resolution

--- .storybook/loaders/font-loader.js
+++ .storybook/loaders/font-loader.js
@@ -1,11 +1,11 @@
 export const FontLoader = async () => ({
 	fonts: new Promise((resolve) => {
 		// First check if the fonts are already loaded
-		if (window.Typekit) resolve();
+		if (typeof window.Typekit !== "undefined") resolve();
 
 		// Listen for a custom event indicating the Adobe Fonts have loaded
 		document.addEventListener("typekit-loaded", () => {
-			if (window.Typekit) resolve();
+			if (typeof window.Typekit !== "undefined") resolve();
 		});
 	}),
 });
