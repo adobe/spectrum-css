a3411e60f580ddd0f5ef694a0ec6261fc9733b33
castastrophe <castastrophe@users.noreply.github.com>
2024-09-26 12:10:12 -0400

    feat(storybook): custom properties panel

--- components/underlay/stories/underlay.stories.js
+++ components/underlay/stories/underlay.stories.js
@@ -31,6 +31,17 @@ export default {
 		chromatic: { disableSnapshot: true },
 		packageJson,
 		metadata,
+		cssprops: {
+			...metadata.modifiers.reduce((collection, item) => {
+				const key = item.replace(/^--/, "");
+				collection[key] = {
+					category: "Modifiers",
+					control: key.includes("color") ? "color" : "text",
+					value: key.includes("color") ? undefined : " ",
+				};
+				return collection;
+			}, {})
+		},
 	}
 };
 
