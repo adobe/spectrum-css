d83a86825d034c08b81f8710398f1c9a902240b1
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-09-26 20:53:08 -0400

    chore(storybook): support static color settings in testing grid

--- .storybook/decorators/context.js
+++ .storybook/decorators/context.js
@@ -20,7 +20,8 @@ export const withContextWrapper = makeDecorator({
 				color = "light",
 				context = "legacy",
 				scale = "medium",
-			} = {},
+				testingPreview = false,
+			},
 			id,
 			viewMode,
 		} = data;
@@ -36,6 +37,37 @@ export const withContextWrapper = makeDecorator({
 			},
 		};
 
+		const updateScale = (scale) => {
+			const isRaw = Boolean(context === "raw");
+
+			for (const container of fetchContainers(id, viewMode === "docs", testingPreview)) {
+				container.classList.toggle("spectrum--medium", scale === "medium" && !isRaw);
+				container.classList.toggle("spectrum--large", scale === "large" && !isRaw);
+			}
+		};
+
+		const resizeObserver = new ResizeObserver((entries) => {
+			for (const entry of entries) {
+				const { contentRect } = entry;
+				const { width } = contentRect;
+
+				if (width >= 480 && scale !== "medium") {
+					scale = "medium";
+					updateScale("medium");
+				}
+				else if (width < 480 && scale !== "large") {
+					scale = "large";
+					updateScale("large");
+				}
+			}
+		});
+
+		const original = {
+			color,
+			context,
+			scale,
+		};
+
 		useEffect(() => {
 			const isRaw = Boolean(context === "raw");
 			const isModern = Boolean(context === "spectrum");
@@ -47,25 +79,35 @@ export const withContextWrapper = makeDecorator({
 				document.body.classList.add("spectrum", "spectrum--light", "spectrum--medium");
 			}
 
-			for (const container of fetchContainers(id, viewMode === "docs")) {
-				// Start by attaching the appropriate tokens to the container
-				toggleStyles(container, "tokens", tokens, !isRaw);
+			// Start by attaching the appropriate tokens to the container
+			toggleStyles(document.body, "tokens", tokens, !isRaw);
+
+			for (const container of fetchContainers(id, viewMode === "docs", testingPreview)) {
+				// Reset the context to the original values
+				color = original.color;
+				context = original.context;
+				scale = original.scale;
 
 				// Check if the container has a static color element
-				const hasStaticElement = container.matches(`:has(.${rootClass}--staticWhite, .${rootClass}--staticBlack, .${rootClass}--overBackground)`);
+				let hasStaticElement = container.matches(`:has([data-html-preview])`) ? container.matches(`:has([data-html-preview] .${rootClass}--staticWhite, [data-html-preview] .${rootClass}--staticBlack, [data-html-preview] .${rootClass}--overBackground)`) : container.matches(`:has(.${rootClass}--staticWhite, .${rootClass}--staticBlack, .${rootClass}--overBackground)`);
+
 				let staticKey = staticColor;
 				if (!staticKey && hasStaticElement) {
 					staticKey = (
-						container.querySelector(`.${rootClass}--staticWhite`) && "white" ||
-						container.querySelector(`.${rootClass}--staticBlack, .${rootClass}--overBackground`) && "black"
+						container.querySelector(`.${rootClass}--staticWhite, .${rootClass}--overBackground`) && "white" ||
+						container.querySelector(`.${rootClass}--staticBlack`) && "black"
 					);
 				}
+
+				// If we can't determine the static key, we can't use the static color
+				if (!staticKey) hasStaticElement = false;
 
 				// Every container gets the spectrum class
 				container.classList.toggle("spectrum", !isRaw);
 
 				// S1 and S1 Express get the legacy class
 				container.classList.toggle("spectrum--legacy", !isModern && !isRaw);
+
 				// Express only gets the express class
 				container.classList.toggle("spectrum--express", isExpress && !isRaw);
 
@@ -76,16 +118,23 @@ export const withContextWrapper = makeDecorator({
 					color = "dark";
 				}
 
-				for (let c of ["light", "dark", "darkest"]) {
-					// Force light or dark mode if the static color is set
-					const isColor = staticKey && c === staticColorSettings[staticKey]?.color || !staticKey && c === color;
+				// Let the static color override the color if it's set
+				if (hasStaticElement && staticColorSettings[staticKey]?.color) {
+					color = staticColorSettings[staticKey].color;
+				}
 
-					container.classList.toggle(`spectrum--${c}`, isColor && !isRaw);
+				// Force a light theme for the body wrapper in testing preview mode
+				// because the individual containers will bring in the correct theme
+				if (testingPreview && container.matches("body:has([data-testing-preview]")) {
+					color = "light";
+				}
+
+				for (let c of ["light", "dark", "darkest"]) {
+					container.classList.toggle(`spectrum--${c}`, c === color && !isRaw);
 				}
 
 				for (const s of ["medium", "large"]) {
-					const isScale = s === scale;
-					container.classList.toggle(`spectrum--${s}`, isScale && !isRaw);
+					container.classList.toggle(`spectrum--${s}`, s === scale && !isRaw);
 				}
 
 				// Start by removing the background color from the container and then add it back if needed
@@ -94,7 +143,12 @@ export const withContextWrapper = makeDecorator({
 					container.style.background = staticColorSettings[staticKey].background;
 				}
 			}
-		}, [color, context, staticColor, scale, viewMode, rootClass, tokens, staticColorSettings]);
+
+		}, [context, viewMode, original, staticColor, color, scale, rootClass, tokens, staticColorSettings]);
+
+
+		// We only need to observe the body element for a viewport change
+		resizeObserver.observe(document.body);
 
 		return StoryFn(data);
 	},
--- components/divider/stories/divider.test.js
+++ components/divider/stories/divider.test.js
@@ -20,6 +20,14 @@ export const DividerGroup = Variants({
 			testHeading: "Vertical",
 			vertical: true,
 			minDimensionValues: true,
-		}
+		},
+		{
+			testHeading: "Static black",
+			staticColor: "black",
+		},
+		{
+			testHeading: "Static white",
+			staticColor: "white",
+		},
 	],
 });
