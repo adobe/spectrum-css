5f523698503e5683f5a769b0134e092238f7e1b0
rise-erpelding <54716846+rise-erpelding@users.noreply.github.com>
2024-09-27 12:50:36 -0500

    docs(tooltip): docs site to storybook (#3144)

--- components/tooltip/stories/template.js
+++ components/tooltip/stories/template.js
@@ -1,8 +1,10 @@
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
+import { Container } from "@spectrum-css/preview/decorators";
 import { html } from "lit";
 import { classMap } from "lit/directives/class-map.js";
 import { styleMap } from "lit/directives/style-map.js";
 import { when } from "lit/directives/when.js";
+import { capitalize } from "lodash-es";
 
 import "../index.css";
 
@@ -68,4 +70,99 @@ export const Template = ({
 			})}></span>
 		</span>
 	`;
+};
+
+const placements = [
+	"top",
+	"top-left",
+	"top-right",
+	"top-start",
+	"top-end",
+	"bottom",
+	"bottom-left",
+	"bottom-right",
+	"bottom-start",
+	"bottom-end",
+	"right",
+	"right-bottom",
+	"right-top",
+	"left",
+	"left-bottom",
+	"left-top",
+	"start",
+	"start-top",
+	"start-bottom",
+	"end",
+	"end-top",
+	"end-bottom",
+];
+
+export const TooltipShowOnHover = (args, context) => {
+	const placementHeadings = ["top", "bottom", "right", "left", "start", "end"];
+	return Container({
+		withBorder: false,
+		content: placementHeadings.map((heading) => html`
+			${Container({
+				heading,
+				content: html`
+					<div style="display: flex; flex-wrap: wrap;">
+						${placements.map((placement) => 
+								when(placement.startsWith(heading), () => html`
+									<span class="u-tooltip-showOnHover" style="margin: 15px 50px; cursor: default;">
+										${capitalize(placement.replace(/-/g, " "))}
+										${Template({
+											...args,
+											context,
+											placement,
+										})}
+									</span>
+								`)
+						)}
+					</div>
+				`
+			})}
+			`)
+	});
+};
+
+export const TooltipPlacementGroup = (args, context) => Container({
+	withBorder: false,
+	content: placements.map((placement) => html`
+		${Container({
+			heading: capitalize(placement.replace(/-/g, " ")),
+			content: html`
+				${Template({
+					...args,
+					context,
+					placement,
+				})}
+			`
+		})}
+	`)
+});
+
+// these variants no longer exist in s2
+export const SemanticVariantGroup = (args, context) => {
+	const variants = [
+		"neutral",
+		"info",
+		"positive",
+		"negative",
+	];
+
+	return Container({
+		withBorder: false,
+		content: variants.map((variant) => html`
+			${Container({
+				heading: capitalize(variant.replace(/-/g, " ")),
+				content: html`
+					${Template({
+						...args,
+						context,
+						variant,
+					})}
+				`
+			})}
+		`)
+	});
 };
