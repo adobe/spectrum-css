55f73e5c3fe9ec81e21894f1d7977744e930a786
Marissa Huysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>
2024-10-07 12:11:59 -0400

    docs: add links to figma assets part 1 (#3202)
    * docs: add links to figma assets
    * docs: add figma links for top-level component pages

--- components/actiongroup/stories/actiongroup.stories.js
+++ components/actiongroup/stories/actiongroup.stories.js
@@ -60,6 +60,10 @@ export default {
 				...(ActionButton?.parameters?.actions?.handles ?? []),
 			],
 		},
+		design: {
+			type: "figma",
+			url: "https://www.figma.com/design/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?node-id=19083-360",
+		},
 		packageJson: pkgJson,
 	},
 };
