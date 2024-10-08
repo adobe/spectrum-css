265e2dcaa17c621201f96467f26553b9143763d2
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-10-08 13:09:51 -0400

    chore: S2 foundations design links (#3217)
    * docs: add links to figma assets (#3202)
    
    ---------
    
    Co-authored-by: Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
    Co-authored-by: Marissa Huysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>

--- components/progressbar/stories/meter.stories.js
+++ components/progressbar/stories/meter.stories.js
@@ -41,6 +41,10 @@ export default {
 		label: "Storage space",
 	},
 	parameters: {
+		design: {
+			type: "figma",
+			url: "https://www.figma.com/design/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?node-id=33840-90",
+		},
 		packageJson,
 		metadata,
 	},
