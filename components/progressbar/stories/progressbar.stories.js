3b9bd424158b058a8e05963c7ae74402df8c0358
[ Cassondra ] <castastrophe@users.noreply.github.com>
2024-10-08 13:09:51 -0400

    chore: S2 foundations design links (#3217)
    * docs: add links to figma assets (#3202)
    
    ---------
    
    Co-authored-by: Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
    Co-authored-by: Marissa Huysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>

--- components/progressbar/stories/progressbar.stories.js
+++ components/progressbar/stories/progressbar.stories.js
@@ -92,6 +92,10 @@ export default {
 		showValueLabel: true,
 	},
 	parameters: {
+		design: {
+			type: "figma",
+			url: "https://www.figma.com/design/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?node-id=13059-181",
+		},
 		packageJson,
 		metadata,
 	},
