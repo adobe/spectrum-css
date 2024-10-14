8efa03b82cf6134c4dd78c76d7062469708a14ed
Aziz Ramos <abdulr@adobe.com>
2024-10-14 13:39:03 -0400

    feat(illustratedmessage): adding new s2 tokens
    feat(illustratedmessage): updating token version
    
    feat(illustratedmessage): work in progress
    
    feat(illustratedmessage): adding new tokens, tshirt sizes, orientation, button group
    
    feat(illustratedmessage): updating CSS structure and adding conditional buttons
    
    feat(illustratedmessage): update tokens
    
    feat(illustratedmessage): changeset, styles, dropzone mods
    
    feat(illustratedmessage): work in progress
    
    feat(illustratedmessage): updating mods
    
    feat(illustratedmessage): removing message accent color selector
    
    chore(illustratedmessage): updating changeset
    
    docs(illustratedmessage): heading controls
    
    fix(illustratedmessage): fixing cjk tokens and removing heading and base classes
    
    fix(illustratedmessage): fixing cjk tokens and removing heading and base classes
    
    feat(illustratedmessage): new mods
    
    docs(illustratedmessage): adding horizontal story context
    
    feat(illustratedmessage): removing extra dashes, reverting dropzone
    
    chore(fix): adding stylelint disable desc
    
    chore(fix): removing unused highcontrast accent color token
    
    feat(illustratedmessage): adding grid display
    
    chore(im): removing unused import
    
    chore(im): removing more unused imports
    
    chore(illustratedmessage): updating testing env
    
    fix(dropzone): removing unused controls
    
    feat(illustratedmessage): reviewing comments
    
    feat(illustratedmessage): adjusting story horiz copy
    
    fix(illustratedmessage): resolving changes
    
    chore(im): bringing back to center
    
    fix: removing lint line
    
    chore: restore stepper index css file
    
    chore(illustratedmessage): removing one liner in dropzone)
    
    chore(illustratedmessage): removing stylelint line
    
    chore(illustratedmessage): removing unused custom props in dropzone

--- components/illustratedmessage/stories/illustratedmessage.stories.js
+++ components/illustratedmessage/stories/illustratedmessage.stories.js
@@ -1,29 +1,19 @@
-import { Template as Link } from "@spectrum-css/link/stories/template.js";
+import { Sizes } from "@spectrum-css/preview/decorators";
 import { disableDefaultModes } from "@spectrum-css/preview/modes";
-import { html } from "lit";
 import metadata from "../dist/metadata.json";
 import packageJson from "../package.json";
 import { IllustratedMessageGroup } from "./illustratedmessage.test.js";
 import { Template } from "./template.js";
 
 /**
- * The Illustrated Message displays an illustration along with a heading and description. Optionally, part of the illustration can use an accent color. It can be used for status and errors, or as a call to action. For example, the Drop Zone component makes use of Illustrated Message as an area to drag and drop files.
+ * The Illustrated Message displays an illustration along with a heading, description and group of buttons. It can be used for status and errors, or as a call to action. For example, the Drop Zone component makes use of Illustrated Message as an area to drag and drop files.
  */
 export default {
 	title: "Illustrated message",
 	component: "IllustratedMessage",
 	argTypes: {
-		useAccentColor: {
-			name: "Illustration accent color",
-			type: { name: "boolean" },
-			table: {
-				type: { summary: "boolean" },
-				category: "Component",
-			},
-			control: "boolean",
-		},
-		heading: {
-			name: "Heading",
+		title: {
+			name: "Title",
 			type: { name: "string" },
 			table: {
 				type: { summary: "string" },
@@ -33,15 +23,50 @@ export default {
 		},
 		description: {
 			name: "Description",
+			type: { name: "string" },
 			table: {
+				type: { summary: "string" },
 				category: "Content",
-				disable: true,
 			},
+			control: "text",
+		},
+		isHorizontal: {
+			name: "Horizontal orientation",
+			description: "The default content orientation is vertical. Add the horizontal class to horizontally align the illustration with the content. The illustration will align left or right determined by the global direction of the text.",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Component",
+			},
+			control: "boolean",
+		},
+		size: {
+			name: "Size",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["s", "m", "l"],
+			control: "select",
+		},
+		hasButtons: {
+			name: "Show button group",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Component",
+			},
+			control: "boolean",
 		},
 	},
 	args: {
 		rootClass: "spectrum-IllustratedMessage",
-		useAccentColor: false,
+		isHorizontal: false,
+		size: "m",
+		hasButtons: true,
+		title: "Error 404: Page not found",
+		description: "This page isn't available. Try checking the URL or visit a different page.",
 	},
 	parameters: {
 		design: {
@@ -50,32 +75,22 @@ export default {
 		},
 		packageJson,
 		metadata,
+		layout: "centered"
 	},
 };
 
 export const Default = IllustratedMessageGroup.bind({});
-Default.args = {
-	heading: "Error 404: Page not found",
-	description: [
-		"This page isn't available. Try checking the URL or visit a different page.",
-	],
-};
+Default.args = {};
 
 /**
- * To use the accent color, the class `.spectrum-IllustratedMessage-accent` can be added to element(s) within the illustration SVG.
- */
-export const AccentColor = Template.bind({});
-AccentColor.tags = ["!dev"];
-AccentColor.args = {
-	heading: "Drag and drop your file",
-	description: [
-		() => {
-			return html`${Link({ url: "#", text: "Select a file" })} from your computer.`;
-		},
-	],
-	useAccentColor: true,
+ * Horizontal illustrated messages are displayed on extra large screen sizes or in a menu.
+*/
+export const Horizontal = Template.bind({});
+Horizontal.tags = ["!dev"];
+Horizontal.args = {
+	isHorizontal: true
 };
-AccentColor.parameters = {
+Horizontal.parameters = {
 	chromatic: { disableSnapshot: true },
 };
 
@@ -87,4 +102,22 @@ WithForcedColors.parameters = {
 		forcedColors: "active",
 		modes: disableDefaultModes
 	},
+};
+/**
+ * Illustrated message comes in three sizes: small, medium, and large.
+ * - Small illustrated message is typically used in quick or in-line actions.
+ * - The medium size is the default, and often used in panels.
+ * - The large size illustrated message is generally used in full page layouts and dialogs.
+ */
+export const Sizing = (args, context) => Sizes({
+	Template: Template,
+	withBorder: false,
+	withHeader: false,
+	...args
+}, context);
+
+Sizing.args = {};
+Sizing.tags = ["!dev"];
+Sizing.parameters = {
+	chromatic: { disableSnapshot: true },
 };
