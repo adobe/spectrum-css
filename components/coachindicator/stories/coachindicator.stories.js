2b0d635dbe5f3f47594225c33b6a101d165d1411
5t3ph <thinkdobecreate@gmail.com>
2024-10-17 12:19:52 -0500

    fix(coachindicator): adds 'static white', marks 'light' and 'dark' for deprecation

--- components/coachindicator/stories/coachindicator.stories.js
+++ components/coachindicator/stories/coachindicator.stories.js
@@ -3,12 +3,14 @@ import { isQuiet } from "@spectrum-css/preview/types";
 import metadata from "../metadata/metadata.json";
 import packageJson from "../package.json";
 import { CoachIndicatorGroup } from "./coachindicator.test.js";
-import { AllVariantsCoachIndicatorGroup } from "./template.js";
+import { AllVariantsCoachIndicatorGroup, StaticWhiteCoachIndicator } from "./template.js";
 
 /**
  * The coach indicator component can be used to bring added attention to specific parts of a page.
  *
  * Coach indicator is primarily used along with the [coach mark](/docs/components-coach-mark--docs) component.
+ *
+ * **Deprecation notice for S2**: The "dark" variant will be deprecated entirely, and "light" variant will be deprecated in favor of "static white".
  */
 export default {
 	title: "Coach indicator",
@@ -22,7 +24,7 @@ export default {
 				type: { summary: "string" },
 				category: "Component",
 			},
-			options: ["default", "dark", "light"],
+			options: ["default", "dark", "light", "staticWhite"],
 			control: "select"
 		},
 	},
@@ -67,6 +69,16 @@ QuietVariants.args = {
 	isQuiet: true,
 };
 QuietVariants.parameters = {
+	chromatic: { disableSnapshot: true }
+};
+
+/**
+ * When an indicator needs to be placed on top of a visual, use the static white option. Static white does not change values depending upon the color theme.
+*/
+export const StaticWhite = StaticWhiteCoachIndicator.bind({});
+StaticWhite.tags = ["!dev"];
+StaticWhite.storyName = "Static White";
+StaticWhite.parameters = {
 	chromatic: { disableSnapshot: true }
 };
 
