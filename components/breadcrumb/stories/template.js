df2b0a2cc378c95847d13fad7d1b94353d03e9a0
Josh Winn <965114+jawinn@users.noreply.github.com>
2024-10-31 15:19:13 -0400

    fix(breadcrumb): latest breadcrumb css and template from main
    Current changes on main, including bugfixes, to assist with testing with
    s2, until the base branch is updated with main.

--- components/breadcrumb/stories/template.js
+++ components/breadcrumb/stories/template.js
@@ -1,71 +1,82 @@
-import { html } from "lit";
-import { classMap } from "lit/directives/class-map.js";
-import { when } from "lit/directives/when.js";
-
 import { Template as ActionButton } from "@spectrum-css/actionbutton/stories/template.js";
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
+import { html } from "lit";
+import { classMap } from "lit/directives/class-map.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import { when } from "lit/directives/when.js";
 
 import "../index.css";
 
-export const Template = ({
-	rootClass = "spectrum-Breadcrumbs",
-	customClasses = [],
-	items = [],
-	variant,
-	isDragged = false,
-	...globals
-}) => html`
+export const Template = (
+	{
+		rootClass = "spectrum-Breadcrumbs",
+		customClasses = [],
+		items = [],
+		variant,
+		isDragged = false,
+	} = {},
+	context = {},
+) => html`
 	<nav>
 		<ul
 			class=${classMap({
 				[rootClass]: true,
-				[`${rootClass}--${variant}`]: typeof variant !== "undefined",
+				[`${rootClass}--${variant}`]: typeof variant !== "undefined" || variant !== "default",
 				...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
 			})}
 		>
 			${items.map((item, idx, arr) => {
-				const { label, isDisabled, iconName } = item;
+				const { label, isDisabled, iconName, iconSet } = item;
 				return html` <li
 					class=${classMap({
 						[`${rootClass}-item`]: true,
-						"is-disabled": isDisabled,
 						"is-dragged": isDragged && item.isDragged,
 					})}
 				>
 					${when(
 						iconName,
 						() =>
-							ActionButton({
-								...globals,
-								iconName,
-								isDisabled,
-								isQuiet: true,
-								customIconClasses: [`${rootClass}-folder`],
-								size: "m",
-							}),
+							ActionButton(
+								{
+									iconName,
+									iconSet,
+									isDisabled,
+									isQuiet: true,
+									customIconClasses: [`${rootClass}-folder`],
+									size: "m",
+								},
+								context,
+							),
 						() =>
 							when(
 								idx !== arr.length - 1,
 								() =>
 									html`<div
-										class="${rootClass}-itemLink"
+										class=${classMap({
+											[`${rootClass}-itemLink`]: true,
+											"is-disabled": isDisabled,
+										})}
+										aria-disabled=${ifDefined(isDisabled ? "true" : undefined)}
 										role="link"
-										tabindex="0"
+										tabindex=${ifDefined(!isDisabled ? "0" : undefined)}
 									>
 										${label}
 									</div>`,
 								() =>
 									html`<a class="${rootClass}-itemLink" aria-current="page"
 										>${label}</a
-									>`
-							)
+									>`,
+							),
 					)}
 					${when(idx !== arr.length - 1, () =>
-						Icon({
-							...globals,
-							iconName: "ChevronRight100",
-							customClasses: [`${rootClass}-itemSeparator`],
-						})
+						Icon(
+							{
+								iconName: "ChevronRight100",
+								setName: "ui",
+								customClasses: [`${rootClass}-itemSeparator`],
+							},
+							context,
+						),
 					)}
 				</li>`;
 			})}
