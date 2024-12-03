75c17f04c1b814aa77acdda9e3bfd8c520649e72
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-12-03 11:37:54 -0500

    chore: creates rgb-reporter.js
    - parses the tokens/dist/index.css
    - returns the *-rgb tokens

---
+++ tasks/rgb-reporter.js
@@ -0,0 +1,90 @@
+/*!
+ * Copyright 2024 Adobe. All rights reserved.
+ *
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/* eslint-disable no-console */
+
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+
+// const postcss = require("postcss");
+// const prettier = require("prettier");
+
+const DIRECTORY_SRC = path.parse("tokens");
+const inputDir = DIRECTORY_SRC.name;
+
+// const OUTPUT_DIR = path.join(DIRECTORY_SRC, 'csv-output');
+
+// Function to find and return custom properties containing `-rgb` and `-opacity`
+async function findCustomProperties(input) {
+  const DISTCSS = path.join(input, "dist", "index.css");
+
+  try {
+    // Check if the dist file exists
+    await fsp.access(DISTCSS);
+
+    // Read the CSS file async
+    const cssContent = await fsp.readFile(DISTCSS, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if(data.includes("-rgb")) {
+      // console.log(data);
+      return data;
+      }
+    });
+
+    // the synchronous version
+    // const cssContent = fs.readFileSync(DISTCSS, 'utf8', (err, data) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    //   if(data.includes("-rgb")) {
+    //   // console.log(data);
+    //   return data;
+    //   }
+    // });
+
+    // Regular expression to match custom properties with `-rgb` or `-opacity`
+    const regex = /(--[\w-]*-(?:rgb)):.*;/g;
+    // const regex = /(--[\w-]*-(?:rgb|opacity)):.*;/g;
+
+    // Find all matches
+    const matches = [...cssContent.matchAll(regex)];
+
+    // Extract matched lines
+    const matchedLines = matches.map(match => match[0]);
+
+    // Output the matches
+    console.log(`Found ${matchedLines.length} custom properties:`);
+    matchedLines.forEach(line => console.log(line));
+
+    return matchedLines;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error(`File not found: ${DISTCSS}`);
+    } else {
+      console.error(`Error reading file: ${err.message}`);
+    }
+  }
+}
+
+(async () => {
+  await findCustomProperties(inputDir);
+})();
+
+// console.log("first pass:", findCustomProperties(inputDir));
+
+
