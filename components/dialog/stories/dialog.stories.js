bd934cc9a5a43b2d453710d462a1faaa5046de08
Marissa Huysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>
2024-12-09 17:36:29 -0500

    feat(dialog): s2 standard dialog migration (#2860)
    * feat(dialog)!: s2 standard dialog migration
    
    * docs(dialog): dialog controls/coverage
    - adds new features/behaviors such as the checkbox in the footer,
    wrapping for footer/header content, being able to have close button and
    button group, hasHeroImage
    - removes Divider support in dialog
    - adds is-hidden-story to replicate what is on main and remove most
    stories from the sidebar
    - adds some comments and TODOs giving context to this choice
    - removes layout from control table from default argTypes
    - removes hasFooter from control table for fullscreen/fullscreenTakeover
    argTypes
    - adds a comment to explain the custom argTypes changes for those stories
    - also adds a comment about multiple conditionals for argTypes
    
    * refactor(modal): use corner-radius-extra-large-default to match design
    specs
    
    * fix(underlay): update comment notes for spectrum-overlay-color
    - also comments out a duplicated custom property and adds a TODO comment
    
    * refactor(dialog): new css for S2 dialog
    - adds some opportunities for mod properties
    - fixes dismissible grid spacing between additional header content and
    close button
    - scope passthrough mods to spectrum-Dialog class
    - add flex-direction: column to footer (this should allow the footer
    content and button group to stack at small screens at smallest dialog
    sizes on the mobile scale.)
    
    * feat(dialog): rebuild mods
    - lots of mods are renamed to use "standard dialog" language
    
    * chore(dialog): create changeset
    
    * chore(dialog): update peer deps data
    - remove divider from peerDependencies and peerDependenciesMeta
    - adds closebutton, checkbox and typography to peerDependenciesMeta
    
    * chore: remove divider tests
    
    * chore(buttongroup): add flex-wrap mod property
    
    * chore(buttongroup): creates buttongroup changeset
    
    * chore(buttongroup,dialog): rebuild mods
    
    * fix(dialog): platform custom variables
    - calls for --spectrum-standard-dialog-spacing-edge-to-content
    
    * chore(tokens): create changeset for custom standard dialog token
    
    * chore(tray): fix spelling of isDismissible arg
    
    * fix(dialog,modal): custom styles for modal background color
    - because modal was originally setting the background color of dialogs,
    there was some 'antialiasing bleed through' behind the dialog. By adding
    customStyles to Modal, we can set --mod-modal-background-color to
    transparent and get rid of the hairline color (especially noticeable
    behind a hero image in a dialog)
    
    * docs(dialog): adds modal background color documentation
    
    * fix(dialog): use font-stack instead of font-family
    
    * docs(dialog): rebase fixes
    - override layout:centered parameter with layout:padded instead so the
    dialogs are visible on their canvases on the docs page.

--- components/dialog/stories/dialog.stories.js
+++ components/dialog/stories/dialog.stories.js
@@ -1,9 +1,15 @@
-import { Template } from "./template";
-
 import { Template as Typography } from "@spectrum-css/typography/stories/template.js";
+import { version } from "../package.json";
+// import { DialogFullscreen, DialogFullscreenTakeover, DialogGroup } from "./dialog.test.js";
+import { Template } from "./template.js";
 
 /**
- * A dialog displays important information that users need to acknowledge. They appear over the interface and block further interactions.
+ * A dialog displays important information that users need to acknowledge. They appear over the interface and block further interactions. Standard dialogs are the most frequent type of dialogs. They appear in the center of the screen over the interface and should be used for moderately complex tasks. Takeover dialogs are large types of dialogs. They use the totality of the screen and should be used for modal experiences with complex workflows.
+ *
+ * ## Usage with modal component
+ * When a dialog component is used in tandem with a [modal](/docs/components-modal--docs), implementations should set `--mod-modal-background-color` to `transparent`. This will prevent any background color used in the modal from peeking through from behind the dialog at the rounded corners, allowing the dialog's background color to take precedence.
+ *
+ * The alert variants that were previously a part of Dialog were moved to their own component, [alert dialog](/docs/components-alert-dialog--docs).
  */
 export default {
 	title: "Dialog",
@@ -11,22 +17,89 @@ export default {
 	argTypes: {
 		heading: {
 			name: "Heading",
+			description: "Title for the dialog.",
 			type: { name: "string" },
 			table: {
 				type: { summary: "string" },
-				category: "Component",
+				category: "Content",
+			},
+			control: { type: "text" },
+		},
+		header: {
+			name: "Additional header content",
+			description: "Controls header content.",
+			type: { name: "string" },
+			table: {
+				type: { summary: "string" },
+				category: "Content",
 			},
 			control: { type: "text" },
 		},
 		content: { table: { disable: true } },
-		isDismissable: {
-			name: "Dismissable",
+		hasFooter: {
+			name: "Has footer",
+			description: "Adds a footer to the dialog, containing the button group, checkbox, and footer text. Currently only supported in the default layout.",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Content",
+			},
+			control: "boolean",
+		},
+		footer: {
+			name: "Footer text",
+			description: "Text content of the dialog footer. Represents the checkbox label if a checkbox is present, or stands alone if there is no checkbox. Currently only supported in the default layout.",
+			type: { name: "string" },
+			table: {
+				type: { summary: "string" },
+				category: "Content",
+			},
+			control: { type: "text" },
+			if: { arg: "hasFooter", truthy: true, },
+		},
+		hasCheckbox: {
+			name: "Has checkbox",
+			description: "Adds a checkbox to the footer content. Currently only supported in the default layout.",
+			type: { name: "boolean" },
+			table: {
+				type: { summary: "boolean" },
+				category: "Content",
+			},
+			control: { type: "boolean" },
+			if: { arg: "hasFooter", truthy: true, },
+		},
+		size: {
+			name: "Size",
+			type: { name: "string", required: true },
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+			},
+			options: ["s", "m", "l"],
+			control: "select",
+		},
+		layout: {
+			name: "Layout",
+			type: { name: "string" },
+			defaultValue: "Default",
+			table: {
+				type: { summary: "string" },
+				category: "Component",
+				defaultValue: { summary: "Default" },
+				disable: true,
+			},
+			options: ["default", "fullscreen", "fullscreenTakeover"],
+			control: "select",
+		},
+		isDismissible: {
+			name: "Dismissible",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
 				category: "Component",
 			},
 			control: "boolean",
+			if: { arg: "layout", eq: "default" },
 		},
 		showModal: {
 			name: "Wrap the dialog in a modal",
@@ -46,39 +119,206 @@ export default {
 			},
 			control: "boolean",
 		},
+		hasHeroImage: {
+			name: "Has hero image",
+			type: { name: "boolean" },
+			description: "Adds a cover image to the header of a dialog.",
+			table: {
+				type: { summary: "boolean" },
+				category: "Content",
+			},
+			control: "boolean",
+			if: { arg: "layout", eq: "default" },
+		},
+		heroImageUrl: {
+			name: "Hero Image",
+			type: { name: "string" },
+			description: "Select a cover image for the hero section of a dialog.",
+			defaultValue: "example-card-portrait.png",
+			table: {
+				type: { summary: "string" },
+				category: "Content",
+				defaultValue: { summary: "example-card-portrait.png" },
+			},
+			control: { type: "file", accept: ".svg,.png,.jpg,.jpeg,.webc" },
+			if: { arg: "hasHeroImage", truthy: true },
+		},
 	},
 	args: {
 		rootClass: "spectrum-Dialog",
-		isDismissable: true,
-		showModal: false,
+		hasFooter: true,
+		footer: "Do not show this message again.",
+		isDismissible: false,
 		isOpen: true,
+		showModal: false,
+		size: "m",
+		layout: "default",
+		hasCheckbox: true,
+		hasHeroImage: false,
 	},
 	parameters: {
 		actions: {
-			handles: ["click .spectrum-Dialog button"],
+			handles: [],
 		},
 		docs: {
 			story: {
+				// inline: false,
 				height: "500px",
 			},
 		},
+		componentVersion: version,
 		status: {
 			type: "migrated",
 		},
+		layout: "padded",
 	},
 };
 
+const ExampleContent = "Standard dialog description. This should briefly communicate any additional information or context about the standard dialog title, to help users make one of the decisions offered by the buttons. Make it no more than a few short sentences.";
+
+/**
+ * The default size for dialog is medium. The default dialog also has a checkbox in the footer.
+ */
 export const Default = Template.bind({});
 Default.args = {
 	heading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+	header: "* Required",
 	showModal: true,
 	content: [
-		() => Typography({
+		(passthroughs, context) =>  Typography({
 			semantics: "body",
 			size: "m",
-			content: [
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor augue mauris augue neque gravida. Libero volutpat sed ornare arcu. Quisque egestas diam in arcu cursus euismod quis viverra. Posuere ac ut consequat semper viverra nam libero justo laoreet. Enim ut tellus elementum sagittis vitae et leo duis ut. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Diam volutpat commodo sed egestas egestas. Dolor magna eget est lorem ipsum dolor. Vitae suscipit tellus mauris a diam maecenas sed. Turpis in eu mi bibendum neque egestas congue. Rhoncus est pellentesque elit ullamcorper dignissim cras lobortis."
-			]
-		}),
+			content: [ ExampleContent ],
+			...passthroughs,
+		}, context),
 	],
+};
+
+/* TODO: For all dialog stories: the "is-hidden-story" tags in older versions of Storybook. In newer versions,
+use "!dev" to remove the stories from the side navigation, reflecting the intended behavior.
+Remove "is-hidden-story" in favor of "!dev" tags when possible.
+*/
+// ********* DOCS ONLY ********* //
+export const DefaultSmall = Template.bind({});
+DefaultSmall.tags = ["is-hidden-story", "!dev"];
+DefaultSmall.storyName = "Dialog - small",
+DefaultSmall.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+DefaultSmall.args = {
+	...Default.args,
+	size: "s",
+};
+
+export const DefaultLarge = Template.bind({});
+DefaultLarge.tags = ["is-hidden-story", "!dev"];
+DefaultLarge.storyName = "Dialog - large",
+DefaultLarge.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+DefaultLarge.args = {
+	...Default.args,
+	size: "l",
+};
+
+/**
+ * A dialog that can be dismissed without taking an action.
+ */
+export const Dismissible = Template.bind({});
+Dismissible.tags = ["is-hidden-story", "!dev"];
+Dismissible.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+Dismissible.args = {
+	...Default.args,
+	isDismissible: true,
+};
+
+/**
+ * Dialogs can have a hero or cover image header.
+ */
+export const WithHero = Template.bind({});
+WithHero.tags = ["is-hidden-story", "!dev"];
+WithHero.storyName = "With hero image";
+WithHero.parameters = {
+	docs: {
+		story: {
+			height: "650px",
+		},
+	},
+	chromatic: { disableSnapshot: true },
+};
+WithHero.args = {
+	...Default.args,
+	hasHeroImage: true,
+};
+
+/**
+ * The content inside the body area can scroll when necessary.
+ */
+export const WithScroll = Template.bind({});
+WithScroll.args = {
+	...Default.args,
+	content: [ ExampleContent, ExampleContent, ExampleContent, ExampleContent ],
+	customStyles: {
+		"max-block-size": "400px",
+	}
+};
+WithScroll.storyName = "Scrollable";
+WithScroll.tags = ["is-hidden-story", "!dev"];
+WithScroll.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+/* TODO: once this gets rebased with `main`, make sure to use the fullscreen and fullscreenTakeover test templates
+that are imported above, but commented out.
+ */
+/**
+ * The full screen variant shows a large dialog background, only revealing a small portion of the page around the outside of the dialog, behind an overlay. The size of the dialog varies with the size of the screen, in both width and height.
+ */
+export const Fullscreen = Template.bind({});
+Fullscreen.args = {
+	...Default.args,
+	layout: "fullscreen",
+	hasFooter: false,
+};
+Fullscreen.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+// TODO: Because storybook doesn't support for multiple conditionals, we've removed the hasFooter
+// arg from the control table for the fullscreen and fullscreenTakeover stories only. Ideally, we
+// could have some of our controls display when more than one arg is met to avoid custom argTypes.
+// For instance, hasCheckbox would appear when layout: "default", AND footer is truthy.
+// https://github.com/storybookjs/storybook/discussions/18542
+Fullscreen.argTypes = {
+	hasFooter: { table: { disable: true, } },
+};
+
+/**
+ * The full screen takeover variant is similar to the full screen variant except that the background covers the entire screen. The page behind the dialog is not visible. This variant should be reserved for workflows where displaying a second dialog on top of the first one is to be expected.
+ */
+export const FullscreenTakeover = Template.bind({});
+FullscreenTakeover.storyName = "Fullscreen takeover";
+FullscreenTakeover.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+FullscreenTakeover.args = {
+	...Default.args,
+	layout: "fullscreenTakeover",
+	hasFooter: false,
+	content: [ ExampleContent, ExampleContent, ExampleContent, ExampleContent, ExampleContent, ExampleContent, ExampleContent, ExampleContent ],
+};
+FullscreenTakeover.argTypes = {
+	hasFooter: { table: { disable: true, } },
+};
+
+// ********* VRT ONLY ********* //
+export const WithForcedColors = Default.bind({});
+WithForcedColors.args = Default.args;
+WithForcedColors.tags = ["!autodocs", "!dev"];
+WithForcedColors.parameters = {
+	chromatic: {
+		forcedColors: "active",
+	},
 };
