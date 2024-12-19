82800d1d3ee22167e9e76b410dd71b9a3cd3b513
Marissa Huysentruyt <69602589+marissahuysentruyt@users.noreply.github.com>
2024-12-19 15:36:03 -0500

    chore(calendar,card,coachmark): remove MDX files (#3443)
    * chore(card): remove default args from test cases
    
    * docs(card): remove MDX file
    
    - adds some missing documentation regarding stories
    - reorganizes some information to sit with appropriate story/variant
    - migrates documentation from MDX file to the stories file instead
    
    * docs(coachmark): remove MDX file
    
    - adds some missing documentation regarding stories
    - reorganizes some information to sit with appropriate story/variant
    - migrates documentation from MDX file to the stories file instead
    
    * chore(coachmark): adds extra chromatic coverage
    
    * docs(calendar): remove MDX file
    
    - migrates documentation from MDX file to the stories file instead
    
    * docs(calendar): clarifies days of the week control name
    
    * docs(card): clarify quiet control requirements
    
    * chore(coachmark): remove html wrapper in favor of content array
    
    * chore(card): fix isQuiet controls and usage in template
    
    * docs(asset,card): isCardAssetOverride renamed to isImageFill

--- components/coachmark/stories/coachmark.test.js
+++ components/coachmark/stories/coachmark.test.js
@@ -29,7 +29,6 @@ export const CoachMarkGroup = Variants({
 			testHeading: "Default",
 			hasActionMenu: false,
 			hasPagination: false,
-			hasImage: false,
 			wrapperStyles: {
 				"background-color": "var(--spectrum-gray-50, white)",
 			},
@@ -46,18 +45,31 @@ export const CoachMarkGroup = Variants({
 		{
 			testHeading: "With action menu",
 			hasPagination: false,
-			hasActionMenu: true,
-			hasImage: false,
 			isOpen: true,
 			wrapperStyles: {
 				"background-color": "var(--spectrum-gray-50, white)"
 			},
 		},
 		{
+			testHeading: "With action menu + media",
+			hasPagination: false,
+			isOpen: true,
+			hasImage: true,
+			wrapperStyles: {
+				"background-color": "var(--spectrum-gray-50, white)"
+			},
+		},
+		{
 			testHeading: "With pagination",
-			hasPagination: true,
 			hasActionMenu: false,
-			hasImage: false,
+			wrapperStyles: {
+				"background-color": "var(--spectrum-gray-50, white)"
+			},
+		},
+		{
+			testHeading: "With pagination + media",
+			hasActionMenu: false,
+			hasImage: true,
 			wrapperStyles: {
 				"background-color": "var(--spectrum-gray-50, white)"
 			},
