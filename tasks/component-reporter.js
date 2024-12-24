50016d1f11e55c0d729fddd4e82913a56e71ccd9
castastrophe <castastrophe@users.noreply.github.com>
2024-12-24 15:47:10 -0500

    build: remove theme processing, site package; add reporter

--- tasks/component-reporter.js
+++ tasks/component-reporter.js
@@ -184,7 +184,6 @@ async function main({
 			dataModel: {
 				modifiers: ["mod"],
 				spectrum: ["spectrum"],
-				"system-theme": ["system"],
 				"high-contrast": ["highcontrast"],
 			},
 		}
@@ -204,7 +203,6 @@ async function main({
 					modifiers: meta.modifiers,
 					component: meta.component,
 					global: meta.global,
-					"system-theme": meta["system-theme"],
 					passthroughs: meta.passthroughs,
 					"high-contrast": meta["high-contrast"],
 				}, null, 2),
