17d1b58b9cd924dae481fb079fa21bd874a18b78
Cory Dransfeldt <cdransfeldt@adobe.com>
2025-01-02 13:39:25 -0800

    chore(coachmark): adds description to imageIsFixedHeight control

--- components/coachmark/stories/coachmark.stories.js
+++ components/coachmark/stories/coachmark.stories.js
@@ -42,6 +42,7 @@ export default {
 		},
 		imageIsFixedHeight: {
 			name: "Image fixed height",
+			description: "By default this displays an image with a 4:3 aspect ratio.",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
