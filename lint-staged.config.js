ea56978bb0a636601869960496c39830826fbe80
5t3ph <thinkdobecreate@gmail.com>
2025-01-06 09:22:32 -0600

    Revert incorrect main merge

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,21 +1,20 @@
 module.exports = {
-	"*.css": [
-		"stylelint --fix --cache --allow-empty-input --report-descriptionless-disables --report-invalid-scope-disables --report-needless-disables",
-		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc",
+	"*.css,!plugins/*/{expected,fixtures}/*.css": [
+		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write",
 	],
 	"*.{js,json}": [
 		"eslint --fix --cache --no-error-on-unmatched-pattern --quiet --ignore-pattern \"!.storybook/\""
 	],
 	"dist/*.css": [
-		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc"
+		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write"
 	],
 	"components/*/metadata/metadata.json": (files) => {
 		return [
 			...(files.map(file => `pajv test --valid -s ./schemas/metadata.schema.json -d "${file}"`) ?? []),
-			`prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write ${files.join(" ")} --config .prettierrc`
+			`prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write ${files.join(" ")}`
 		];
 	},
 	"*.{md,mdx}": [
-		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc"
+		"prettier --no-config --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write"
 	]
 };
