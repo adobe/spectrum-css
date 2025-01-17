85ab196a1a69de87b3d13931bb34ad1626eb1816
Aziz Ramos <abdulr@adobe.com>
2025-01-17 15:49:51 -0500

    fix(illustratedmessage): resolving changes

--- components/illustratedmessage/stories/illustratedmessage.test.js
+++ components/illustratedmessage/stories/illustratedmessage.test.js
@@ -1,6 +1,4 @@
-import { Template as Link } from "@spectrum-css/link/stories/template.js";
 import { Variants } from "@spectrum-css/preview/decorators";
-import { html } from "lit";
 import { Template } from "./template.js";
 
 export const IllustratedMessageGroup = Variants({
@@ -10,16 +8,6 @@ export const IllustratedMessageGroup = Variants({
 	{
 		testHeading: "Horizontal layout",
 		isHorizontal: true
-	},
-	{
-		testHeading: "With link",
-		title: "Drag and drop your file",
-		description: [
-			() => {
-				return html`${Link({ url: "#", text: "Select a file" })} from your computer.`;
-			},
-		],
-		hasButtons: false
 	},
 	{
 		testHeading: "W/o button group",
