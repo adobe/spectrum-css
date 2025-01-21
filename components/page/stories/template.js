7582c1f5c73de5ecf962bfad0294f162d7035751
Cory Dransfeldt <cdransfeldt@adobe.com>
2025-01-21 13:48:21 -0800

    fix(*): define missing custom properties from theme directory (#3517)
    * fix(*): define missing custom properties from theme directory
    
    * fix(*): restore missing mods and tokens
    
    * fix(*): restore missing mod and update metadata
    
    * fix(*): restore missing mods and update metadata
    
    * fix(*): restore basebutton import
    
    * chore(*): update metadata
    
    This reverts commit 3d489fefc50a94a3bf05d3f05505349a600cf00d.
    
    * fix(*): restore actionbutton staticwhite background color
    
    * chore(*): update metadata
    
    * chore: resolve conflict
    
    * fix(*): actionbutton emphasized + static conflict
    
    * fix(*): actionbutton restore missing mods
    
    * fix(*): actionbutton clean up mods; fix missing states
    
    * fix(*): actionbutton restore focus indicator mods
    
    * chore(*): remove unused theme files

--- components/page/stories/template.js
+++ components/page/stories/template.js
@@ -2,9 +2,6 @@ import { renderContent } from "@spectrum-css/preview/decorators";
 import { html } from "lit";
 
 import "../index.css";
-import "../themes/spectrum.css";
-/* Must be imported last */
-import "../themes/express.css";
 
 export const Template = ({ content = [] } = {}, context = {}) => {
 	return html`${renderContent(content, { context })}`;
