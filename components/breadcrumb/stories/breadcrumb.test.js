213e652f557a2032200bc9ad5be599b1b0410af2
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-01-23 17:22:36 -0500

    docs(breadcrumb): refactor tests
    Update VRTs tests file to use new refactored controls and updated s2
    variants. More options can now live in the state data. Disabled and
    dragged was also moved to state data item instead of applying to all
    tests.

--- components/breadcrumb/stories/breadcrumb.test.js
+++ components/breadcrumb/stories/breadcrumb.test.js
@@ -1,109 +1,54 @@
 import { Variants } from "@spectrum-css/preview/decorators";
 import { Template } from "./template.js";
 
-const DefaultItems = [
-	{
-		label: "Nav root",
-	},
-	{
-		label: "Dragged",
-		isDragged: true,
-	},
-	{
-		label: "Disabled sub item",
-		isDisabled: true,
-	},
-	{
-		label: "January 2019 assets",
-	},
-];
-
-const NestedItems = [
-	{
-		iconName: "FolderOpen",
-		iconSet: "workflow",
-	},
-	{
-		label: "Dragged",
-		isDragged: true,
-	},
-	{
-		label: "Disabled",
-		isDisabled: true,
-	},
-	{
-		label: "January 2019 assets",
-	},
-];
-
-const NestedRootVisibleItems = [
-	{
-		label: "Nav root",
-	},
-	{
-		iconName: "FolderOpen",
-		iconSet: "workflow",
-		isDisabled: true,
-	},
-	{
-		label: "Dragged sub item",
-		isDragged: true,
-	},
-	{
-		label: "January 2019 assets",
-	},
-];
-
 export const BreadcrumbGroup = Variants({
 	Template,
 	testData: [
 		{
 			testHeading: "Default",
-			items: DefaultItems,
 		},
 		{
-			testHeading: "Default (nested)",
-			items: NestedItems,
-		},
-		{
-			testHeading: "Default (nested, root visible)",
-			items: NestedRootVisibleItems,
-		},
-		{
-			testHeading: "Compact",
-			variant: "compact",
-			items: DefaultItems,
-		},
-		{
-			testHeading: "Compact (nested)",
-			variant: "compact",
-			items: NestedItems,
-		},
-		{
-			testHeading: "Compact (nested, root visible)",
-			variant: "compact",
-			items: NestedRootVisibleItems,
+			testHeading: "Large",
+			size: "l",
 		},
 		{
 			testHeading: "Multiline",
 			variant: "multiline",
-			items: DefaultItems,
 		},
 		{
-			testHeading: "Multiline (nested)",
+			testHeading: "Multiline with small heading",
 			variant: "multiline",
-			items: NestedItems,
-		},
-		{
-			testHeading: "Multiline (nested, root visible)",
-			variant: "multiline",
-			items: NestedRootVisibleItems,
+			titleHeadingSize: "s",
 		},
 	],
 	stateData: [
 		{
-			testHeading: "Dragged, disabled",
+			testHeading: "Nested",
+			showTruncatedMenu: true,
+		},
+		{
+			testHeading: "Nested with root context",
+			showTruncatedMenu: true,
+			showRootContext: true,
+		},
+		{
+			testHeading: "Disabled items and dragged item",
 			isDragged: true,
-		}
+			showTruncatedMenu: true,
+			truncatedMenuIsDisabled: true,
+			items: [
+				{
+					label: "Dragged item",
+					isDragged: true,
+				},
+				{
+					label: "Disabled sub item",
+					isDisabled: true,
+				},
+				{
+					label: "January 2019 assets",
+				},
+			]
+		},
 	]
 });
