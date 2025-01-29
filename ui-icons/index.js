0b9cb8fbad8ad60e92550347d08fd52be719e853
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-01-29 13:53:03 -0500

    feat(icon): updated s2 icons - workflow and ui
    Uses and updates to the latest icon sets for S2.
    
    ==== Workflow icons ====
    Uses the latest version of this package. There is a large difference
    between the set of icons available for S1 and S2. These differences are
    documented separately from this work.
    
    ==== UI icons ====
    Updates the ui-icons package with the latest set of icons. This includes
    deprecations and some new icons. See the change log for a full list of
    changes.
    
    The UI icons no longer contain both a medium and a large SVG asset. The
    file structure and loading have been changed to use the single asset.
    
    Co-authored-by: [ Cassondra ] <castastrophe@users.noreply.github.com>

--- ui-icons/index.js
+++ ui-icons/index.js
@@ -1,118 +1,169 @@
-const fs = require('fs');
+const fs = require("fs");
 const fsp = fs.promises;
-const path = require('path');
+const path = require("path");
 
-const fg = require('fast-glob');
-const { optimize } = require('svgo');
-const combine = require('svgcombiner');
-const svgstore = require('svgstore');
+const fg = require("fast-glob");
+const { optimize } = require("svgo");
 
-async function setupIconStores() {
-  const stores = new Map();
-  stores.set('all', svgstore());
+const svgstore = require("svgstore");
 
-  for (const variant of await fg(['*'], {
-    cwd: __dirname,
-    ignore: ['dist', 'node_modules'],
-    onlyDirectories: true,
-  })) {
-    stores.set(variant, svgstore());
-  }
+// Not using svgo's loadConfig because it doesn't support dynamic config files
+const getSvgoConfig = require("./svgo.config.js");
 
-  return stores;
+/**
+ * @description Clean the file name
+ */
+const cleanFileName = (file) => {
+	return file.replace(/^S2_/, "").replace(/Size/, "");
+};
+
+/**
+ * @description Process the SVG assets using svgo config settings
+ */
+async function processSVG(filePath, destPath) {
+	if (!fs.existsSync(filePath)) return;
+
+	// Read in the SVG contents
+	const contents = await fsp.readFile(filePath, "utf-8");
+	const result = optimize(contents, getSvgoConfig({
+		clean: true
+	}));
+
+	if (!result?.data) return;
+
+	const assetName = cleanFileName(path.basename(filePath, ".svg"));
+
+	return fsp.writeFile(path.join(destPath, `${assetName}.svg`), result.data);
+}
+
+/**
+ * @description Attempts to fetch icon assets from their source
+ */
+async function copySVGs() {
+	let source;
+
+	// workflow icons: @a4u/a4u-s2-icon-global-set-open-source-processed
+	try {
+		source = require.resolve("@a4u/a4u-s2-ui-icon-global-set", {
+			paths: [process.cwd(), path.join(__dirname, ".."), "~/"]
+		});
+	}
+	catch (e) { /* empty */ }
+
+	if (!source) return Promise.resolve();
+
+	const sourcePath = path.join(path.dirname(source), "assets");
+	if (!fs.existsSync(sourcePath)) return Promise.resolve();
+
+	const files = await fg(["svg/*.svg"], {
+		cwd: sourcePath,
+		onlyFiles: true,
+	});
+
+	if (!files || files.length === 0) return Promise.resolve();
+
+	const outputPath = path.join(__dirname, "dist", "svg");
+
+	if (fs.existsSync(outputPath)) {
+		// Remove existing assets to replace them fully with the new ones
+		fs.rmdirSync(outputPath, {
+			force: true,
+			recursive: true
+		});
+	}
+
+	// Recreate the folder to store the new assets
+	await fsp.mkdir(outputPath, {
+		force: true,
+		recursive: true
+	});
+
+	return Promise.all(
+		files.map(file =>
+			processSVG(
+				path.join(sourcePath, file),
+				outputPath
+			)
+		)
+	);
+}
+
+/**
+ * @description A custom alpha-numeric sort
+ * @param {string} a
+ * @param {string} b
+ * @returns number
+ */
+function alphaNumericSort (a, b) {
+	const aSet = a.match(/^([a-z]+)([0-9]+)\.svg$/i);
+	const bSet = b.match(/^([a-z]+)([0-9]+)\.svg$/i);
+	const aChar = aSet[1];
+	const bChar = bSet[1];
+
+	if (aChar !== bChar) return aChar > bChar ? 1 : -1;
+
+	const aInt = parseInt(aSet[2] ?? 0);
+	const bInt = parseInt(bSet[2] ?? 0);
+	return aInt - bInt;
 }
 
 async function main() {
-  // Not using svgo's loadConfig because it doesn't support dynamic config files
-  const getSvgoConfig = require('./svgo.config.js');
+	// This can only be run by an Adobe employee on the VPN with appropriate access configured on their machine
+	await copySVGs();
 
-  // Hash to hold all icons arranged by processed name
-  const icons = new Map();
-  const stores = await setupIconStores();
-  const variants = [...stores.keys()].filter((key) => key !== 'all');
+	// Hash to hold all icons arranged by processed name
+	const icons = [];
+	const store = svgstore();
 
-  const files = await fg([`{${variants.join(',')}}/*.svg`], {
-    cwd: __dirname,
-    onlyFiles: true,
-  });
+	const files = await fg(["*.svg"], {
+		cwd: path.join(__dirname, "dist", "svg"),
+		onlyFiles: true,
+	});
 
-  const promises = [];
-  for (const file of files) {
-    const assetName = path.basename(file, '.svg');
+	if (!files || files.length === 0) {
+		return Promise.resolve("No assets detected.");
+	}
 
-    // Capture metadata about the icon from the path and filename
-    const variant = path.dirname(file);
+	/**	Sorting here controls the order in which entries are added to the store
+	 * such that they print in the same order every time; preventing unnecessary diffs */
+	for (const file of files.sort(alphaNumericSort)) {
+		const assetName = path.basename(file, ".svg");
+		if (icons.includes(`${assetName}.svg`)) {
+			// eslint-disable-next-line no-console
+			console.log(`⚠️  The icon set already includes an icon with the name ${assetName}.`);
+		}
+		else icons.push(`${assetName}.svg`);
 
-    // Read in the SVG contents
-    const contents = await fsp.readFile(file, 'utf-8');
-    const result = optimize(contents, getSvgoConfig({ clean: true }));
+		const content = await fsp.readFile(path.join(__dirname, "dist", "svg", file), { encoding: "utf-8" });
+		store.add(assetName, content);
+	}
 
-    icons.set(assetName, {
-      ...(icons.has(assetName) ? icons.get(assetName) : {}),
-      [variant]: result.data,
-    });
+	// Write the icon array to icons.json as a resource
+	await fsp.writeFile(path.join(__dirname, "dist", "icons.json"), JSON.stringify(icons.sort(alphaNumericSort), null, 2));
 
-    stores.get(variant).add(assetName, result.data);
+	// Finally, we write out the store to disk
+	const dest = path.join(__dirname, "dist", "spectrum-css-icons.svg");
+	const config = getSvgoConfig({
+		idPrefix: "spectrum-css-icon",
+		removeViewBox: true,
+	});
 
-    const dest = path.join(__dirname, 'dist', file);
-    if (!fs.existsSync(path.dirname(dest))) {
-      fs.mkdirSync(path.dirname(dest), { recursive: true });
-    }
+	const result = optimize(store.toString(), config);
 
-    promises.push(fsp.writeFile(dest, result.data));
-  }
+	if (!result?.data) {
+		return Promise.reject("Encountered an error optimizing SVG assets via svgo.");
+	}
 
-  // Wait for all the SVG content to be read in and processed before continuing
-  await Promise.all(promises);
-
-  if (icons.size === 0) return;
-
-  // For each entry in the map, combine the SVG variants into a single SVG file with multiple symbols
-  promises.length = 0;
-  [...icons.entries()].map(([iconName, data]) => {
-    // Combine the SVG variants into a single SVG file with multiple symbols
-    const contents = combine(iconName, data);
-
-    // Add the combined SVG to the "all" store so we can generate a single SVG file with all icons
-    stores.get('all').add(iconName, contents);
-
-    // Write the combined SVG to the dist folder
-    const destPath = path.join(__dirname, 'dist/combined');
-    if (!fs.existsSync(destPath)) fs.mkdirSync(destPath, { recursive: true });
-
-    promises.push(fsp.writeFile(path.join(destPath, `${iconName}.svg`), optimize(contents, getSvgoConfig())?.data));
-  });
-
-  // Finally, we write out the stores to disk
-  for (const [identifier, store] of stores.entries()) {
-    const isFullSet = identifier === 'all';
-    const filename = isFullSet ? 'spectrum-css-icons' : `spectrum-css-icons-${identifier}`;
-    const dest = path.join(__dirname, `dist/${filename}.svg`);
-    const config = getSvgoConfig(
-      isFullSet
-        ? {
-          idPrefix: 'spectrum-css-icon',
-          removeViewBox: true,
-        }
-        : {},
-    );
-
-    const result = optimize(store.toString(), config);
-
-    if (!result?.data) continue;
-
-    promises.push(fsp.writeFile(dest, result?.data));
-  }
-
-  return Promise.all(promises);
+	return fsp.writeFile(dest, result?.data);
 }
 
 main()
-  .then(() => {
-    console.log('✔ Icons generated successfully.');
-    process.exit(0);
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+	.then(() => {
+		// eslint-disable-next-line no-console
+		console.log("✔ Icons generated successfully.");
+		process.exit(0);
+	})
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
