545df4382f72dfe791ca624fc3e4f4a47e312092
Josh Winn <965114+jawinn@users.noreply.github.com>
2025-02-18 15:12:23 -0500

    feat(icons)!: s2 icons - use latest workflow and ui icon sets (#3001)
    * feat(icon): updated s2 icons - workflow and ui
    
    Uses and updates to the latest icon sets for S2.
    
    ==== Workflow icons ====
    Uses the latest version of this package. There is a large difference
    between the set of icons available for S1 and S2. These differences are
    documented separately from this work.
    
    ==== UI icons ====
    Updates the ui-icons package with the latest set of icons. This includes
    deprecations and some new icons. See the change log for a full list of
    changes.
    
    The UI icons no longer contain both a medium and a large SVG asset. The
    file structure and loading have been changed to use the single asset.
    
    Co-authored-by: [ Cassondra ] <castastrophe@users.noreply.github.com>
    
    Also includes:
    * build(storybook): use latest 4.0.0 version of workflow icons
    * feat(icon): remove guessing of icon set if not provided
    * refactor(icon): refactors, cleans up, and documents template logic and utilities
    * feat(icon): exclude new ui icons not yet ready for production (CSS-1115)
    * feat(icon): exclude 22x20 workflow icons (CSS-1116)

--- components/icon/stories/utilities.js
+++ components/icon/stories/utilities.js
@@ -1,44 +1,192 @@
-// Imports an array of all icon names in the workflow set
-import iconOpts from "@adobe/spectrum-css-workflow-icons";
+// Import arrays containing data with all icon names in both icon sets.
+import workflowIconOpts from "@adobe/spectrum-css-workflow-icons/dist/manifest.json";
+import uiIconOpts from "@spectrum-css/ui-icons/dist/icons.json";
 
-export const workflowIcons = (iconOpts || []).map((icon) =>
-	icon.replace(/\.svg$/, "")
+/**
+ * Clean workflow icon name to strip out unnecessary prefix and postfix text.
+ * Also removes file extension if present.
+ * Example: "S2_Icon_AlertCircle_20_N" becomes "AlertCircle"
+ *
+ * @param {string} iconName
+ * @returns {string}
+ */
+export const cleanWorkflowIconName = (iconName) => (
+	iconName.replace(/\.svg$/, "").replace(/^S2_Icon_/, "").replace(/_N$/, "").replace(/_20$/, "")
 );
 
 /**
- * UI Icons have specific sizes represented by a number.
- * Each size has its own individual file and a CSS class with defined dimensions.
+ * Clean UI icon name to remove file extension and any unnecessary text.
+ *
+ * @param {string} iconName
+ * @returns {string}
  */
-export const uiIconSizes = {
-	"Arrow": ["75","100","200","300","400","500","600"],
-	"Asterisk": ["75","100","200","300"],
-	"Checkmark": ["50","75","100","200","300","400","500","600"],
-	"Chevron": ["50","75","100","200","300","400","500"],
-	"CornerTriangle": ["75","100","200","300"],
-	"Cross": ["75","100","200","300","400","500","600"],
-	"Dash": ["50","75","100","200","300","400","500","600"],
-	"SingleGripper": [],
-	"DoubleGripper": [],
-	"TripleGripper": [],
+export const cleanUiIconName = (iconName) => (
+	iconName.replace(/\.svg$/, "")
+);
+
+/**
+ * Get the ID of the icon within the sprite sheet.
+ *
+ * The sprite sheets are single SVG files containing all of the icons in the set, each with a
+ * unique ID. The ID can then be used to display the icon elsewhere by referencing the ID
+ * within an SVG <use> element.
+ *
+ * Example of the format of IDs within the different sprite sheets:
+ *   ui ID: #spectrum-css-icon-alert-triangle
+ *   workflow ID: #icon-spectrum-css-icon-Arrow100
+ *
+ * @param {string} iconName Icon name (original or cleaned).
+ * @param {string} setName Icon set.
+ * @returns {string} Icon name with original file name prefix and postfix added.
+ */
+export const getSpriteSheetName = (iconName, setName) => {
+	if (setName == "ui") {
+		return "spectrum-css-icon-" + iconName;
+	}
+	else if (setName == "workflow") {
+		// Use cleaned name, without file name prefix/postfix.
+		let iconID = cleanWorkflowIconName(iconName);
+
+		// These regexes convert camel case or pascal case strings into kebab-case:
+		// -------
+		// Matches a lowercase letter or digit followed by an uppercase letter and inserts a hyphen between them.
+		// E.g. "AddCircle" becomes "Add-Circle".
+		// Digits are included in the first grouping because of the exception of "3D" being "3-d" in the IDs.
+		iconID = iconID.replaceAll(/([a-z0-9])([A-Z])/g, "$1-$2");
+
+		// Matches an uppercase sequence followed by an uppercase letter with a lowercase letter.
+		// Replacing this separately helps prevent uppercase acronyms like CC and CCW from being split up by dashes.
+		iconID = iconID.replaceAll(/([A-Z]+)([A-Z][a-z])/g, "$1-$2");
+		// -------
+
+		// Underscores become dashes.
+		iconID = iconID.replace("_", "-");
+
+		// All IDs begin with this prefix and are lowercase.
+		iconID = "icon-" + iconID.toLowerCase();
+
+		return iconID;
+	}
+	else {
+		console.error("setSpriteSheetName received an invalid setName and could not create the ID.");
+		return "";
+	}
 };
 
 /**
- * List of UI icon names, corresponding to files.
+ * Sorted array with all the SVG workflow icon names, cleaned to remove file extension and
+ * unnecessary prefix and postfix text. These might look something like "3DAsset".
+ *
+ * Excludes 22x20 icons, which currently do not match workflow icon sizing.
  */
-export const uiIcons = Object.keys(uiIconSizes);
+export const workflowIconsCleaned = (workflowIconOpts?.svg ?? [])
+	.filter(iconName => !iconName.includes("22x20"))
+	.map(iconName => cleanWorkflowIconName(iconName))
+	.sort(alphaNumericSort);
 
 /**
- * List of all UI icon names for CSS. Chevron and Arrow have directional suffixes
- * for rotating the same base icon, e.g. Arrow becomes ArrowRight, ArrowDown, etc.
+ * Sorted array with all UI icon names, without the file extension.
  */
-export const uiIconsWithDirections = [
-	...uiIcons.filter((c) => !["Chevron", "Arrow"].includes(c)),
-	"ArrowRight",
-	"ArrowLeft",
-	"ArrowUp",
-	"ArrowDown",
-	"ChevronRight",
-	"ChevronLeft",
-	"ChevronUp",
-	"ChevronDown",
+export const uiIconsCleaned = (uiIconOpts || [])
+	.map(iconName => cleanUiIconName(iconName))
+	.sort(alphaNumericSort);
+
+/**
+ * @description A custom alpha-numeric sort that helps keep the order of the sizing numbers at the end of the string.
+ * @param {string} a
+ * @param {string} b
+ * @returns {number}
+ */
+function alphaNumericSort(a, b) {
+	const aSet = a.match(/^([a-zA-Z]+)([0-9]+)(\.svg)?$/i);
+	const bSet = b.match(/^([a-zA-Z]+)([0-9]+)(\.svg)?$/i);
+
+	// Handle cases where match fails
+	if (!aSet || !bSet) return 0;
+
+	// Compare characters at start of string (case-insensitive)
+	const aChar = aSet[1].toLowerCase();
+	const bChar = bSet[1].toLowerCase();
+	if (aChar !== bChar) return aChar > bChar ? 1 : -1;
+
+	// Compare numbers at end of string
+	const aInt = parseInt(aSet[2]);
+	const bInt = parseInt(bSet[2]);
+	return aInt - bInt;
+}
+
+/**
+ * Array of all UI icon names for CSS. Chevron and Arrow have directional suffixes
+ * for rotating the same base icon, e.g. Arrow becomes ArrowRight, ArrowDown, etc.
+ *
+ * @param {string[]} uiIcons Array of icon names.
+ * @returns {string[]}
+ */
+export const getUiIconsWithDirections = (uiIcons) => [
+	...uiIcons.filter((c) => !["Chevron", "Arrow"].some(prefix => c.startsWith(prefix))),
+	...uiIcons.filter((c) => ["Chevron", "Arrow"].some(prefix => c.startsWith(prefix))).map(i => i.replace(/(Chevron|Arrow)(\d{2,3})/, "$1Right$2")),
+	...uiIcons.filter((c) => ["Chevron", "Arrow"].some(prefix => c.startsWith(prefix))).map(i => i.replace(/(Chevron|Arrow)(\d{2,3})/, "$1Left$2")),
+	...uiIcons.filter((c) => ["Chevron", "Arrow"].some(prefix => c.startsWith(prefix))).map(i => i.replace(/(Chevron|Arrow)(\d{2,3})/, "$1Up$2")),
+	...uiIcons.filter((c) => ["Chevron", "Arrow"].some(prefix => c.startsWith(prefix))).map(i => i.replace(/(Chevron|Arrow)(\d{2,3})/, "$1Down$2")),
+].sort(alphaNumericSort);
+
+/**
+ * Array of all cleaned UI icon names for CSS. Including sizing numbers and directional suffixes.
+ */
+export const uiIconsWithDirections = getUiIconsWithDirections(uiIconsCleaned);
+
+/**
+ * Array of all unique base UI icon names without their sizing numbers.
+ */
+export const getUniqueUiIconBaseNames = (uiIcons) => [
+	...new Set(uiIcons.map(ui => ui.replace(/\d{2,3}$/, "")))
 ];
+
+/**
+ * Array of all base UI icon names (without sizing numbers) for CSS (including directional suffixes).
+ */
+export const uniqueUiIconBaseNames = getUniqueUiIconBaseNames(uiIconsWithDirections);
+
+/**
+ * Workflow icon sizes. Does not apply to UI icons. Note: XXL is not part of the design
+ * spec and may be deprecated in the future.
+ */
+export const workflowSizes = ["xs", "s", "m", "l", "xl", "xxl"];
+
+/**
+ * Create an object where the key is the UI icon name, and the value is an array of
+ * strings with every sizing number available.
+ *
+ * Returns something like the following:
+ * {
+ *   "Arrow": ['100', '400'],
+ *   "Asterisk": ['100', '200', '300'],
+ *   ...
+ * }
+ *
+ * @param {string[]} uiIcons Array of all UI icon names.
+ * @returns {object}
+ */
+export const getUiIconSizes = (uiIcons) => {
+	let uiIconSizes = {};
+
+	uiIcons.forEach(iconName => {
+		// UI icon name without the sizing number at the end.
+		const iconNameRoot = iconName.replace(/\d{2,3}$/, "");
+
+		// Sizing number at the end of the icon name (e.g. "100").
+		const iconNameSize = iconName.match(/\d{2,3}/g)?.[0];
+
+		// Add to object (with no duplicates).
+		uiIconSizes[iconNameRoot] = [
+			...new Set([
+				...uiIconSizes[iconNameRoot] ?? [],
+				...(iconNameSize ? [iconNameSize] : []),
+			])
+		];
+	});
+
+	return uiIconSizes;
+};
+
+export const uiIconSizes = getUiIconSizes(uiIconsWithDirections);
