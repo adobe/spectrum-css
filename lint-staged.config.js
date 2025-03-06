d10cd6a213c2fcd36377b2340105ca0af71a47fd
Cory Dransfeldt <cdransfeldt@adobe.com>
2025-03-06 12:59:42 -0800

    fix(lint-staged): add yarn command to lint staged to ensure the lock file is updated (#3603)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		"eslint --fix --cache --no-error-on-unmatched-pattern --quiet"
 	],
 	"package.json": (files) => [
-		"yarn constraints --fix",
+		"yarn constraints --fix && yarn install",
 		`eslint --fix --cache --no-error-on-unmatched-pattern --quiet ${files.join(" ")}`,
 	],
 	"dist/*.css": [
