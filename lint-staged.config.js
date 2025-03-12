dd9f170b0747f62dba721f764509b375d7667ea8
castastrophe <castastrophe@users.noreply.github.com>
2025-03-12 16:32:28 -0400

    chore: update release script to lean on lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,10 +6,14 @@ module.exports = {
 	"*.{js,json}": [
 		"eslint --fix --cache --no-error-on-unmatched-pattern --quiet"
 	],
-	"package.json": (files) => [
-		"yarn constraints --fix",
-		`eslint --fix --cache --no-error-on-unmatched-pattern --quiet ${files.join(" ")}`,
+	"*.{md,mdx}": [
+		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc",
+		"markdownlint --config .markdownlint.json --fix"
 	],
+	"package.json": () => ([
+		"yarn constraints --fix",
+		"yarn install --refresh-lockfile",
+	]),
 	"dist/*.css": [
 		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc"
 	],
@@ -17,9 +21,5 @@ module.exports = {
 		return [
 			...(files.map(file => `pajv test --valid -s ./schemas/metadata.schema.json -d "${file}"`) ?? []),
 		];
-	},
-	"*.{md,mdx}": [
-		"prettier --no-error-on-unmatched-pattern --ignore-unknown --log-level silent --write --config .prettierrc",
-		"markdownlint --config .markdownlint.json --fix"
-	]
+	}
 };
