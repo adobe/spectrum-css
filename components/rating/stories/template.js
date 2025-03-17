027424dc0cddc8c70d4d66e8d1294d5f47ad0f56
Cory Dransfeldt <cdransfeldt@adobe.com>
2025-03-17 12:39:47 -0700

    chore(rating): rating component hover logic

--- components/rating/stories/template.js
+++ components/rating/stories/template.js
@@ -21,6 +21,66 @@ export const Template = ({
 } = {}, context = {}) => {
 	const { updateArgs } = context;
 
+	document.addEventListener("DOMContentLoaded", function() {
+		const rating = document.getElementById(id);
+		if (!rating) return;
+		if (rating.classList.contains("is-disabled") || rating.classList.contains("is-readOnly")) return;
+		const icons = Array.from(rating.getElementsByClassName("spectrum-Rating-icon"));
+		let hoverIndex = -1;
+		let selectedIndex = -1;
+
+		const updateHoverState = () => {
+			icons.forEach((icon, index) => {
+				const activeStar = icon.querySelector(".spectrum-Rating-starActive");
+				const inactiveStar = icon.querySelector(".spectrum-Rating-starInactive");
+
+				if (index <= hoverIndex ||
+					(index <= selectedIndex && hoverIndex === -1)
+				) {
+					icon.classList.add("is-hoverSelection");
+					activeStar.style.display = "block";
+					inactiveStar.style.display = "none";
+				}
+				else {
+					icon.classList.remove("is-hoverSelection");
+					activeStar.style.display = "none";
+					inactiveStar.style.display = "block";
+				}
+			});
+		};
+
+		rating.addEventListener("mouseleave", function() {
+			icons.forEach(icon => {
+				icon.classList.remove("is-hoverSelection");
+				icon.querySelector(".spectrum-Rating-starActive").style.display = "";
+				icon.querySelector(".spectrum-Rating-starInactive").style.display = "";
+			});
+		});
+
+		icons.forEach((icon, index) => {
+			if (icon.classList.contains("is-selected")) selectedIndex = index;
+
+			icon.addEventListener("mouseover", function() {
+				hoverIndex = index;
+				updateHoverState();
+			});
+
+			icon.addEventListener("mouseleave", function(event) {
+				if (!rating.contains(event.relatedTarget)) {
+					hoverIndex = -1;
+					updateHoverState();
+				}
+			});
+
+			icon.addEventListener("click", function() {
+				selectedIndex = index;
+				updateHoverState();
+			});
+		});
+
+		updateHoverState();
+	});
+
 	return html`
 		<div
 			class=${classMap({
