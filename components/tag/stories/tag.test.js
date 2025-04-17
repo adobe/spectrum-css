daefae456ccbf5e419bbbc65ceffc1162321b185
Rise Erpelding <rise@heysparkbox.com>
2025-04-17 11:38:39 -0500

    test(tag): set up testing grid

--- components/tag/stories/tag.test.js
+++ components/tag/stories/tag.test.js
@@ -9,43 +9,104 @@ export const TagGroups = Variants({
 			testHeading: "Default",
 		},
 		{
-			testHeading: "Is removable",
-			hasClearButton: true,
-		},
-		{
-			testHeading: "With icon",
-			hasIcon: true,
-			iconName: "Info",
-		},
-		{
-			testHeading: "With avatar",
-			hasAvatar: true,
-			avatarUrl: "example-ava.png",
-		},
-		{
-			testHeading: "Truncated",
-			label: "Tag label that truncates when it gets too long",
-			customStyles: {
-				"max-inline-size": "200px"
-			}
-		}
-	],
-	stateData: [
-		{
-			testHeading: "Invalid",
-			isInvalid: true,
-		},
-		{
-			testHeading: "Disabled",
-			isDisabled: true,
+			testHeading: "Emphasized",
+			isEmphasized: true,
 		},
 		{
 			testHeading: "Selected",
 			isSelected: true,
 		},
 		{
-			testHeading: "Emphasized",
+			testHeading: "Emphasized, selected",
+			isEmphasized: true,
+			isSelected: true,
+		},
+		// show removable tags for these variants so hover and focus can be captured
+		{
+			testHeading: "Default, removable",
+			hasClearButton: true,
+		},
+		{
+			testHeading: "Emphasized, removable",
+			isEmphasized: true,
+			hasClearButton: true,
+		},
+		{
+			testHeading: "Selected, removable",
+			isSelected: true,
+			hasClearButton: true,
+		},
+		{
+			testHeading: "Disabled with states",
+			isDisabled: true,
+		},
+		// variants with visuals
+		{
+			testHeading: "Default, with icon",
+			iconName: "Circle",
+		},
+		{
+			testHeading: "Default, with avatar",
+			avatarUrl: "example-ava.png",
+		},
+		{
+			testHeading: "Emphasized, with thumbnail",
+			isEmphasized: true,
+			thumbnailUrl: "example-card-landscape.png",
+		},
+		// read-only
+		{
+			testHeading: "Default, read-only",
+			isReadOnly: true,
+		},
+		{
+			testHeading: "Emphasized, read-only",
+			isReadOnly: true,
 			isEmphasized: true,
 		},
+		// truncated, which many states below ignore
+		{
+			testHeading: "Truncated",
+			label: "Tag label that truncates when it gets too long",
+		}
+	],
+	stateData: [
+		// show removable as a state for test headings that don't already test it
+		{
+			testHeading: "Removable",
+			hasClearButton: true,
+			include: ["Default, with icon" , "Default, with avatar", "Emphasized, with thumbnail", "Truncated"],
+		},
+		{
+			testHeading: "Hovered",
+			isHovered: true,
+			ignore: ["Truncated"],
+		},
+		{
+			testHeading: "Focused",
+			isFocused: true,
+			ignore: ["Truncated"],
+		},
+		{
+			testHeading: "Active",
+			isActive: true,
+			ignore: ["Truncated"],
+		},
+		{
+			testHeading: "Disabled",
+			isDisabled: true,
+			ignore: ["Truncated", "Disabled with states"],
+		},
+		// testing icon/avatar/thumbnail/clear button to ensure size is applied correctly
+		{
+			testHeading: "Small",
+			size: "s",
+			include: ["Default, removable", "Default, with icon", "Default, with avatar", "Emphasized, with thumbnail"],
+		},
+		{
+			testHeading: "Large",
+			size: "l",
+			include: ["Default, removable", "Default, with icon", "Default, with avatar", "Emphasized, with thumbnail"],
+		}
 	]
 });
