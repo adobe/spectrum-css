010e080441cc2952d6b85f0d60956a33f4d48864
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2025-04-23 13:24:55 -0400

    docs(numberfield): update stories for docs page
    - updates documentation to use number field language instead of stepper
    - imports new numberfield templates
    - adds support for displaying the field label, label position, help text
    and label text
    - adds side label, invalid states stories
    - updates some sentence-case display names for stories
    - removes quiet variant stories
    - updates display title to Number field

--- components/stepper/stories/stepper.stories.js
+++ components/stepper/stories/stepper.stories.js
@@ -1,22 +1,22 @@
 import { Sizes } from "@spectrum-css/preview/decorators";
 import { disableDefaultModes } from "@spectrum-css/preview/modes";
-import { isDisabled, isFocused, isHovered, isInvalid, isKeyboardFocused, isQuiet, size } from "@spectrum-css/preview/types";
+import { isDisabled, isFocused, isHovered, isKeyboardFocused, size } from "@spectrum-css/preview/types";
+import { default as TextfieldStories } from "@spectrum-css/textfield/stories/textfield.stories.js";
 import metadata from "../dist/metadata.json";
 import packageJson from "../package.json";
-import { StepperGroup } from "./stepper.test.js";
+import { NumberFieldGroup } from "./stepper.test.js";
 import { AllDefaultVariantsGroup, DisabledVariantsGroup, Template } from "./template.js";
 
 /**
- * A stepper can be used to increment or decrement a value by a specified amount via an up/down button. An input field displays the current value.
+ * A number field can be used to increment or decrement a value by a specified amount via an up/down button. An input field displays the current value.
  */
 export default {
-	title: "Stepper",
+	title: "Number field",
 	component: "Stepper",
 	argTypes: {
 		size: size(["s", "m", "l", "xl"]),
-		isQuiet,
 		hideStepper: {
-			name: "Hide stepper",
+			name: "Hide stepper buttons",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
@@ -25,21 +25,39 @@ export default {
 			control: "boolean",
 		},
 		isDisabled,
-		isInvalid,
+		isInvalid: {
+			...TextfieldStories?.argTypes?.isInvalid ?? {},
+		},
 		isFocused,
 		isHovered,
 		isKeyboardFocused,
+		displayLabel: {
+			...TextfieldStories?.argTypes?.displayLabel ?? {},
+		},
+		label: {
+			...TextfieldStories?.argTypes?.labelText ?? {},
+		},
+		labelPosition: {
+			...TextfieldStories?.argTypes?.labelPosition ?? {},
+		},
+		helpText: {
+			...TextfieldStories?.argTypes?.helpText,
+		},
+		value: { table: { disable: true, } },
 	},
 	args: {
-		rootClass: "spectrum-Stepper",
+		rootClass: "spectrum-NumberField",
 		size: "m",
-		isQuiet: false,
 		isFocused: false,
 		isHovered: false,
 		isKeyboardFocused: false,
 		isInvalid: false,
 		isDisabled: false,
-		hideStepper: false
+		hideStepper: false,
+		displayLabel: true,
+		label: "Field label",
+		labelPosition: "top",
+		helpText: "",
 	},
 	parameters: {
 		design: {
@@ -51,7 +69,7 @@ export default {
 	},
 };
 
-export const Default = StepperGroup.bind({});
+export const Default = NumberFieldGroup.bind({});
 Default.args = {};
 Default.tags = ["!autodocs"];
 
@@ -68,7 +86,9 @@ Sizing.parameters = {
 };
 
 /**
- * Steppers come in four different sizes: small, medium, large, and extra-large. The medium size is the default and most frequently used option. Use the other sizes sparingly; they should be used to create a hierarchy of importance within the page.
+ * Number fields come in four different sizes: small, medium, large, and extra-large. The medium size is the default and most frequently used option. Use the other sizes sparingly; they should be used to create a hierarchy of importance within the page.
+ *
+ * Number fields have a [field label](/docs/components-field-label--docs) that is positioned above the field by default, with a secondary option to be positioned on the side of the field. The [inline infield buttons](/docs/components-in-field-button--docs#inline) are usually visible, but can be hidden. When the label is to long for the available space, it will wrap to the next line. If the value within the number field input overflows, it will truncate with an ellipsis.
  */
 export const DefaultStates = AllDefaultVariantsGroup.bind({});
 DefaultStates.args = {};
@@ -77,6 +97,26 @@ DefaultStates.parameters = {
 	chromatic: { disableSnapshot: true },
 };
 DefaultStates.storyName = "Default";
+
+export const SideLabel = AllDefaultVariantsGroup.bind({});
+SideLabel.args = {
+	labelPosition: "side",
+};
+SideLabel.tags = ["!dev"];
+SideLabel.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+
+export const InvalidStates = AllDefaultVariantsGroup.bind({});
+InvalidStates.args = {
+	isInvalid: true,
+	helpText: "Help text is here to assist",
+};
+InvalidStates.tags = ["!dev"];
+InvalidStates.parameters = {
+	chromatic: { disableSnapshot: true },
+};
+InvalidStates.storyName = "Invalid";
 
 export const Disabled = DisabledVariantsGroup.bind({});
 Disabled.args = {
@@ -88,19 +128,8 @@ Disabled.parameters = {
 };
 Disabled.storyName = "Disabled";
 
-export const QuietStates = AllDefaultVariantsGroup.bind({});
-QuietStates.args = {
-	isQuiet: true,
-};
-QuietStates.tags = ["!dev"];
-QuietStates.parameters = {
-	chromatic: { disableSnapshot: true },
-};
-QuietStates.storyName = "Quiet";
-
-
 // ********* VRT ONLY ********* //
-export const WithForcedColors = StepperGroup.bind({});
+export const WithForcedColors = NumberFieldGroup.bind({});
 WithForcedColors.tags = ["!autodocs", "!dev"];
 WithForcedColors.parameters = {
 	chromatic: {
@@ -111,7 +140,7 @@ WithForcedColors.parameters = {
 
 // ********* DOCS ONLY ********* //
 /**
- * Optional stepper buttons would appear to the side of the field. Regardless of if a stepper has these buttons or not, is should always accommodate arrow key shortcuts to increase or decrease the number.
+ * Number fields typically have stepper buttons	visible by default, but implementations have the option to hide them. The component should always accommodate arrow key shortcuts to increase or decrease the value, regardless of whether its stepper buttons are visible.
  */
 export const HideStepper = Template.bind({});
 HideStepper.tags = ["!dev"];
@@ -123,4 +152,4 @@ HideStepper.parameters = {
 		disableSnapshot: true,
 	},
 };
-HideStepper.storyName = "Hide stepper";
+HideStepper.storyName = "Hide stepper buttons";
