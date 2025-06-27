094f0c658fb9078cb3159c87e534553de93754f1
Rise Erpelding <rise@heysparkbox.com>
2025-06-27 13:56:47 -1000

    docs(tabs): storybook cleanup

--- components/tabs/stories/tabs.stories.js
+++ components/tabs/stories/tabs.stories.js
@@ -1,9 +1,9 @@
 import { disableDefaultModes } from "@spectrum-css/preview/modes";
-import { isEmphasized, isQuiet } from "@spectrum-css/preview/types";
+import { isEmphasized } from "@spectrum-css/preview/types";
 import metadata from "../dist/metadata.json";
 import packageJson from "../package.json";
 import { TabsGroups } from "./tabs.test.js";
-import { CompactGroup, OverflowGroup, QuietGroup, Template, VerticalGroup } from "./template.js";
+import { CompactGroup, OverflowGroup, Template, VerticalGroup } from "./template.js";
 
 /**
  * Tabs organize content into multiple sections and allow users to navigate between them. The content under the set of tabs should be related and form a coherent unit. Tabs can be either horizontal or vertical.
@@ -11,11 +11,13 @@ import { CompactGroup, OverflowGroup, QuietGroup, Template, VerticalGroup } from
  * ## Usage notes
  *
  * ### Use icons consistently
- * Icons are optional, but don’t mix the use of icons in tabs if they are utilized. Navigation controls require a clear spacial relationship to one another, and mixing the use of icons can dramatically impact the visual balance and presence for each tab item.
+ * Icons are optional, but don't mix the use of icons in tabs if they are utilized. Navigation controls require a clear spacial relationship to one another, and mixing the use of icons can dramatically impact the visual balance and presence for each tab item.
  *
  * ### Setting the selected tab item
- * Only one tab item can be selected at any given time. The selected tab item is designated by the `is-selected` class. A selection indicator line is shown under or next to the selected tab item.
+ * Only one tab item can be selected at any given time. The selected tab item is designated by the `.is-selected` class. A selection indicator line is shown under or next to the selected tab item.
  *
+ * ### Quiet tabs
+ * The quiet variant has been deprecated for Spectrum 2. By default, all tabs look similar to what was formerly the quiet variant, with no divider spanning across all tab items.
  */
 
 export default {
@@ -35,7 +37,6 @@ export default {
 			control: "select",
 			default: "horizontal",
 		},
-		isQuiet,
 		isEmphasized,
 		hasRightAlignedTabs: {
 			name: "Right-aligned tabs",
@@ -61,14 +62,12 @@ export default {
 		},
 		isCompact: {
 			name: "Compact",
-			description: "Compact tabs should never be used without the quiet styles.",
 			type: { name: "boolean" },
 			table: {
 				type: { summary: "boolean" },
 				category: "State",
 			},
 			control: "boolean",
-			if: { arg: "isQuiet", truthy: true },
 		},
 		iconOnly: {
 			name: "Icon only",
@@ -82,7 +81,6 @@ export default {
 	},
 	args: {
 		rootClass: "spectrum-Tabs",
-		isQuiet: false,
 		isEmphasized: false,
 		isCompact: false,
 		iconOnly: false,
@@ -116,23 +114,26 @@ export default {
 		},
 		packageJson,
 		metadata,
+		status: {
+			type: "migrated",
+		},
 	},
+	tags: ["migrated"],
 };
 
 /**
- * Basic, default tab items should have a label for accessibility. If a label isn’t present, it must include an icon and becomes an icon-only tab item.
+ * #### Labels
+ * Basic, default tab items should have a label for accessibility. If a label isn't present, it must include an icon and becomes an icon-only tab item.
  *
- * By default, tabs have a divider that spans across all tab items. This style works as a way to anchor them to the page. These types of tabs are best used at the top of a page, as a top-level navigation.
- *
- * Tabs are horizontal by default and should be used when horizontal space is limited.
- *
+ * #### Icons
+ * Icons can be displayed in tab items. Icons should only be used in a tab item when absolutely necessary: when adding essential value and having a strong association with the label. Icons should not be used just as decoration. If the tab item does not have a visible label, it must still have a tooltip to disclose the label.
  */
 export const Default = TabsGroups.bind({});
 Default.args = {};
 
 // ********* DOCS ONLY ********* //
 /**
- * Vertical tabs should be used when horizontal space is more generous and when the list of sections is greater than can be presented to the user in a horizontal format.
+ * Vertical tabs should be used when horizontal space is more limited and when the list of sections is greater than can be presented to the user in a horizontal format.
  */
 export const Vertical = VerticalGroup.bind({});
 Vertical.args = {
@@ -169,24 +170,12 @@ Overflow.parameters = {
 };
 
 /**
- * Quiet tabs have no visible divider across the tab items apart from the one that shows the selected tab item. These should be used as sub-level navigation or for containers.
- */
-export const Quiet = QuietGroup.bind({});
-Quiet.args = {
-	isQuiet: true,
-};
-Quiet.tags = ["!dev"];
-Quiet.parameters = {
-	chromatic: { disableSnapshot: true },
-};
-
-/**
- * Compact tabs should never be used without the quiet variation.
+ * In addition to the default, regular density, tabs also come in a compact density which has tighter spacing.
  */
 export const Compact = CompactGroup.bind({});
+Compact.storyName = "Density: Compact";
 Compact.args = {
 	isCompact: true,
-	isQuiet: true,
 };
 Compact.tags = ["!dev"];
 Compact.parameters = {
@@ -221,7 +210,7 @@ Disabled.parameters = {
 };
 
 /**
- * Emphasized tabs have blue text for the selected state for visual prominence and to draw more attention to them. This is optimal for when the selection should call attention, such as the main navigation for a website.
+ * By default, tabs are not emphasized. Emphasized tabs have blue text and selection indicatorfor the selected state for visual prominence and to draw more attention to them. This is optimal for when the selection should call attention, such as the main navigation for a website.
  */
 export const Emphasized = Template.bind({});
 Emphasized.args = {
