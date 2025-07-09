9107a09155b7abeadab6551b432f5ed07af81800
castastrophe <castastrophe@users.noreply.github.com>
2025-07-09 14:22:33 -0400

    feat(tokens): support for composite formats

--- tokens/utilities/css-sets-formatter.js
+++ tokens/utilities/css-sets-formatter.js
@@ -1,70 +1,50 @@
-import { usesReferences } from "style-dictionary/utils";
-
-export const generateNameArray = (token, prefix) => {
-	let name = prefix ? [prefix] : [];
-	const cleanTokenPath = [];
-	for (let i = 0; i < token.path.length; i++) {
-		if (token.path[i] === "sets") {
-			i++;
-		}
-		else {
-			cleanTokenPath.push(token.path[i]);
-		}
-	}
-	name = name.concat(cleanTokenPath);
-	return name;
-};
-
-/**
- * @description Formats the value of a token for CSS
- * @param {import('style-dictionary').Token} token
- * @param {import('style-dictionary').PlatformConfig} platform
- * @returns {string[]}
- */
-const valueFormatter = (value, prefix) => {
-	if (!value) return;
-
-	/* @todo resolve for the new typography objects */
-	if (typeof value === "object") {
-		return Object.keys(value).map(key => valueFormatter(key, prefix))?.flat();
-	}
-
-	if (!usesReferences(value)) return [String(value)];
-
-	const resultAr = String(value).substring(1, value.length - 1).split(".");
-	if (prefix) resultAr.splice(0, 0, prefix);
-
-	return [`var(--${resultAr.join("-")})`];
-};
+import { valueFormatter } from "./shared-logic.js";
 
 /**
  * The format function to split out the token set data into distinct CSS variables
  * @type {import('style-dictionary/types').FormatFn} format
  */
-const format = ({ dictionary, platform, options }) => {
-	let selector = '.spectrum';
+export const format = ({ dictionary, platform, options }) => {
+	const { prefix } = platform;
+	let selector = ":root";
 	const context = options.sets?.[0];
 
-	const resultAr = [];
+	const definitions = [];
 
 	dictionary.allTokens.forEach((token) => {
 		if (!token) return;
 
-		const name = generateNameArray(token, platform.prefix);
-		const values = valueFormatter(token.original?.value ?? token.value, platform.prefix);
+		const values = valueFormatter(token.original?.value ?? token?.value, { path: token.path, prefix });
 
 		if (!values || values.length === 0) return;
-		if (name[1]?.startsWith("android-")) {
-			return;
-		}
 
-		resultAr.push(...values.map(value => `  --${name.join("-")}: ${value};`));
+		values.forEach(({ key, prop, value, ref, ...sets }) => {
+			if (key.includes("android")) return;
+			// If there are no default values, check for contextual values
+			if (!value) {
+				if (context && sets[context]) definitions.push(`${prop}: ${sets[context]};`);
+				else if (sets.light) definitions.push(`${prop}: ${sets.light};`);
+				else if (sets.desktop) definitions.push(`${prop}: ${sets.desktop};`);
+				else if (ref) definitions.push(`${prop}: ${ref};`);
+			}
+			else definitions.push(`${prop}: ${value};`);
+		});
 	});
 
-	const mediaQuery = context === 'dark' ? '@media (prefers-color-scheme: dark) {\n' : '';
-	if (context && ['desktop', 'light', 'dark'].every(c => context !== c)) selector = `.spectrum--${context === 'mobile' ? 'large' : context}`;
+	if (context) {
+		switch (context) {
+			case "mobile":
+				selector = ".spectrum--large";
+				break;
+			case "desktop":
+				selector = ".spectrum--medium";
+				break;
+			default:
+				selector = `.spectrum--${context}`;
+		}
+	}
 
-	return `${mediaQuery}${selector} {\n${resultAr.join("\n")}\n}\n${mediaQuery ? '}\n' : ''}`;
+	return `${selector} { ${definitions.join(" ")} }`;
 };
 format.nested = true;
 
