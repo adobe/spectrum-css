1e8df385d2112db2204bd29f099024427503f766
castastrophe <castastrophe@users.noreply.github.com>
2025-09-16 09:29:02 -0400

    chore: remove build requirement for constraints

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -127,7 +127,7 @@ module.exports = defineConfig({
 		 * @returns {void}
 		 */
 		function validateComponentPackageJson(workspace, folderName) {
-			const hasDist = existsSync(join(workspace.cwd, "dist"));
+			const hasDist = isComponent(workspace) && folderName !== "commons";
 			const hasIndexCSS = existsSync(join(workspace.cwd, "index.css"));
 			const hasOtherCSS = fg.sync(["*.css", "!index.css"], { cwd: workspace.cwd, onlyFiles: true }).length > 0;
 
